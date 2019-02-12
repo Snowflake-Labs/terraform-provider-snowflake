e51874f5385e7d6ed1c083002ce7aa438fc08094
Ryan King <rking@chanzuckerberg.com>
2019-02-12 12:04:01 -0800

    use struct scanning for database

--- pkg/resources/database.go
+++ pkg/resources/database.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/chanzuckerberg/terraform-provider-snowflake/pkg/snowflake"
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 )
 
@@ -63,40 +64,48 @@ func CreateDatabase(data *schema.ResourceData, meta interface{}) error {
 	return ReadDatabase(data, meta)
 }
 
+type database struct {
+	CreatedOn     sql.NullString `db:"created_on"`
+	DBName        sql.NullString `db:"name"`
+	IsDefault     sql.NullString `db:"is_default"`
+	IsCurrent     sql.NullString `db:"is_current"`
+	Origin        sql.NullString `db:"origin"`
+	Owner         sql.NullString `db:"owner"`
+	Comment       sql.NullString `db:"comment"`
+	Options       sql.NullString `db:"options"`
+	RetentionTime sql.NullString `db:"retentionTime"`
+}
+
 func ReadDatabase(data *schema.ResourceData, meta interface{}) error {
 	db := meta.(*sql.DB)
+	sdb := sqlx.NewDb(db, "snowflake")
 
 	// TODO Not sure if we should use id or name here.
 	name := data.Id()
 
 	// TODO make sure there are no wildcard-y characters here, otherwise it could match more than 1 row.
 	stmt := fmt.Sprintf("SHOW DATABASES LIKE '%s'", name)
+
 	log.Printf("[DEBUG] stmt %s", stmt)
+	row := sdb.QueryRowx(stmt)
 
-	// TODO if we try to read a row and there is none, this will return an error. We should probably
-	//      do something more graceful
-	row := db.QueryRow(stmt)
-
-	var createdOn, dbname, isDefault, isCurrent, origin, owner, comment, options, retentionTime sql.NullString
-
-	err := row.Scan(
-		&createdOn, &dbname, &isDefault, &isCurrent, &origin, &owner, &comment, &options, &retentionTime,
-	)
+	database := &database{}
+	err := row.StructScan(database)
 
 	if err != nil {
 		return errors.Wrap(err, "unable to scan row for SHOW DATABASES")
 	}
 
-	err = data.Set("name", dbname.String)
+	err = data.Set("name", database.DBName.String)
 	if err != nil {
 		return err
 	}
-	err = data.Set("comment", comment.String)
+	err = data.Set("comment", database.Comment.String)
 	if err != nil {
 		return err
 	}
 
-	i, err := strconv.ParseInt(retentionTime.String, 10, 64)
+	i, err := strconv.ParseInt(database.RetentionTime.String, 10, 64)
 	if err != nil {
 		return err
 	}
