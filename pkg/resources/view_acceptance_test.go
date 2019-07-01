d46d811af52864fed1395d13798aa778415a47f4
Stuart Auld <stuart.auld@viostream.com>
2019-07-01 12:25:16 +1000

    Add support to skip share-related acceptance tests via ENV

--- pkg/resources/view_acceptance_test.go
+++ pkg/resources/view_acceptance_test.go
@@ -18,7 +18,7 @@ func TestAccView(t *testing.T) {
 				Config: viewConfig(accName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("snowflake_view.test", "name", accName),
-					resource.TestCheckResourceAttr("snowflake_view.test", "database", "DEMO_DB"),
+					resource.TestCheckResourceAttr("snowflake_view.test", "database", accName),
 					resource.TestCheckResourceAttr("snowflake_view.test", "comment", "Terraform test resource"),
 					checkBool("snowflake_view.test", "is_secure", true), // this is from user_acceptance_test.go
 				),
@@ -29,12 +29,16 @@ func TestAccView(t *testing.T) {
 
 func viewConfig(n string) string {
 	return fmt.Sprintf(`
+resource "snowflake_database" "test" {
+	name = "%v"
+}
+
 resource "snowflake_view" "test" {
 	name      = "%v"
 	comment   = "Terraform test resource"
-	database  = "DEMO_DB"
+	database  = snowflake_database.test.name
 	is_secure = true
 	statement = "SELECT ROLE_NAME, ROLE_OWNER FROM INFORMATION_SCHEMA.APPLICABLE_ROLES"
 }
-`, n)
+`, n, n)
 }
