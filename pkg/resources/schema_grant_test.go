d642f838fb54cb0f9834413cc29a7981ae8c8656
Troy Sankey <tsankey@edx.org>
2019-09-04 12:59:23 -0400

    Fix unit tests first to resemble intended correct behavior.
    Fixes #68

--- pkg/resources/schema_grant_test.go
+++ pkg/resources/schema_grant_test.go
@@ -2,6 +2,7 @@ package resources_test
 
 import (
 	"database/sql"
+	"fmt"
 	"testing"
 	"time"
 
@@ -25,30 +26,48 @@ func TestSchemaGrant(t *testing.T) {
 func TestSchemaGrantCreate(t *testing.T) {
 	a := assert.New(t)
 
-	in := map[string]interface{}{
-		"schema_name":   "test-schema",
-		"database_name": "test-db",
-		"privilege":     "USAGE",
-		"roles":         []interface{}{"test-role-1", "test-role-2"},
-		"shares":        []interface{}{"test-share-1", "test-share-2"},
-	}
-	d := schema.TestResourceDataRaw(t, resources.SchemaGrant().Schema, in)
-	a.NotNil(d)
+	for _, test_priv := range []string{"USAGE", "MODIFY"} {
+		in := map[string]interface{}{
+			"schema_name":   "test-schema",
+			"database_name": "test-db",
+			"privilege":     test_priv,
+			"roles":         []interface{}{"test-role-1", "test-role-2"},
+			"shares":        []interface{}{"test-share-1", "test-share-2"},
+		}
+		d := schema.TestResourceDataRaw(t, resources.SchemaGrant().Schema, in)
+		a.NotNil(d)
 
-	WithMockDb(t, func(db *sql.DB, mock sqlmock.Sqlmock) {
-		mock.ExpectExec(`^GRANT USAGE ON SCHEMA "test-db"."test-schema" TO ROLE "test-role-1"$`).WillReturnResult(sqlmock.NewResult(1, 1))
-		mock.ExpectExec(`^GRANT USAGE ON SCHEMA "test-db"."test-schema" TO ROLE "test-role-2"$`).WillReturnResult(sqlmock.NewResult(1, 1))
-		mock.ExpectExec(`^GRANT USAGE ON SCHEMA "test-db"."test-schema" TO SHARE "test-share-1"$`).WillReturnResult(sqlmock.NewResult(1, 1))
-		mock.ExpectExec(`^GRANT USAGE ON SCHEMA "test-db"."test-schema" TO SHARE "test-share-2"$`).WillReturnResult(sqlmock.NewResult(1, 1))
-		expectReadSchemaGrant(mock)
-		err := resources.CreateSchemaGrant(d, db)
-		a.NoError(err)
-	})
+		WithMockDb(t, func(db *sql.DB, mock sqlmock.Sqlmock) {
+			mock.ExpectExec(
+				fmt.Sprintf(`^GRANT %s ON SCHEMA "test-db"."test-schema" TO ROLE "test-role-1"$`, test_priv),
+			).WillReturnResult(sqlmock.NewResult(1, 1))
+			mock.ExpectExec(
+				fmt.Sprintf(`^GRANT %s ON SCHEMA "test-db"."test-schema" TO ROLE "test-role-2"$`, test_priv),
+			).WillReturnResult(sqlmock.NewResult(1, 1))
+			mock.ExpectExec(
+				fmt.Sprintf(`^GRANT %s ON SCHEMA "test-db"."test-schema" TO SHARE "test-share-1"$`, test_priv),
+			).WillReturnResult(sqlmock.NewResult(1, 1))
+			mock.ExpectExec(
+				fmt.Sprintf(`^GRANT %s ON SCHEMA "test-db"."test-schema" TO SHARE "test-share-2"$`, test_priv),
+			).WillReturnResult(sqlmock.NewResult(1, 1))
+			expectReadSchemaGrant(mock, test_priv)
+			err := resources.CreateSchemaGrant(d, db)
+			a.NoError(err)
+		})
+	}
 }
 
-func expectReadSchemaGrant(mock sqlmock.Sqlmock) {
+func expectReadSchemaGrant(mock sqlmock.Sqlmock, test_priv string) {
 	rows := sqlmock.NewRows([]string{
 		"created_on", "privilege", "granted_on", "name", "granted_to", "grantee_name", "grant_option", "granted_by",
-	}).AddRow(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), "USAGE", "SCHEMA", "test-schema", "ROLE", "test-role-1", false, "bob").AddRow(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), "SELECT", "VIEW", "test-view", "ROLE", "test-role-2", false, "bob").AddRow(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), "SELECT", "VIEW", "test-view", "SHARE", "test-share-1", false, "bob").AddRow(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), "SELECT", "VIEW", "test-view", "SHARE", "test-share-2", false, "bob")
+	}).AddRow(
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), test_priv, "SCHEMA", "test-schema", "ROLE", "test-role-1", false, "bob",
+	).AddRow(
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), test_priv, "SCHEMA", "test-schema", "ROLE", "test-role-2", false, "bob",
+	).AddRow(
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), test_priv, "SCHEMA", "test-schema", "SHARE", "test-share-1", false, "bob",
+	).AddRow(
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), test_priv, "SCHEMA", "test-schema", "SHARE", "test-share-2", false, "bob",
+	)
 	mock.ExpectQuery(`^SHOW GRANTS ON SCHEMA "test-db"."test-schema"$`).WillReturnRows(rows)
 }
