fa6b2b8ca5482b6ab62fbcd24061be255b7a8d11
Annie <anne.ku@chanzuckerberg.com>
2019-10-21 15:37:40 -0700

    Revert "Revert "Refactored all the pipe delimiting fields to use grantID struct""
    This reverts commit 382c76e84154d5cf7d1a7bbffe68f734a5299d41.

--- pkg/resources/grant_helpers_internal_test.go
+++ pkg/resources/grant_helpers_internal_test.go
@@ -7,52 +7,52 @@ import (
 func TestSplitGrantID(t *testing.T) {
 	// Vanilla
 	id := "database_name|schema|view_name|privilege"
-	db, schema, view, priv, err := splitGrantID(id)
+	grant, err := grantIDFromString(id)
 	if err != nil {
 		t.Error(err)
 	}
-	if db != "database_name" {
-		t.Errorf("Expected db to be database_name, got %v", db)
+	if grant.ResourceName != "database_name" {
+		t.Errorf("Expected db to be database_name, got %v", grant.ResourceName)
 	}
-	if schema != "schema" {
-		t.Errorf("Expected schema to be schema, got %v", schema)
+	if grant.SchemaName != "schema" {
+		t.Errorf("Expected schema to be schema, got %v", grant.SchemaName)
 	}
-	if view != "view_name" {
-		t.Errorf("Expected view to be view_name, got %v", view)
+	if grant.ViewName != "view_name" {
+		t.Errorf("Expected view to be view_name, got %v", grant.ViewName)
 	}
-	if priv != "privilege" {
-		t.Errorf("Expected priv to be privilege, got %v", priv)
+	if grant.Privilege != "privilege" {
+		t.Errorf("Expected priv to be privilege, got %v", grant.Privilege)
 	}
 
 	// No view
 	id = "database_name|||privilege"
-	db, schema, view, priv, err = splitGrantID(id)
+	grant, err = grantIDFromString(id)
 	if err != nil {
 		t.Error(err)
 	}
-	if db != "database_name" {
-		t.Errorf("Expected db to be database_name, got %v", db)
+	if grant.ResourceName != "database_name" {
+		t.Errorf("Expected db to be database_name, got %v", grant.ResourceName)
 	}
-	if schema != "" {
-		t.Errorf("Expected schema to be blank, got %v", schema)
+	if grant.SchemaName != "" {
+		t.Errorf("Expected schema to be blank, got %v", grant.SchemaName)
 	}
-	if view != "" {
-		t.Errorf("Expected view to be blank, got %v", view)
+	if grant.ViewName != "" {
+		t.Errorf("Expected view to be blank, got %v", grant.ViewName)
 	}
-	if priv != "privilege" {
-		t.Errorf("Expected priv to be privilege, got %v", priv)
+	if grant.Privilege != "privilege" {
+		t.Errorf("Expected priv to be privilege, got %v", grant.Privilege)
 	}
 
 	// Bad ID
 	id = "database|name-privilege"
-	_, _, _, _, err = splitGrantID(id)
+	grant, err = grantIDFromString(id)
 	if err == nil {
 		t.Error("Expected an error, got none")
 	}
 
 	// Bad ID
 	id = "database||||name-privilege"
-	_, _, _, _, err = splitGrantID(id)
+	grant, err = grantIDFromString(id)
 	if err == nil {
 		t.Error("Expected an error, got none")
 	}
