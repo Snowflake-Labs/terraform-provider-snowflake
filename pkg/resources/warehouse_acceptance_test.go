132cadc2d69980451128fea7d8d8bc607b5bbbf9
Thales Mello <thalesmello@gmail.com>
2020-01-21 13:37:32 -0800

    Add option to SKIP_DATABASE_TESTS and SKIP_WAREHOUSE_TESTS
    In the Snowflake Standard accouynt, multi-warehouses are not enabled,
    which make the warehouse integration tests fail. Similarly, the
    database integration tests fail because it doesn't allow retention
    days larger than 1 day.
    
    A contributor using a Snowflake trial account to open a pull request
    will need those two options make the tests pass in the pull request.
    Instructions on how to do it have been included in the README.

--- pkg/resources/warehouse_acceptance_test.go
+++ pkg/resources/warehouse_acceptance_test.go
@@ -2,6 +2,7 @@ package resources_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -9,6 +10,10 @@ import (
 )
 
 func TestAccWarehouse(t *testing.T) {
+	if _, ok := os.LookupEnv("SKIP_WAREHOUSE_TESTS"); ok {
+		t.Skip("Skipping TestAccWarehouse")
+	}
+
 	prefix := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
 	prefix2 := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
 
