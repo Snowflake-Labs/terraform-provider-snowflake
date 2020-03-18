6a02a96baf4f28a345373bd1e12669cb77005c2f
Henri Blancke <blanckehenri@gmail.com>
2020-03-17 22:39:55 -0400

    [ADD] future grants on schemas and databases

--- pkg/snowflake/future_grant.go
+++ pkg/snowflake/future_grant.go
@@ -5,17 +5,39 @@ import (
 )
 
 type futureGrantType string
+type futureGrantTarget string
 
 const (
-	futureTableType futureGrantType = "TABLE"
-	futureViewType  futureGrantType = "VIEW"
+	futureSchemaType futureGrantType = "SCHEMA"
+	futureTableType  futureGrantType = "TABLE"
+	futureViewType   futureGrantType = "VIEW"
+)
+
+const (
+	futureSchemaTarget   futureGrantTarget = "SCHEMA"
+	futureDatabaseTarget futureGrantTarget = "DATABASE"
 )
 
 // FutureGrantBuilder abstracts the creation of FutureGrantExecutables
 type FutureGrantBuilder struct {
-	name            string
-	qualifiedName   string
-	futureGrantType futureGrantType
+	name              string
+	qualifiedName     string
+	futureGrantType   futureGrantType
+	futureGrantTarget futureGrantTarget
+}
+
+func getNameAndQualifiedName(db, schema string) (string, string, futureGrantTarget) {
+	name := schema
+	futureTarget := futureSchemaTarget
+	qualifiedName := fmt.Sprintf(`"%v"."%v"`, db, schema)
+
+	if schema == "" {
+		name = db
+		futureTarget = futureDatabaseTarget
+		qualifiedName = fmt.Sprintf(`"%v"`, db)
+	}
+
+	return name, qualifiedName, futureTarget
 }
 
 // Name returns the object name for this FutureGrantBuilder
@@ -23,43 +45,59 @@ func (fgb *FutureGrantBuilder) Name() string {
 	return fgb.name
 }
 
+// FutureSchemaGrant returns a pointer to a FutureGrantBuilder for a schema
+func FutureSchemaGrant(db string) GrantBuilder {
+	return &FutureGrantBuilder{
+		name:              db,
+		qualifiedName:     fmt.Sprintf(`"%v"`, db),
+		futureGrantType:   futureSchemaType,
+		futureGrantTarget: futureDatabaseTarget,
+	}
+}
+
 // FutureTableGrant returns a pointer to a FutureGrantBuilder for a table
 func FutureTableGrant(db, schema string) GrantBuilder {
+	name, qualifiedName, futureTarget := getNameAndQualifiedName(db, schema)
 	return &FutureGrantBuilder{
-		name:            schema,
-		qualifiedName:   fmt.Sprintf(`"%v"."%v"`, db, schema),
-		futureGrantType: futureTableType,
+		name:              name,
+		qualifiedName:     qualifiedName,
+		futureGrantType:   futureTableType,
+		futureGrantTarget: futureTarget,
 	}
 }
 
 // FutureViewGrant returns a pointer to a FutureGrantBuilder for a view
 func FutureViewGrant(db, schema string) GrantBuilder {
+	name, qualifiedName, futureTarget := getNameAndQualifiedName(db, schema)
 	return &FutureGrantBuilder{
-		name:            schema,
-		qualifiedName:   fmt.Sprintf(`"%v"."%v"`, db, schema),
-		futureGrantType: futureViewType,
+		name:              name,
+		qualifiedName:     qualifiedName,
+		futureGrantType:   futureViewType,
+		futureGrantTarget: futureTarget,
 	}
 }
 
 // Show returns the SQL that will show all privileges on the grant
 func (fgb *FutureGrantBuilder) Show() string {
-	return fmt.Sprintf(`SHOW FUTURE GRANTS IN SCHEMA %v`, fgb.qualifiedName)
+	return fmt.Sprintf(`SHOW FUTURE GRANTS IN %v %v`, fgb.futureGrantTarget, fgb.qualifiedName)
 }
 
 // FutureGrantExecutable abstracts the creation of SQL queries to build future grants for
 // different future grant types.
 type FutureGrantExecutable struct {
-	grantName       string
-	futureGrantType futureGrantType
-	granteeName     string
+	grantName         string
+	granteeName       string
+	futureGrantType   futureGrantType
+	futureGrantTarget futureGrantTarget
 }
 
 // Role returns a pointer to a FutureGrantExecutable for a role
 func (fgb *FutureGrantBuilder) Role(n string) GrantExecutable {
 	return &FutureGrantExecutable{
-		grantName:       fgb.qualifiedName,
-		futureGrantType: fgb.futureGrantType,
-		granteeName:     n,
+		granteeName:       n,
+		grantName:         fgb.qualifiedName,
+		futureGrantType:   fgb.futureGrantType,
+		futureGrantTarget: fgb.futureGrantTarget,
 	}
 }
 
@@ -70,17 +108,17 @@ func (gb *FutureGrantBuilder) Share(n string) GrantExecutable {
 
 // Grant returns the SQL that will grant future privileges on the grant to the grantee
 func (fge *FutureGrantExecutable) Grant(p string) string {
-	return fmt.Sprintf(`GRANT %v ON FUTURE %vS IN SCHEMA %v TO ROLE "%v"`,
-		p, fge.futureGrantType, fge.grantName, fge.granteeName)
+	return fmt.Sprintf(`GRANT %v ON FUTURE %vS IN %v %v TO ROLE "%v"`,
+		p, fge.futureGrantType, fge.futureGrantTarget, fge.grantName, fge.granteeName)
 }
 
 // Revoke returns the SQL that will revoke future privileges on the grant from the grantee
 func (fge *FutureGrantExecutable) Revoke(p string) string {
-	return fmt.Sprintf(`REVOKE %v ON FUTURE %vS IN SCHEMA %v FROM ROLE "%v"`,
-		p, fge.futureGrantType, fge.grantName, fge.granteeName)
+	return fmt.Sprintf(`REVOKE %v ON FUTURE %vS IN %v %v FROM ROLE "%v"`,
+		p, fge.futureGrantType, fge.futureGrantTarget, fge.grantName, fge.granteeName)
 }
 
 // Show returns the SQL that will show all future grants on the schema
 func (fge *FutureGrantExecutable) Show() string {
-	return fmt.Sprintf(`SHOW FUTURE GRANTS IN SCHEMA %v`, fge.grantName)
+	return fmt.Sprintf(`SHOW FUTURE GRANTS IN %v %v`, fge.futureGrantTarget, fge.grantName)
 }
--- pkg/snowflake/future_grant_test.go
+++ pkg/snowflake/future_grant_test.go
@@ -7,6 +7,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestFutureSchemaGrant(t *testing.T) {
+	a := assert.New(t)
+	fvg := snowflake.FutureSchemaGrant("test_db")
+	a.Equal(fvg.Name(), "test_db")
+
+	s := fvg.Show()
+	a.Equal(`SHOW FUTURE GRANTS IN DATABASE "test_db"`, s)
+
+	s = fvg.Role("bob").Grant("USAGE")
+	a.Equal(`GRANT USAGE ON FUTURE SCHEMAS IN DATABASE "test_db" TO ROLE "bob"`, s)
+
+	s = fvg.Role("bob").Revoke("USAGE")
+	a.Equal(`REVOKE USAGE ON FUTURE SCHEMAS IN DATABASE "test_db" FROM ROLE "bob"`, s)
+}
+
 func TestFutureTableGrant(t *testing.T) {
 	a := assert.New(t)
 	fvg := snowflake.FutureTableGrant("test_db", "PUBLIC")
@@ -20,6 +35,19 @@ func TestFutureTableGrant(t *testing.T) {
 
 	s = fvg.Role("bob").Revoke("USAGE")
 	a.Equal(`REVOKE USAGE ON FUTURE TABLES IN SCHEMA "test_db"."PUBLIC" FROM ROLE "bob"`, s)
+
+	b := assert.New(t)
+	fvgd := snowflake.FutureTableGrant("test_db", "")
+	b.Equal(fvgd.Name(), "test_db")
+
+	s = fvgd.Show()
+	b.Equal(`SHOW FUTURE GRANTS IN DATABASE "test_db"`, s)
+
+	s = fvgd.Role("bob").Grant("USAGE")
+	b.Equal(`GRANT USAGE ON FUTURE TABLES IN DATABASE "test_db" TO ROLE "bob"`, s)
+
+	s = fvgd.Role("bob").Revoke("USAGE")
+	b.Equal(`REVOKE USAGE ON FUTURE TABLES IN DATABASE "test_db" FROM ROLE "bob"`, s)
 }
 
 func TestFutureViewGrant(t *testing.T) {
@@ -35,6 +63,19 @@ func TestFutureViewGrant(t *testing.T) {
 
 	s = fvg.Role("bob").Revoke("USAGE")
 	a.Equal(`REVOKE USAGE ON FUTURE VIEWS IN SCHEMA "test_db"."PUBLIC" FROM ROLE "bob"`, s)
+
+	b := assert.New(t)
+	fvgd := snowflake.FutureViewGrant("test_db", "")
+	b.Equal(fvgd.Name(), "test_db")
+
+	s = fvgd.Show()
+	b.Equal(`SHOW FUTURE GRANTS IN DATABASE "test_db"`, s)
+
+	s = fvgd.Role("bob").Grant("USAGE")
+	b.Equal(`GRANT USAGE ON FUTURE VIEWS IN DATABASE "test_db" TO ROLE "bob"`, s)
+
+	s = fvgd.Role("bob").Revoke("USAGE")
+	b.Equal(`REVOKE USAGE ON FUTURE VIEWS IN DATABASE "test_db" FROM ROLE "bob"`, s)
 }
 
 func TestShowFutureGrantsInSchema(t *testing.T) {
@@ -42,6 +83,12 @@ func TestShowFutureGrantsInSchema(t *testing.T) {
 	s := snowflake.FutureTableGrant("test_db", "PUBLIC").Role("testRole").Show()
 	a.Equal(`SHOW FUTURE GRANTS IN SCHEMA "test_db"."PUBLIC"`, s)
 
+	s = snowflake.FutureTableGrant("test_db", "").Role("testRole").Show()
+	a.Equal(`SHOW FUTURE GRANTS IN DATABASE "test_db"`, s)
+
 	s = snowflake.FutureViewGrant("test_db", "PUBLIC").Role("testRole").Show()
 	a.Equal(`SHOW FUTURE GRANTS IN SCHEMA "test_db"."PUBLIC"`, s)
+
+	s = snowflake.FutureViewGrant("test_db", "").Role("testRole").Show()
+	a.Equal(`SHOW FUTURE GRANTS IN DATABASE "test_db"`, s)
 }
