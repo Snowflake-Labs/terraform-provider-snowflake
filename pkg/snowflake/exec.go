e7d6b048432f4e57bba2c5b21352f18b44b5906e
Ryan King <rking@chanzuckerberg.com>
2020-05-12 12:47:49 -0700

    docs

--- pkg/snowflake/exec.go
+++ pkg/snowflake/exec.go
@@ -14,13 +14,18 @@ func Exec(db *sql.DB, query string) error {
 	return err
 }
 
+// QueryRow will run stmt against the db and return the row. We use
+// [DB.Unsafe](https://godoc.org/github.com/jmoiron/sqlx#DB.Unsafe) so that we can scan to structs
+// without worrying about newly introduced columns
 func QueryRow(db *sql.DB, stmt string) *sqlx.Row {
 	log.Print("[DEBUG] stmt ", stmt)
 	sdb := sqlx.NewDb(db, "snowflake").Unsafe()
 	return sdb.QueryRowx(stmt)
 }
 
-// TODO https://godoc.org/github.com/jmoiron/sqlx#Stmt.Unsafe
+// Query will run stmt against the db and return the rows. We use
+// [DB.Unsafe](https://godoc.org/github.com/jmoiron/sqlx#DB.Unsafe) so that we can scan to structs
+// without worrying about newly introduced columns
 func Query(db *sql.DB, stmt string) (*sqlx.Rows, error) {
 	sdb := sqlx.NewDb(db, "snowflake").Unsafe()
 	return sdb.Queryx(stmt)
