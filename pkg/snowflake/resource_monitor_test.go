82054a3811da3ee4ba1056ecbe074dde52bf0e13
andybeeswax <48691060+andybeeswax@users.noreply.github.com>
2020-10-12 20:45:14 -0400

    change credit quota to float on read, int on write

--- pkg/snowflake/resource_monitor_test.go
+++ pkg/snowflake/resource_monitor_test.go
@@ -22,20 +22,15 @@ func TestResourceMonitor(t *testing.T) {
 	r.Equal(`DROP RESOURCE MONITOR "resource_monitor"`, q)
 
 	ab := rm.Alter()
-	ab.SetFloat("credit_quota", 66.6)
+	ab.SetInt("credit_quota", 66)
 	q = ab.Statement()
-	r.Equal(`ALTER RESOURCE MONITOR "resource_monitor" SET CREDIT_QUOTA=66.60`, q)
+	r.Equal(`ALTER RESOURCE MONITOR "resource_monitor" SET CREDIT_QUOTA=66`, q)
 
 	cb := snowflake.ResourceMonitor("resource_monitor").Create()
 	cb.NotifyAt(80).NotifyAt(90).SuspendAt(95).SuspendImmediatelyAt(100)
 	cb.SetString("frequency", "YEARLY")
 
-	cb.SetFloat("credit_quota", 666.66666666)
+	cb.SetInt("credit_quota", 666)
 	q = cb.Statement()
-	r.Equal(`CREATE RESOURCE MONITOR "resource_monitor" FREQUENCY='YEARLY' CREDIT_QUOTA=666.67 TRIGGERS ON 80 PERCENT DO NOTIFY ON 90 PERCENT DO NOTIFY ON 95 PERCENT DO SUSPEND ON 100 PERCENT DO SUSPEND_IMMEDIATE`, q)
-
-	// Check if credit quota can be parsed correctly to float if given an integer
-	cb.SetFloat("credit_quota", 666)
-	q = cb.Statement()
-	r.Equal(`CREATE RESOURCE MONITOR "resource_monitor" FREQUENCY='YEARLY' CREDIT_QUOTA=666.00 TRIGGERS ON 80 PERCENT DO NOTIFY ON 90 PERCENT DO NOTIFY ON 95 PERCENT DO SUSPEND ON 100 PERCENT DO SUSPEND_IMMEDIATE`, q)
+	r.Equal(`CREATE RESOURCE MONITOR "resource_monitor" FREQUENCY='YEARLY' CREDIT_QUOTA=666 TRIGGERS ON 80 PERCENT DO NOTIFY ON 90 PERCENT DO NOTIFY ON 95 PERCENT DO SUSPEND ON 100 PERCENT DO SUSPEND_IMMEDIATE`, q)
 }
