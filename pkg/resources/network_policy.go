400124866567fd938d99f61f3fd62b1b1ff4e095
Graham Place <grahamgplace@gmail.com>
2020-10-19 16:58:39 -0700

    Split network policy attachment out into separate resource

--- pkg/resources/network_policy.go
+++ pkg/resources/network_policy.go
@@ -37,20 +37,6 @@ var networkPolicySchema = map[string]*schema.Schema{
 		Optional:    true,
 		Description: "Specifies a comment for the network policy.",
 	},
-	"set_for_account": {
-		Type:        schema.TypeBool,
-		Optional:    true,
-		Description: "Specifies whether this network policy should be applied globally to your Snowflake account<br><br>**Note:** The Snowflake user running `terraform apply` must be on an IP address allowed by the network policy to set that policy globally on the Snowflake account.<br><br>Additionally, a Snowflake account can only have one network policy set globally at any given time. This resource does not enforce one-policy-per-account, it is the user's responsibility to enforce this. If multiple network policy resources have `set_for_account: true`, the final policy set on the account will be non-deterministic.",
-		Default:     false,
-		ForceNew:    true,
-	},
-	"users": {
-		Type:        schema.TypeSet,
-		Elem:        &schema.Schema{Type: schema.TypeString},
-		Optional:    true,
-		Description: "Specifies which users this network policy should be applied to.<br><br>**Note**: The network policy resource creates exclusive assignments of policies to users. Any users that have a network policy assigned outside of that policy's Terraform resource definition will have the policy unset.<br><br>For safety, you should *only* ever assign/unset network policies to users via Terraform.<br><br>Additionally, a Snowflake user can only have one network policy applied to it. This resource does not enforce one-policy-per-user, it is the user's responsibility to enforce this. If a user appears in multiple network policy resource definitions, the final policy set on the user will be non-deterministic.",
-		ForceNew:    true,
-	},
 }
 
 // NetworkPolicy returns a pointer to the resource representing a network policy
@@ -87,29 +73,12 @@ func CreateNetworkPolicy(data *schema.ResourceData, meta interface{}) error {
 		builder.WithBlockedIpList(expandStringList(v.(*schema.Set).List()))
 	}
 
-	err := ensureUserAlterPrivileges(data, meta)
-	if err != nil {
-		return err
-	}
-
 	stmt := builder.Create()
-	err = snowflake.Exec(db, stmt)
+	err := snowflake.Exec(db, stmt)
 	if err != nil {
 		return errors.Wrapf(err, "error creating network policy %v", name)
 	}
 	data.SetId(name)
-
-	if data.Get("set_for_account").(bool) {
-		err := setOnAccount(data, meta)
-		if err != nil {
-			return errors.Wrapf(err, "error creating network policy %v", name)
-		}
-	}
-
-	err = setOnUsers(data, meta)
-	if err != nil {
-		return err
-	}
 
 	return ReadNetworkPolicy(data, meta)
 }
@@ -233,31 +202,13 @@ func DeleteNetworkPolicy(data *schema.ResourceData, meta interface{}) error {
 	db := meta.(*sql.DB)
 	name := data.Id()
 
-	err := ensureUserAlterPrivileges(data, meta)
-	if err != nil {
-		return err
-	}
-
 	dropSql := snowflake.NetworkPolicy(name).Drop()
-	err = snowflake.Exec(db, dropSql)
+	err := snowflake.Exec(db, dropSql)
 	if err != nil {
 		return errors.Wrapf(err, "error deleting network policy %v", name)
 	}
 
 	data.SetId("")
-
-	if data.Get("set_for_account").(bool) {
-		err := unsetOnAccount(data, meta)
-		if err != nil {
-			return errors.Wrapf(err, "error deleting network policy %v", name)
-		}
-	}
-
-	err = unsetOnUsers(data, meta)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
@@ -270,88 +221,4 @@ func ipChangeParser(data *schema.ResourceData, key string) []string {
 		newIps[idx] = fmt.Sprintf("%v", value)
 	}
 	return newIps
-}
-
-// setOnAccount sets the network policy globally for the Snowflake account
-// Note: the ip address of the session executing this SQL must be allowed by the network policy being set
-func setOnAccount(data *schema.ResourceData, meta interface{}) error {
-	db := meta.(*sql.DB)
-	name := data.Id()
-
-	acctSql := snowflake.NetworkPolicy(name).SetOnAccount()
-
-	err := snowflake.Exec(db, acctSql)
-	if err != nil {
-		return errors.Wrapf(err, "error setting network policy %v on account", name)
-	}
-
-	return nil
-}
-
-// setOnAccount unsets the network policy globally for the Snowflake account
-func unsetOnAccount(data *schema.ResourceData, meta interface{}) error {
-	db := meta.(*sql.DB)
-	name := data.Id()
-
-	acctSql := snowflake.NetworkPolicy(name).UnsetOnAccount()
-
-	err := snowflake.Exec(db, acctSql)
-	if err != nil {
-		return errors.Wrapf(err, "error unsetting network policy %v on account", name)
-	}
-
-	return nil
-}
-
-// setOnUsers sets the network policy for all users currently specified in the network policy resource
-func setOnUsers(data *schema.ResourceData, meta interface{}) error {
-	db := meta.(*sql.DB)
-	name := data.Get("name").(string)
-	if u, ok := data.GetOk("users"); ok {
-		users := expandStringList(u.(*schema.Set).List())
-		for _, user := range users {
-			userSql := snowflake.NetworkPolicy(name).SetOnUser(user)
-			err := snowflake.Exec(db, userSql)
-			if err != nil {
-				return errors.Wrapf(err, "error setting network policy %v on user %v", name, user)
-			}
-		}
-	}
-
-	return nil
-}
-
-// unsetOnUsers unsets the network policy for all users currently specified in the network policy resource
-func unsetOnUsers(data *schema.ResourceData, meta interface{}) error {
-	db := meta.(*sql.DB)
-	name := data.Get("name").(string)
-	if u, ok := data.GetOk("users"); ok {
-		users := expandStringList(u.(*schema.Set).List())
-		for _, user := range users {
-			userSql := snowflake.NetworkPolicy(name).UnsetOnUser(user)
-			err := snowflake.Exec(db, userSql)
-			if err != nil {
-				return errors.Wrapf(err, "error unsetting network policy %v on user %v", name, user)
-			}
-		}
-	}
-
-	return nil
-}
-
-// ensureUserAlterPrivileges ensures the executing Snowflake user can alter all users in the network policy resource definition
-func ensureUserAlterPrivileges(data *schema.ResourceData, meta interface{}) error {
-	db := meta.(*sql.DB)
-	if u, ok := data.GetOk("users"); ok {
-		users := expandStringList(u.(*schema.Set).List())
-		for _, user := range users {
-			userDescSql := snowflake.User(user).Describe()
-			err := snowflake.Exec(db, userDescSql)
-			if err != nil {
-				return errors.Wrapf(err, "error altering network policy of user %v", user)
-			}
-		}
-	}
-
-	return nil
 }
---
+++ pkg/resources/network_policy_attachment_test.go
@@ -0,0 +1,63 @@
+package resources_test
+
+import (
+	"database/sql"
+	"testing"
+
+	sqlmock "github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/require"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+
+	"github.com/chanzuckerberg/terraform-provider-snowflake/pkg/provider"
+	"github.com/chanzuckerberg/terraform-provider-snowflake/pkg/resources"
+	. "github.com/chanzuckerberg/terraform-provider-snowflake/pkg/testhelpers"
+)
+
+func TestNetworkPolicyAttachment(t *testing.T) {
+	r := require.New(t)
+	err := resources.NetworkPolicyAttachment().InternalValidate(provider.Provider().Schema, true)
+	r.NoError(err)
+}
+
+func TestNetworkPolicyAttachmentCreate(t *testing.T) {
+	r := require.New(t)
+
+	in := map[string]interface{}{
+		"network_policy_name": "test-network-policy",
+		"set_for_account":     true,
+		"users":               []interface{}{"test-user"},
+	}
+	d := schema.TestResourceDataRaw(t, resources.NetworkPolicyAttachment().Schema, in)
+	r.NotNil(d)
+
+	WithMockDb(t, func(db *sql.DB, mock sqlmock.Sqlmock) {
+		mock.ExpectExec(`^ALTER ACCOUNT SET NETWORK_POLICY = "test-network-policy"$`).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec(`^DESCRIBE USER "test-user"$`).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec(`^ALTER USER "test-user" SET NETWORK_POLICY = "test-network-policy"$`).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		err := resources.CreateNetworkPolicyAttachment(d, db)
+		r.NoError(err)
+	})
+}
+
+func TestNetworkPolicyAttachmentDelete(t *testing.T) {
+	r := require.New(t)
+
+	in := map[string]interface{}{
+		"network_policy_name": "test-network-policy",
+		"set_for_account":     true,
+		"users":               []interface{}{"test-user"},
+	}
+	d := schema.TestResourceDataRaw(t, resources.NetworkPolicyAttachment().Schema, in)
+	r.NotNil(d)
+
+	WithMockDb(t, func(db *sql.DB, mock sqlmock.Sqlmock) {
+		mock.ExpectExec(`^ALTER ACCOUNT UNSET NETWORK_POLICY$`).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec(`^DESCRIBE USER "test-user"$`).WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectExec(`^ALTER USER "test-user" UNSET NETWORK_POLICY$`).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		err := resources.DeleteNetworkPolicyAttachment(d, db)
+		r.NoError(err)
+	})
+}
