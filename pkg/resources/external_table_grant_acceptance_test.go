95d45adba7421f32bd8e7c02b4417a5752b2e063
Robert Spurrier <robert_spurrier@progressive.com>
2020-11-13 16:25:04 -0500

    adding rest of acceptance tests

--- pkg/resources/external_table_grant_acceptance_test.go
+++ pkg/resources/external_table_grant_acceptance_test.go
@@ -9,7 +9,7 @@ import (
 	"text/template"
 )
 
-func TestAccMaterializedViewGrantFuture(t *testing.T) {
+func TestAccExternalTableGrantFuture(t *testing.T) {
 	databaseName := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
 	schemaName := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
 	roleName := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
@@ -18,21 +18,21 @@ func TestAccMaterializedViewGrantFuture(t *testing.T) {
 		Providers: providers(),
 		Steps: []resource.TestStep{
 			{
-				Config: materializedViewGrantConfigFuture(t, databaseName, schemaName ,roleName),
+				Config: externalTableGrantConfigFuture(t, databaseName, schemaName ,roleName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("snowflake_materialized_view_grant.test", "database_name", databaseName),
-					resource.TestCheckResourceAttr("snowflake_materialized_view_grant.test", "schema_name", schemaName),
-					resource.TestCheckResourceAttr("snowflake_materialized_view_grant.test", "materialized_view_name", ""),
-					resource.TestCheckResourceAttr("snowflake_materialized_view_grant.test", "with_grant_option", "false"),
-					resource.TestCheckResourceAttr("snowflake_materialized_view_grant.test", "on_future", "true"),
-					resource.TestCheckResourceAttr("snowflake_materialized_view_grant.test", "privilege", "SELECT"),
+					resource.TestCheckResourceAttr("snowflake_external_table_grant.test", "database_name", databaseName),
+					resource.TestCheckResourceAttr("snowflake_external_table_grant.test", "schema_name", schemaName),
+					resource.TestCheckResourceAttr("snowflake_external_table_grant.test", "external_table_name", ""),
+					resource.TestCheckResourceAttr("snowflake_external_table_grant.test", "with_grant_option", "false"),
+					resource.TestCheckResourceAttr("snowflake_external_table_grant.test", "on_future", "true"),
+					resource.TestCheckResourceAttr("snowflake_external_table_grant.test", "privilege", "SELECT"),
 				),
 			},
 		},
 	})
 }
 
-func materializedViewGrantConfigFuture(t *testing.T, database_name, schema_name, role string) string {
+func externalTableGrantConfigFuture(t *testing.T, database_name, schema_name, role string) string {
 	r := require.New(t)
 
 	config := `
@@ -49,9 +49,9 @@ resource "snowflake_role" "test" {
   name = "{{.role_name}}"
 }
 
-resource "snowflake_materialized_view_grant" "test" {
+resource "snowflake_external_table_grant" "test" {
     database_name = snowflake_database.test.name	
-	roles         = [snowflake_role.test.name]
+	roles         = ["{{.role_name}}"]
 	schema_name   = snowflake_schema.test.name
 	on_future = true
 	depends_on = [snowflake_role.test]
