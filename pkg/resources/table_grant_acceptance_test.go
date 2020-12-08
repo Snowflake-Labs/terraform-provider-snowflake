77591dcb8c9deceb97ab119e9480fb39488baa94
Ryan King <rking@chanzuckerberg.com>
2020-12-07 20:16:34 -0800

    fix priv

--- pkg/resources/table_grant_acceptance_test.go
+++ pkg/resources/table_grant_acceptance_test.go
@@ -25,7 +25,7 @@ func TestAccTableGrant_defaults(t *testing.T) {
 					resource.TestCheckResourceAttr("snowflake_table_grant.g", "database_name", name),
 					resource.TestCheckResourceAttr("snowflake_table_grant.g", "schema_name", name),
 					resource.TestCheckResourceAttr("snowflake_table_grant.g", "table_name", name),
-					resource.TestCheckResourceAttr("snowflake_table_grant.g", "privilege", "READ"),
+					resource.TestCheckResourceAttr("snowflake_table_grant.g", "privilege", "SELECT"),
 					testRolesAndShares(t, "snowflake_table_grant.g", []string{name}, []string{}),
 				),
 			},
