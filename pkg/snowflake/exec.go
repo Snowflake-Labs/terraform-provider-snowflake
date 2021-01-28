ca4d48c383f4be4c0479b014476ecd2341c99521
edulop <elopez@chanzuckerberg.com>
2021-01-28 10:59:02 -0500

    [fix] Multi-statement revokes not allowed

--- pkg/snowflake/exec.go
+++ pkg/snowflake/exec.go
@@ -14,6 +14,23 @@ func Exec(db *sql.DB, query string) error {
 	return err
 }
 
+func ExecMulti(db *sql.DB, queries []string) error {
+	log.Print("[DEBUG] exec stmts ", queries)
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	for _, query := range queries {
+		_, err = tx.Exec(query)
+		if err != nil {
+			return tx.Rollback()
+		}
+	}
+	return tx.Commit()
+}
+
 // QueryRow will run stmt against the db and return the row. We use
 // [DB.Unsafe](https://godoc.org/github.com/jmoiron/sqlx#DB.Unsafe) so that we can scan to structs
 // without worrying about newly introduced columns
