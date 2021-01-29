28b06fc44c527de64fdef2569d49063b5a1c6fd7
Ryan King <rking@chanzuckerberg.com>
2021-01-29 12:41:06 -0800

    [fix] schema import docs

--- examples/resources/snowflake_schema/import.sh
+++ examples/resources/snowflake_schema/import.sh
@@ -1,2 +1,1 @@
-# format is schema name | privilege | true/false for with_grant_option
-terraform import snowflake_schema.example 'schemaName|USAGE|true'
+terraform import snowflake_schema.example 'datbaseName|schemaName'
