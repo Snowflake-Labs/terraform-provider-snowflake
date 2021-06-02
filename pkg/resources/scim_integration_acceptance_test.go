63753b764fc589b16b4504dcd8930521176bbebc
Christopher Isidora <christopher.isidora@ns.nl>
2021-06-02 23:49:35 +0200

    Followed ACC convention

--- pkg/resources/scim_integration_acceptance_test.go
+++ pkg/resources/scim_integration_acceptance_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccScimIntegration(t *testing.T) {
+func TestAcc_ScimIntegration(t *testing.T) {
 	if _, ok := os.LookupEnv("SKIP_SCIM_INTEGRATION_TESTS"); ok {
 		t.Skip("Skipping TestAccScimIntegration")
 	}
