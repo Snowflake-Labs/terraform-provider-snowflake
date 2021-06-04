c58f9e159636273acf6e0831dadc5924ce179188
ChrisIsidora <christopher.isidora@gmail.com>
2021-06-05 00:50:34 +0200

    Add SCIM access token Datasource (#557)

--- examples/resources/snowflake_scim_integration/resource.tf
+++ examples/resources/snowflake_scim_integration/resource.tf
@@ -3,5 +3,4 @@ resource "snowflake_scim_integration" "aad" {
   network_policy   = "AAD_NETWORK_POLICY"
   provisioner_role = "AAD_PROVISIONER"
   scim_client      = "AZURE"
-  enabled          = true
 }
