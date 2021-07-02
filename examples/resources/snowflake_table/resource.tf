96a4228a899a9caa8ec324c7ce2753d0565129bf
Ben Rosen <ben.rosen@smartsheet.com>
2021-07-02 12:31:00 -0700

    update example

--- examples/resources/snowflake_table/resource.tf
+++ examples/resources/snowflake_table/resource.tf
@@ -1,20 +1,22 @@
 resource snowflake_table table {
-  database   = "database"
-  schema     = "schema"
-  name       = "table"
-  comment    = "A table."
-  cluster_by = ["to_date(DATE)"]
-  
-  owner      = "me"
+  database    = "database"
+  schema      = "schema"
+  name        = "table"
+  comment     = "A table."
+  cluster_by  = ["to_date(DATE)"]
+  primary_key = ["\"data\""]
+  owner       = "me"
   
   column {
-    name = "id"
-    type = "int"
+    name     = "id"
+    type     = "int"
+    nullable = true
   }
 
   column {
-    name = "data"
-    type = "text"
+    name     = "data"
+    type     = "text"
+    nullable = false
   }
 
   column {
