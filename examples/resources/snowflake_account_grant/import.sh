0a880b360a0549ccbc48a3f05cea651a4c1e1385
Naoki Kanatani <k12naoki@gmail.com>
2021-07-14 07:06:31 +0900

    Fix import format for snowflake_account_grant
    Same problem as #429.
    The argument of `grantIDFromString` named `stringID` expects 4 or 5 fields delimitered by "|".

--- examples/resources/snowflake_account_grant/import.sh
+++ examples/resources/snowflake_account_grant/import.sh
@@ -1,2 +1,2 @@
-# format is account name | privilege | true/false for with_grant_option
-terraform import snowflake_account_grant.example 'accountName|USAGE|true'
+# format is account name | | | privilege | true/false for with_grant_option
+terraform import snowflake_account_grant.example 'accountName|||USAGE|true'
