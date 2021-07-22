b75a091dae43a15a8deab3d85d6599c1c868fc86
Robert Bruce <robert.bruce@crimsonmacaw.com>
2021-07-22 13:35:45 +0100

    Updated docs for table resource

--- examples/resources/snowflake_table/resource.tf
+++ examples/resources/snowflake_table/resource.tf
@@ -1,10 +1,12 @@
-resource snowflake_table table {
-  database   = "database"
-  schema     = "schmea"
-  name       = "table"
-  comment    = "A table."
-  cluster_by = ["to_date(DATE)"]
-  
+resource "snowflake_table" "table" {
+  database            = "database"
+  schema              = "schmea"
+  name                = "table"
+  comment             = "A table."
+  cluster_by          = ["to_date(DATE)"]
+  data_retention_days = 1
+  change_tracking     = false
+
   column {
     name     = "id"
     type     = "int"
@@ -22,9 +24,14 @@ resource snowflake_table table {
     type = "TIMESTAMP_NTZ(9)"
   }
 
+  column {
+    name    = "extra"
+    type    = "VARIANT"
+    comment = "extra data"
+  }
+
   primary_key {
     name = "my_key"
     keys = ["data"]
-
   }
 }
