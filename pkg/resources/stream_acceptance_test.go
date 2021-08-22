2546704a04410adfdb27593f182492484a2de0a2
Michael Koslap <michael.koslap@gmail.com>
2021-08-22 08:20:28 -0400

    fix: Updated stream acceptance test for append_only

--- pkg/resources/stream_acceptance_test.go
+++ pkg/resources/stream_acceptance_test.go
@@ -16,7 +16,7 @@ func TestAcc_Stream(t *testing.T) {
 		Providers: providers(),
 		Steps: []resource.TestStep{
 			{
-				Config: streamConfig(accName),
+				Config: streamConfig(accName, false),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("snowflake_stream.test_stream", "name", accName),
 					resource.TestCheckResourceAttr("snowflake_stream.test_stream", "database", accName),
@@ -27,59 +27,8 @@ func TestAcc_Stream(t *testing.T) {
 					checkBool("snowflake_stream.test_stream", "show_initial_rows", false),
 				),
 			},
-		},
-	})
-}
-
-func streamConfig(name string) string {
-	s := `
-resource "snowflake_database" "test_database" {
-	name    = "%s"
-	comment = "Terraform acceptance test"
-}
-
-resource "snowflake_schema" "test_schema" {
-	name     = "%s"
-	database = snowflake_database.test_database.name
-	comment  = "Terraform acceptance test"
-}
-
-resource "snowflake_table" "test_stream_on_table" {
-	database        = snowflake_database.test_database.name
-	schema          = snowflake_schema.test_schema.name
-	name            = "STREAM_ON_TABLE"
-	comment         = "Terraform acceptance test"
-	change_tracking = true
-
-	column {
-		name = "column1"
-		type = "VARIANT"
-	}
-	column {
-		name = "column2"
-		type = "VARCHAR(16777216)"
-	}
-}
-
-resource "snowflake_stream" "test_stream" {
-	database = snowflake_database.test_database.name
-	schema   = snowflake_schema.test_schema.name
-	name     = "%s"
-	comment  = "Terraform acceptance test"
-	on_table = "${snowflake_database.test_database.name}.${snowflake_schema.test_schema.name}.${snowflake_table.test_stream_on_table.name}"
-}
-`
-	return fmt.Sprintf(s, name, name, name)
-}
-
-func TestAcc_Stream_AppendOnly(t *testing.T) {
-	accName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
-
-	resource.ParallelTest(t, resource.TestCase{
-		Providers: providers(),
-		Steps: []resource.TestStep{
 			{
-				Config: streamConfig(accName),
+				Config: streamConfig(accName, true),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("snowflake_stream.test_stream", "name", accName),
 					resource.TestCheckResourceAttr("snowflake_stream.test_stream", "database", accName),
@@ -94,7 +43,12 @@ func TestAcc_Stream_AppendOnly(t *testing.T) {
 	})
 }
 
-func streamAppendOnlyConfig(name string) string {
+func streamConfig(name string, append_only bool) string {
+	append_only_config := ""
+	if append_only {
+		append_only_config = "append_only = true"
+	}
+
 	s := `
 resource "snowflake_database" "test_database" {
 	name    = "%s"
@@ -130,8 +84,8 @@ resource "snowflake_stream" "test_stream" {
 	name        = "%s"
 	comment     = "Terraform acceptance test"
 	on_table    = "${snowflake_database.test_database.name}.${snowflake_schema.test_schema.name}.${snowflake_table.test_stream_on_table.name}"
-	append_only = true
+	%s
 }
 `
-	return fmt.Sprintf(s, name, name, name)
+	return fmt.Sprintf(s, name, name, name, append_only_config)
 }
