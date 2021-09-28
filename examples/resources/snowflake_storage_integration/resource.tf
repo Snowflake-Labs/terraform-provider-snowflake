43f7295a506e411d332e472431ee15708d0aafdc
Bram Neijt <bram@neijt.nl>
2021-09-28 14:37:18 +0200

    Add extensive example of setting up a storage integration

--- examples/resources/snowflake_storage_integration/resource.tf
+++ examples/resources/snowflake_storage_integration/resource.tf
@@ -1,17 +1,40 @@
-resource snowflake_storage_integration integration {
-  name    = "storage"
+locals {
+  snowflake = {
+    role_name = "snowflake"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "snowflake" {
+  name               = local.snowflake.role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_snowflake.json
+}
+
+data "aws_iam_policy_document" "assume_snowflake" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [snowflake_storage_integration.integration.storage_aws_iam_user_arn]
+    }
+    actions = ["sts:AssumeRole"]
+    condition {
+      test     = "StringEquals"
+      values   = [snowflake_storage_integration.integration.storage_aws_external_id]
+      variable = "sts:ExternalId"
+    }
+  }
+}
+
+resource "snowflake_storage_integration" "integration" {
+  name    = "storage_integration"
   comment = "A storage integration."
   type    = "EXTERNAL_STAGE"
 
   enabled = true
 
-  #   storage_allowed_locations = [""]
-  #   storage_blocked_locations = [""]
+  storage_allowed_locations = ["*"]
 
-  storage_provider         = "S3"
-  storage_aws_external_id  = "..."
-  storage_aws_iam_user_arn = "..."
-  storage_aws_role_arn     = "..."
-
-  # azure_tenant_id
+  storage_provider     = "S3"
+  storage_aws_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.snowflake.role_name}"
 }
