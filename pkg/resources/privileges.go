54c1f3a8fe14bed032a995994d6e3be8b369b365
vritzenthaler <vincent.ritzenthaler@accor.com>
2021-11-09 16:19:52 +0100

    handle-account-grant-managed-task

--- pkg/resources/privileges.go
+++ pkg/resources/privileges.go
@@ -53,15 +53,16 @@ const (
 	privilegeApply                     Privilege = "APPLY"
 	privilegeAttachPolicy              Privilege = "ATTACH POLICY"
 
-	privilegeCreateRole        Privilege = "CREATE ROLE"
-	privilegeCreateUser        Privilege = "CREATE USER"
-	privilegeCreateWarehouse   Privilege = "CREATE WAREHOUSE"
-	privilegeCreateDatabase    Privilege = "CREATE DATABASE"
-	privilegeCreateIntegration Privilege = "CREATE INTEGRATION"
-	privilegeManageGrants      Privilege = "MANAGE GRANTS"
-	privilegeMonitorUsage      Privilege = "MONITOR USAGE"
-	privilegeMonitorExecution  Privilege = "MONITOR EXECUTION"
-	privilegeExecuteTask       Privilege = "EXECUTE TASK"
+	privilegeCreateRole         Privilege = "CREATE ROLE"
+	privilegeCreateUser         Privilege = "CREATE USER"
+	privilegeCreateWarehouse    Privilege = "CREATE WAREHOUSE"
+	privilegeCreateDatabase     Privilege = "CREATE DATABASE"
+	privilegeCreateIntegration  Privilege = "CREATE INTEGRATION"
+	privilegeManageGrants       Privilege = "MANAGE GRANTS"
+	privilegeMonitorUsage       Privilege = "MONITOR USAGE"
+	privilegeMonitorExecution   Privilege = "MONITOR EXECUTION"
+	privilegeExecuteTask        Privilege = "EXECUTE TASK"
+	privilegeExecuteManagedTask Privilege = "EXECUTE MANAGED TASK"
 )
 
 type PrivilegeSet map[Privilege]struct{}
