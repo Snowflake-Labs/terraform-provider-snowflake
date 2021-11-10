66ae04484fd0b47b9b1b80b38f2ca8ae8275cfed
Shane Tzen <stzen@nerdwallet.com>
2021-11-09 18:40:30 -0800

    update storage integration acceptance tests

--- pkg/resources/storage_integration_acceptance_test.go
+++ pkg/resources/storage_integration_acceptance_test.go
@@ -16,7 +16,7 @@ func TestAccStorageIntegration_validation(t *testing.T) {
 		Providers: providers(),
 		Steps: []resource.TestStep{
 			{
-				Config:      storageIntegrationConfig(name, []string{}),
+				Config:      storageIntegrationConfig(name, []string{}, false),
 				ExpectError: regexp.MustCompile("1 item minimum, but config has only 0 declared"),
 			},
 		},
@@ -30,13 +30,28 @@ func TestAccStorageIntegration_aws(t *testing.T) {
 		Providers: providers(),
 		Steps: []resource.TestStep{
 			{
-				Config: storageIntegrationConfig(name, []string{"s3://foo/"}),
+				Config: storageIntegrationConfig(name, []string{"s3://foo/"}, false),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("snowflake_storage_integration.i", "name", name),
+					resource.TestCheckNoResourceAttr("snowflake_storage_integration.i", "storage_aws_object_acl"),
+				),
+			},
+			{
+				Config: storageIntegrationConfig(name, []string{"s3://foo/"}, true),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("snowflake_storage_integration.i", "name", name),
+					resource.TestCheckResourceAttr("snowflake_storage_integration.i", "storage_aws_object_acl", "bucket-owner-full-control"),
+				),
 			},
 		},
 	})
 }
 
-func storageIntegrationConfig(name string, locations []string) string {
+func storageIntegrationConfig(name string, locations []string, aws_object_acl bool) string {
+	aws_object_acl_config := ""
+	if aws_object_acl {
+		aws_object_acl_config = "storage_aws_object_acl = \"bucket-owner-full-control\""
+	}
 	return fmt.Sprintf(`
 resource snowflake_storage_integration i {
 	name = "%s"
@@ -44,6 +59,7 @@ resource snowflake_storage_integration i {
 	storage_provider = "S3"
 
 	storage_aws_role_arn = "arn:aws:iam::000000000001:/role/test"
+	%s
 }
-`, name, locations)
+`, name, locations, aws_object_acl_config)
 }
