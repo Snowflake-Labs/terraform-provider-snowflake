237bc37d87d5947060b2eac0723c4386e0170c71
Antti Rasinen <antti.rasinen@relexsolutions.com>
2021-11-23 08:56:03 +0200

    Add failing test

--- pkg/snowflake/validation_test.go
+++ pkg/snowflake/validation_test.go
@@ -1,3 +1,38 @@
 package snowflake_test
 
-// TODO write tests here
+import (
+	"testing"
+
+	"github.com/chanzuckerberg/terraform-provider-snowflake/pkg/snowflake"
+)
+
+// TODO write a test for a candidate that's not castable to a string
+func TestValidateIdentifier(t *testing.T) {
+	cases := []struct {
+		candidate string
+		valid     bool
+	}{
+		{"word", true},
+		{"_1", true},
+		{"Aword", true},
+		{"azAZ09_$", true},
+		{"invalidcharacter!", false},
+		{"1startwithnumber", false},
+		{"$startwithdollar", false},
+	}
+
+	for _, tc := range cases {
+		_, errs := snowflake.ValidateIdentifier(tc.candidate)
+		actual := len(errs) == 0
+
+		if actual == tc.valid {
+			continue
+		}
+
+		if tc.valid {
+			t.Fatalf("identifier %s should pass validation", tc.candidate)
+		} else {
+			t.Fatalf("identifier %s should fail validation", tc.candidate)
+		}
+	}
+}
