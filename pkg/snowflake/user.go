143020f2f3123dd5a5735fa19c10fe473893b94c
Michael Ehrlich <michael@coho.ai>
2021-11-26 18:58:19 +0200

    fix: use "DESCRIBE USER" in ReadUser, UserExists

--- pkg/snowflake/user.go
+++ pkg/snowflake/user.go
@@ -34,6 +34,53 @@ func ScanUser(row *sqlx.Row) (*user, error) {
 	return r, err
 }
 
+func ScanUserDescription(rows *sqlx.Rows) (*user, error) {
+	r := &user{}
+	var err error
+
+	for rows.Next() {
+		userProp := &DescribeUserProp{}
+		err = rows.StructScan(userProp)
+
+		// The "DESCRIBE USER ..." command returns the string "null" for null values
+		if userProp.Value.String == "null" {
+			userProp.Value.Valid = false
+			userProp.Value.String = ""
+		}
+
+		switch propery := userProp.Property; propery {
+		case "COMMENT":
+			r.Comment = userProp.Value
+		case "DEFAULT_NAMESPACE":
+			r.DefaultNamespace = userProp.Value
+		case "DEFAULT_ROLE":
+			r.DefaultRole = userProp.Value
+		case "DEFAULT_WAREHOUSE":
+			r.DefaultWarehouse = userProp.Value
+		case "DISABLED":
+			r.Disabled = userProp.Value.String == "true"
+		case "DISPLAY_NAME":
+			r.DisplayName = userProp.Value
+		case "EMAIL":
+			r.Email = userProp.Value
+		case "FIRST_NAME":
+			r.FirstName = userProp.Value
+		case "RSA_PUBLIC_KEY_FP":
+			r.HasRsaPublicKey = userProp.Value.Valid
+		case "LAST_NAME":
+			r.LastName = userProp.Value
+		case "LOGIN_NAME":
+			r.LoginName = userProp.Value
+		case "NAME":
+			r.Name = userProp.Value
+		}
+	}
+
+	err = rows.Err()
+
+	return r, err
+}
+
 type DescribeUserProp struct {
 	Property string         `db:"property"`
 	Value    sql.NullString `db:"value"`
