14b17b00d47de1b971bf8967605ae38b348531f8
satoshishirouchi <68636603+satoshishirouchi@users.noreply.github.com>
2021-11-30 06:24:13 +0900

    fix: remove table where is_external is Y (#667)

--- pkg/datasources/tables_acceptance_test.go
+++ pkg/datasources/tables_acceptance_test.go
@@ -13,11 +13,13 @@ func TestAccTables(t *testing.T) {
 	databaseName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
 	schemaName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
 	tableName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	stageName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	externalTableName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
 	resource.ParallelTest(t, resource.TestCase{
 		Providers: providers(),
 		Steps: []resource.TestStep{
 			{
-				Config: tables(databaseName, schemaName, tableName),
+				Config: tables(databaseName, schemaName, tableName, stageName, externalTableName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.snowflake_tables.t", "database", databaseName),
 					resource.TestCheckResourceAttr("data.snowflake_tables.t", "schema", schemaName),
@@ -30,7 +32,7 @@ func TestAccTables(t *testing.T) {
 	})
 }
 
-func tables(databaseName string, schemaName string, tableName string) string {
+func tables(databaseName string, schemaName string, tableName string, stageName string, externalTableName string) string {
 	return fmt.Sprintf(`
 
 	resource snowflake_database "d" {
@@ -52,10 +54,30 @@ func tables(databaseName string, schemaName string, tableName string) string {
 		}
 	}
 
+	resource "snowflake_stage" "s" {
+		name = "%v"
+		url = "s3://snowflake-workshop-lab/weather-nyc"
+		database = snowflake_database.d.name
+		schema = snowflake_schema.s.name
+	}
+
+	resource "snowflake_external_table" "et" {
+		database = snowflake_database.d.name
+		schema   = snowflake_schema.s.name
+		name     = "%v"
+		column {
+			name = "column1"
+			type = "STRING"
+		as = "TO_VARCHAR(TO_TIMESTAMP_NTZ(value:unix_timestamp_property::NUMBER, 3), 'yyyy-mm-dd-hh')"
+		}
+	    file_format = "TYPE = CSV"
+	    location = "@${snowflake_database.d.name}.${snowflake_schema.s.name}.${snowflake_stage.s.name}"
+	}
+
 	data snowflake_tables "t" {
 		database = snowflake_table.t.database
 		schema = snowflake_table.t.schema
-		depends_on = [snowflake_table.t]
+		depends_on = [snowflake_table.t, snowflake_external_table.et]
 	}
-	`, databaseName, schemaName, tableName)
+	`, databaseName, schemaName, tableName, stageName, externalTableName)
 }
