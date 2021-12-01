c2634c876c7db9a5277ecbdc937b162fd997be91
Mike Gouline <1960272+gouline@users.noreply.github.com>
2021-12-01 11:42:42 +1100

    Add oauth_use_secondary_roles default for OAuth integration

--- pkg/resources/oauth_integration.go
+++ pkg/resources/oauth_integration.go
@@ -41,6 +41,7 @@ var oauthIntegrationSchema = map[string]*schema.Schema{
 	"oauth_use_secondary_roles": {
 		Type:        schema.TypeString,
 		Optional:    true,
+		Default:     "NONE",
 		Description: "Specifies whether default secondary roles set in the user properties are activated by default in the session being opened.",
 		ValidateFunc: validation.StringInSlice([]string{
 			"IMPLICIT", "NONE",
