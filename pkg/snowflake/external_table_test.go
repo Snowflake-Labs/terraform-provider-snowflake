7aff6a1e04358790a3890e8534ea4ffbc414024b
Scott Winkler <scott.winkler@snowflake.com>
2022-01-03 12:16:41 -0800

    fix: tagging for db, external_table, schema (#795)

--- pkg/snowflake/external_table_test.go
+++ pkg/snowflake/external_table_test.go
@@ -21,6 +21,13 @@ func TestExternalTableCreate(t *testing.T) {
 	r.Equal(s.Create(), `CREATE EXTERNAL TABLE "test_db"."test_schema"."test_table" ("column1" OBJECT AS expression1, "column2" VARCHAR AS expression2) WITH LOCATION = location REFRESH_ON_CREATE = false AUTO_REFRESH = false PATTERN = 'pattern' FILE_FORMAT = ( file format ) COMMENT = 'Test Comment'`)
 }
 
+func TestExternalTableUpdate(t *testing.T){
+	r := require.New(t)
+	s := ExternalTable("test_table", "test_db", "test_schema")
+	s.WithTags([]TagValue{{Name: "tag1", Value: "value1", Schema: "test_schema", Database: "test_db"}})
+	r.Equal(s.Update(), `ALTER EXTERNAL TABLE "test_db"."test_schema"."test_table" TAG "test_db"."test_schema"."tag1" = "value1"`)
+}
+
 func TestExternalTableDrop(t *testing.T) {
 	r := require.New(t)
 	s := ExternalTable("test_table", "test_db", "test_schema")
