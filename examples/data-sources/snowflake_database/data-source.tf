6f34c158c5dc0b7b1ed24429dceb11302bf95128
Chema <chema.robles@civica-soft.com>
2022-02-17 09:39:27 +0100

    FIX: remove unnecessary resource

--- examples/data-sources/snowflake_database/data-source.tf
+++ examples/data-sources/snowflake_database/data-source.tf
@@ -1,8 +1,3 @@
 data "snowflake_database" "this" {
     name = "DEMO_DB"
 }
-
-resource "snowflake_database" "backup" {
-    name  = "BACKUP_${data.snowflake_database.this.name}"
-    comment = "Backup of ${data.snowflake_database.this.name} - ${data.snowflake_database.this.comment}"
-}
