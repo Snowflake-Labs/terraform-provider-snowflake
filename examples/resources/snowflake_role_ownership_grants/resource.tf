896af15f2a09c37c43f35ec1ad4be525b4225391
aidanmelen <aidan-melen@protonmail.com>
2022-03-12 13:08:17 -0700

    added comment to example

--- examples/resources/snowflake_role_ownership_grants/resource.tf
+++ examples/resources/snowflake_role_ownership_grants/resource.tf
@@ -17,7 +17,8 @@ resource "snowflake_role" "other_role" {
   name = "rking_test_role2"
 }
 
-# ensure Terraform user has privileges
+# ensure the Terraform user inherits ownership privileges for the rking_test_user role
+# otherwise Terraform will fail to destroy the rking_test_role2 role due to insufficient privileges
 resource "snowflake_role_grants" "grants" {
   role_name = "${snowflake_role.role.name}"
 
