c6d698b7ef326a4a20990b6248fd10769cfe8855
aidanmelen <aidan-melen@protonmail.com>
2022-03-12 13:01:17 -0700

    fixed issue where snowflake_role_ownership_grant example would fail because terraform would lose ownership of the role.

--- examples/resources/snowflake_role_ownership_grants/resource.tf
+++ examples/resources/snowflake_role_ownership_grants/resource.tf
@@ -17,6 +17,15 @@ resource "snowflake_role" "other_role" {
   name = "rking_test_role2"
 }
 
+# ensure Terraform user has privileges
+resource "snowflake_role_grants" "grants" {
+  role_name = "${snowflake_role.role.name}"
+
+  roles = [
+    "ACCOUNTADMIN",
+  ]
+}
+
 resource "snowflake_role_ownership_grants" "grants" {
   role_name = "${snowflake_role.role.name}"
 
