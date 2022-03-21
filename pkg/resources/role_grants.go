ae9a755e46eb739f669c0c16a817aaa8a95c7432
ChrisIsidora <christopher.isidora@gmail.com>
2022-03-21 09:13:10 +0100

    Rollback Role Grants Changes

--- pkg/resources/role_grants.go
+++ pkg/resources/role_grants.go
@@ -3,13 +3,11 @@ package resources
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"strings"
 
 	"github.com/chanzuckerberg/terraform-provider-snowflake/pkg/snowflake"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/jmoiron/sqlx"
-	"github.com/pkg/errors"
 )
 
 func RoleGrants() *schema.Resource {
@@ -59,16 +57,6 @@ func CreateRoleGrants(d *schema.ResourceData, meta interface{}) error {
 		return fmt.Errorf("no users or roles specified for role grants")
 	}
 
-	grant := &grantID{
-		ResourceName: roleName,
-		Roles:        roles,
-	}
-	dataIDInput, err := grant.String()
-	d.SetId(dataIDInput)
-
-	if err != nil {
-		return errors.Wrap(err, "error creating role grant")
-	}
 	for _, role := range roles {
 		err := grantRoleToRole(db, roleName, role)
 		if err != nil {
@@ -82,7 +70,7 @@ func CreateRoleGrants(d *schema.ResourceData, meta interface{}) error {
 			return err
 		}
 	}
-
+	d.SetId(roleName)
 	return ReadRoleGrants(d, meta)
 }
 
@@ -108,12 +96,7 @@ type roleGrant struct {
 
 func ReadRoleGrants(d *schema.ResourceData, meta interface{}) error {
 	db := meta.(*sql.DB)
-	log.Println(d.Id())
-	grantID, err := grantIDFromString(d.Id())
-	if err != nil {
-		return err
-	}
-	roleName := grantID.ResourceName
+	roleName := d.Id()
 
 	tfRoles := expandStringList(d.Get("roles").(*schema.Set).List())
 	tfUsers := expandStringList(d.Get("users").(*schema.Set).List())
