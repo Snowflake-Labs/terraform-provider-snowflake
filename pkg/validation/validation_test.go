f3c4d540dbae9ebfb0b5a8fa65f7557e167efe39
Jason Lin <jason.lin@snowflake.com>
2022-07-06 10:02:33 -0700

    Updating shares to disallow account locators

--- pkg/validation/validation_test.go
+++ pkg/validation/validation_test.go
@@ -56,3 +56,27 @@ func TestValidatePrivilege(t *testing.T) {
 	r.Empty(w)
 	r.Empty(errs)
 }
+
+var validAccounts = []string{
+	"testOrg.testAcc",
+	"testingOrg2.testingAcc2",
+	"abc.12345",
+}
+
+var invalidAccounts = []interface{}{
+	"abc12345",
+	"xyz56789",
+}
+
+func TestValidateIsNotAccountLocator(t *testing.T) {
+	r := require.New(t)
+	for _, p := range validAccounts {
+		_, errs := ValidateIsNotAccountLocator(p, "test_valid_accounts")
+		r.Len(errs, 0, "account locators are not allowed - please use 'organization_name.account_name]", p, errs)
+	}
+
+	for _, p := range invalidAccounts {
+		_, errs := ValidateIsNotAccountLocator(p, "test_invalid_accounts")
+		r.NotZero(len(errs), "account locators are not allowed - please use 'organization_name.account_name]", p, errs)
+	}
+}
