3f05a6d10e60da4fc35c4a8762a65067c7f1c1e6
Scott Winkler <scott.winkler@snowflake.com>
2022-09-19 15:51:43 -0700

    delete if not found

--- pkg/resources/stage.go
+++ pkg/resources/stage.go
@@ -11,6 +11,7 @@ import (
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/snowflake"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/pkg/errors"
+	"github.com/snowflakedb/gosnowflake"
 )
 
 const (
@@ -235,14 +236,14 @@ func ReadStage(d *schema.ResourceData, meta interface{}) error {
 
 	q := snowflake.Stage(stage, dbName, schema).Describe()
 	stageDesc, err := snowflake.DescStage(db, q)
-	if err == sql.ErrNoRows {
-		// If not found, mark resource to be removed from statefile during apply or refresh
-		log.Printf("[DEBUG] stage (%s) not found", d.Id())
-		d.SetId("")
-		return nil
-	}
-	if err != nil {
-		return err
+	if driverErr, ok := err.(*gosnowflake.SnowflakeError); ok {
+		// 002003 (02000): SQL compilation error:
+		// 'XXX' does not exist or not authorized.
+		if driverErr.Number == 2003 {
+			log.Printf("[DEBUG] stage (%s) not found", d.Id())
+			d.SetId("")
+			return nil
+		}
 	}
 
 	sq := snowflake.Stage(stage, dbName, schema).Show()
