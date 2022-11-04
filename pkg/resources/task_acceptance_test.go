9f3054b8f1c6084a86b59511d8c22f403b5ddcbc
Scott Winkler <scott.winkler@snowflake.com>
2022-11-04 15:48:58 -0700

    update dag task

--- pkg/resources/task_acceptance_test.go
+++ pkg/resources/task_acceptance_test.go
@@ -181,7 +181,7 @@ func TestAcc_Task(t *testing.T) {
 				Config: taskConfig(initialState),
 				Check: resource.ComposeTestCheckFunc(
 					checkBool("snowflake_task.root_task", "enabled", true),
-					checkBool("snowflake_task.child_task", "enabled", true),
+					checkBool("snowflake_task.child_task", "enabled", false),
 					resource.TestCheckResourceAttr("snowflake_task.root_task", "name", rootname),
 					resource.TestCheckResourceAttr("snowflake_task.child_task", "name", childname),
 					resource.TestCheckResourceAttr("snowflake_task.root_task", "database", databasename),
