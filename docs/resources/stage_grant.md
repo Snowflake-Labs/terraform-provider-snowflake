1cf40d773e71249f026fc60f84ab0d68815019a5
Benny Lu <blu@sofi.org>
2023-01-10 14:45:41 -0800

    refactor

--- docs/resources/stage_grant.md
+++ docs/resources/stage_grant.md
@@ -33,13 +33,13 @@ resource "snowflake_stage_grant" "grant" {
 ### Required
 
 - `database_name` (String) The name of the database containing the current stage on which to grant privileges.
+- `roles` (Set of String) Grants privilege to these roles.
 
 ### Optional
 
 - `enable_multiple_grants` (Boolean) When this is set to true, multiple grants of the same type can be created. This will cause Terraform to not revoke grants applied to roles and objects outside Terraform.
 - `on_future` (Boolean) When this is set to true and a schema_name is provided, apply this grant on all future stages in the given schema. When this is true and no schema_name is provided apply this grant on all future stages in the given database. The stage_name field must be unset in order to use on_future.
 - `privilege` (String) The privilege to grant on the stage.
-- `roles` (Set of String) Grants privilege to these roles.
 - `schema_name` (String) The name of the schema containing the current stage on which to grant privileges.
 - `stage_name` (String) The name of the stage on which to grant privilege (only valid if on_future is false).
 - `with_grant_option` (Boolean) When this is set to true, allows the recipient role to grant the privileges to other roles.
--- pkg/resources/stage_grant.go
+++ pkg/resources/stage_grant.go
@@ -17,24 +17,26 @@ var validStagePrivileges = NewPrivilegeSet(
 )
 
 var stageGrantSchema = map[string]*schema.Schema{
-	"stage_name": {
-		Type:          schema.TypeString,
-		Optional:      true,
-		Description:   "The name of the stage on which to grant privilege (only valid if on_future is false).",
-		ForceNew:      true,
-		ConflictsWith: []string{"on_future"},
-	},
-	"schema_name": {
-		Type:        schema.TypeString,
-		Optional:    true,
-		Description: "The name of the schema containing the current stage on which to grant privileges.",
-		ForceNew:    true,
-	},
 	"database_name": {
 		Type:        schema.TypeString,
 		Required:    true,
 		Description: "The name of the database containing the current stage on which to grant privileges.",
 		ForceNew:    true,
+	},
+	"enable_multiple_grants": {
+		Type:        schema.TypeBool,
+		Optional:    true,
+		Description: "When this is set to true, multiple grants of the same type can be created. This will cause Terraform to not revoke grants applied to roles and objects outside Terraform.",
+		Default:     false,
+		ForceNew:    true,
+	},
+	"on_future": {
+		Type:          schema.TypeBool,
+		Optional:      true,
+		Description:   "When this is set to true and a schema_name is provided, apply this grant on all future stages in the given schema. When this is true and no schema_name is provided apply this grant on all future stages in the given database. The stage_name field must be unset in order to use on_future.",
+		Default:       false,
+		ForceNew:      true,
+		ConflictsWith: []string{"stage_name"},
 	},
 	"privilege": {
 		Type:         schema.TypeString,
@@ -46,29 +48,27 @@ var stageGrantSchema = map[string]*schema.Schema{
 	},
 	"roles": {
 		Type:        schema.TypeSet,
+		Required:    true,
 		Elem:        &schema.Schema{Type: schema.TypeString},
-		Optional:    true,
 		Description: "Grants privilege to these roles.",
 	},
-	"on_future": {
-		Type:          schema.TypeBool,
+	"schema_name": {
+		Type:        schema.TypeString,
+		Optional:    true,
+		Description: "The name of the schema containing the current stage on which to grant privileges.",
+		ForceNew:    true,
+	},
+	"stage_name": {
+		Type:          schema.TypeString,
 		Optional:      true,
-		Description:   "When this is set to true and a schema_name is provided, apply this grant on all future stages in the given schema. When this is true and no schema_name is provided apply this grant on all future stages in the given database. The stage_name field must be unset in order to use on_future.",
-		Default:       false,
+		Description:   "The name of the stage on which to grant privilege (only valid if on_future is false).",
 		ForceNew:      true,
-		ConflictsWith: []string{"stage_name"},
+		ConflictsWith: []string{"on_future"},
 	},
 	"with_grant_option": {
 		Type:        schema.TypeBool,
 		Optional:    true,
 		Description: "When this is set to true, allows the recipient role to grant the privileges to other roles.",
-		Default:     false,
-		ForceNew:    true,
-	},
-	"enable_multiple_grants": {
-		Type:        schema.TypeBool,
-		Optional:    true,
-		Description: "When this is set to true, multiple grants of the same type can be created. This will cause Terraform to not revoke grants applied to roles and objects outside Terraform.",
 		Default:     false,
 		ForceNew:    true,
 	},
@@ -99,17 +99,17 @@ func CreateStageGrant(d *schema.ResourceData, meta interface{}) error {
 		stageName  string
 	)
 
-	if name, ok := d.GetOk("stage_name"); ok {
-		stageName = name.(string)
-	}
+	dbName := d.Get("database_name").(string)
+	onFuture := d.Get("on_future").(bool)
+	priv := d.Get("privilege").(string)
+	grantOption := d.Get("with_grant_option").(bool)
+
 	if name, ok := d.GetOk("schema_name"); ok {
 		schemaName = name.(string)
 	}
-
-	dbName := d.Get("database_name").(string)
-	priv := d.Get("privilege").(string)
-	onFuture := d.Get("on_future").(bool)
-	grantOption := d.Get("with_grant_option").(bool)
+	if name, ok := d.GetOk("stage_name"); ok {
+		stageName = name.(string)
+	}
 
 	if (schemaName == "") && !onFuture {
 		return errors.New("schema_name must be set unless on_future is true")
@@ -151,27 +151,27 @@ func ReadStageGrant(d *schema.ResourceData, meta interface{}) error {
 	if err != nil {
 		return err
 	}
+
 	dbName := grantID.ResourceName
 	schemaName := grantID.SchemaName
 	stageName := grantID.ObjectName
 	priv := grantID.Privilege
 
-	if err := d.Set("database_name", dbName); err != nil {
-		return err
-	}
-	if err := d.Set("schema_name", schemaName); err != nil {
-		return err
-	}
-
 	onFuture := (stageName == "")
 
-	if err := d.Set("stage_name", stageName); err != nil {
+	if err := d.Set("database_name", dbName); err != nil {
 		return err
 	}
 	if err := d.Set("on_future", onFuture); err != nil {
 		return err
 	}
 	if err := d.Set("privilege", priv); err != nil {
+		return err
+	}
+	if err := d.Set("schema_name", schemaName); err != nil {
+		return err
+	}
+	if err := d.Set("stage_name", stageName); err != nil {
 		return err
 	}
 	if err := d.Set("with_grant_option", grantID.GrantOption); err != nil {
@@ -188,28 +188,6 @@ func ReadStageGrant(d *schema.ResourceData, meta interface{}) error {
 	return readGenericGrant(d, meta, stageGrantSchema, builder, onFuture, validStagePrivileges)
 }
 
-// DeleteStageGrant implements schema.DeleteFunc.
-func DeleteStageGrant(d *schema.ResourceData, meta interface{}) error {
-	grantID, err := grantIDFromString(d.Id())
-	if err != nil {
-		return err
-	}
-	dbName := grantID.ResourceName
-	schemaName := grantID.SchemaName
-	stageName := grantID.ObjectName
-
-	onFuture := (stageName == "")
-
-	var builder snowflake.GrantBuilder
-	if onFuture {
-		builder = snowflake.FutureStageGrant(dbName, schemaName)
-	} else {
-		builder = snowflake.StageGrant(dbName, schemaName, stageName)
-	}
-
-	return deleteGenericGrant(d, meta, builder)
-}
-
 // UpdateStageGrant implements schema.UpdateFunc.
 func UpdateStageGrant(d *schema.ResourceData, meta interface{}) error {
 	// for now the only thing we can update are roles or shares
@@ -218,13 +196,6 @@ func UpdateStageGrant(d *schema.ResourceData, meta interface{}) error {
 		return nil
 	}
 
-	rolesToAdd := []string{}
-	rolesToRevoke := []string{}
-
-	if d.HasChange("roles") {
-		rolesToAdd, rolesToRevoke = changeDiff(d, "roles")
-	}
-
 	grantID, err := grantIDFromString(d.Id())
 	if err != nil {
 		return err
@@ -233,7 +204,15 @@ func UpdateStageGrant(d *schema.ResourceData, meta interface{}) error {
 	dbName := grantID.ResourceName
 	schemaName := grantID.SchemaName
 	stageName := grantID.ObjectName
+
 	onFuture := (stageName == "")
+
+	rolesToAdd := []string{}
+	rolesToRevoke := []string{}
+
+	if d.HasChange("roles") {
+		rolesToAdd, rolesToRevoke = changeDiff(d, "roles")
+	}
 
 	var builder snowflake.GrantBuilder
 	if onFuture {
@@ -257,4 +236,27 @@ func UpdateStageGrant(d *schema.ResourceData, meta interface{}) error {
 
 	// Done, refresh state
 	return ReadStageGrant(d, meta)
+}
+
+// DeleteStageGrant implements schema.DeleteFunc.
+func DeleteStageGrant(d *schema.ResourceData, meta interface{}) error {
+	grantID, err := grantIDFromString(d.Id())
+	if err != nil {
+		return err
+	}
+
+	dbName := grantID.ResourceName
+	schemaName := grantID.SchemaName
+	stageName := grantID.ObjectName
+
+	onFuture := (stageName == "")
+
+	var builder snowflake.GrantBuilder
+	if onFuture {
+		builder = snowflake.FutureStageGrant(dbName, schemaName)
+	} else {
+		builder = snowflake.StageGrant(dbName, schemaName, stageName)
+	}
+
+	return deleteGenericGrant(d, meta, builder)
 }
