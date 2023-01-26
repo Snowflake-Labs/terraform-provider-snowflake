9f37b997de073f01b66c86820237eff8049346ba
nagahama <s.c.x0x0www10@icloud.com>
2023-01-27 03:25:31 +0900

    fix: refactor handling error to be simple (#1473)

--- pkg/resources/warehouse.go
+++ pkg/resources/warehouse.go
@@ -192,49 +192,37 @@ func ReadWarehouse(d *schema.ResourceData, meta interface{}) error {
 	if err != nil {
 		return err
 	}
-
-	err = d.Set("name", w.Name)
-	if err != nil {
+	if err = d.Set("name", w.Name); err != nil {
 		return err
 	}
-	err = d.Set("comment", w.Comment)
-	if err != nil {
+	if err = d.Set("comment", w.Comment); err != nil {
 		return err
 	}
-	err = d.Set("warehouse_size", w.Size)
-	if err != nil {
+	if err = d.Set("warehouse_size", w.Size); err != nil {
 		return err
 	}
-	err = d.Set("max_cluster_count", w.MaxClusterCount)
-	if err != nil {
+	if err = d.Set("max_cluster_count", w.MaxClusterCount); err != nil {
 		return err
 	}
-	err = d.Set("min_cluster_count", w.MinClusterCount)
-	if err != nil {
+	if err = d.Set("min_cluster_count", w.MinClusterCount); err != nil {
 		return err
 	}
-	err = d.Set("scaling_policy", w.ScalingPolicy)
-	if err != nil {
+	if err = d.Set("scaling_policy", w.ScalingPolicy); err != nil {
 		return err
 	}
-	err = d.Set("auto_suspend", w.AutoSuspend.Int64)
-	if err != nil {
+	if err = d.Set("auto_suspend", w.AutoSuspend.Int64); err != nil {
 		return err
 	}
-	err = d.Set("auto_resume", w.AutoResume)
-	if err != nil {
+	if err = d.Set("auto_resume", w.AutoResume); err != nil {
 		return err
 	}
-	err = d.Set("resource_monitor", w.ResourceMonitor)
-	if err != nil {
+	if err = d.Set("resource_monitor", w.ResourceMonitor); err != nil {
 		return err
 	}
-	err = d.Set("enable_query_acceleration", w.EnableQueryAcceleration)
-	if err != nil {
+	if err = d.Set("enable_query_acceleration", w.EnableQueryAcceleration); err != nil {
 		return err
 	}
-	err = d.Set("query_acceleration_max_scale_factor", w.QueryAccelerationMaxScaleFactor)
-	if err != nil {
+	if err = d.Set("query_acceleration_max_scale_factor", w.QueryAccelerationMaxScaleFactor); err != nil {
 		return err
 	}
 	log.Printf("[DEBUG] warehouse type is: %s", w.WarehouseType)
