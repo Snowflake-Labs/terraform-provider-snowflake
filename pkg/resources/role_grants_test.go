2652203a84d1b66461828c7fcf7c034a1af68f44
Scott Winkler <scott.winkler@snowflake.com>
2023-02-16 16:01:08 -0800

    revert account grant ID

--- pkg/resources/role_grants_test.go
+++ pkg/resources/role_grants_test.go
@@ -67,7 +67,7 @@ func TestRoleGrantsRead(t *testing.T) {
 		err := resources.ReadRoleGrants(d, db)
 		r.NoError(err)
 		r.Len(d.Get("users").(*schema.Set).List(), 0)
-		r.Len(d.Get("roles").(*schema.Set).List(), 0)
+		r.Len(d.Get("roles").(*schema.Set).List(), 2)
 	})
 }
 
@@ -121,7 +121,7 @@ func TestIgnoreUnknownRoleGrants(t *testing.T) {
 		expectReadUnhandledRoleGrants(mock)
 		err := resources.ReadRoleGrants(d, db)
 		r.NoError(err)
-		r.Len(d.Get("users").(*schema.Set).List(), 2)
+		r.Len(d.Get("users").(*schema.Set).List(), 0)
 		r.Len(d.Get("roles").(*schema.Set).List(), 2)
 	})
 }
