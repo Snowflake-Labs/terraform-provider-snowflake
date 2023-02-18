71cfb132ee711a39cdc32e8a4fb2aaf77b0f408f
Scott Winkler <scott.winkler@snowflake.com>
2023-02-17 16:31:40 -0800

    fix id read for no arguments

--- pkg/resources/procedure_grant.go
+++ pkg/resources/procedure_grant.go
@@ -355,11 +355,15 @@ func parseProcedureGrantID(s string) (*ProcedureGrantID, error) {
 		}
 		objectNameParts := strings.Split(objectIdentifier, "(")
 
+		argumentDataTypes := []string{}
+		if len(objectNameParts) > 1 {
+			argumentDataTypes = helpers.SplitStringToSlice(objectNameParts[1], ",")
+		}
 		return &ProcedureGrantID{
 			DatabaseName:      idParts[0],
 			SchemaName:        idParts[1],
 			ObjectName:        objectNameParts[0],
-			ArgumentDataTypes: helpers.SplitStringToSlice(objectNameParts[1], ","),
+			ArgumentDataTypes: argumentDataTypes,
 			Privilege:         idParts[3],
 			Roles:             helpers.SplitStringToSlice(idParts[4], ","),
 			Shares:            []string{},
