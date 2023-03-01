427be2071085d6eec39bbb6672e191ba841cc0ea
Scott Winkler <scott.winkler@snowflake.com>
2023-02-28 16:20:28 -0800

    update roles

--- pkg/resources/role_acceptance_test.go
+++ pkg/resources/role_acceptance_test.go
@@ -13,9 +13,7 @@ func TestAcc_Role(t *testing.T) {
 	name := "tst-terraform" + strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
 	name2 := "5tst-terraform" + strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
 
-	resource.Test(t, resource.TestCase{
-		
-	})
+	resource.Test(t, resource.TestCase{})
 	resource.ParallelTest(t, resource.TestCase{
 		Providers:    providers(),
 		CheckDestroy: nil,
@@ -26,7 +24,6 @@ func TestAcc_Role(t *testing.T) {
 					resource.TestCheckResourceAttr("snowflake_role.role", "name", name),
 					resource.TestCheckResourceAttr("snowflake_role.role", "comment", "test comment"),
 				),
-				
 			},
 			// IMPORT
 			{
