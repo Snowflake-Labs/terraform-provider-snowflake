56909cdc18245f38b0f58bceaf2aa9cbc013d212
Philippe Boyd <philippeboyd@users.noreply.github.com>
2023-03-02 03:28:44 -0500

    feat(integration): add google api integration (#1589)

--- pkg/resources/api_integration_acceptance_test.go
+++ pkg/resources/api_integration_acceptance_test.go
@@ -15,18 +15,20 @@ func TestAcc_ApiIntegration(t *testing.T) {
 		t.Skip("Skipping TestAccApiIntegration")
 	}
 
-	apiIntName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
-	apiIntName2 := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	apiIntNameAWS := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	apiIntNameAzure := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	apiIntNameGCP := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
 
 	resource.Test(t, resource.TestCase{
 		Providers:    providers(),
 		CheckDestroy: nil,
 		Steps: []resource.TestStep{
 			{
-				Config: apiIntegrationConfigAWS(apiIntName, []string{"https://123456.execute-api.us-west-2.amazonaws.com/prod/"}),
+				Config: apiIntegrationConfigAWS(apiIntNameAWS, []string{"https://123456.execute-api.us-west-2.amazonaws.com/prod/"}),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("snowflake_api_integration.test_aws_int", "name", apiIntName),
+					resource.TestCheckResourceAttr("snowflake_api_integration.test_aws_int", "name", apiIntNameAWS),
 					resource.TestCheckResourceAttr("snowflake_api_integration.test_aws_int", "api_provider", "aws_api_gateway"),
+					resource.TestCheckResourceAttr("snowflake_api_integration.test_aws_int", "comment", "acceptance test"),
 					resource.TestCheckResourceAttrSet("snowflake_api_integration.test_aws_int", "created_on"),
 					resource.TestCheckResourceAttrSet("snowflake_api_integration.test_aws_int", "api_aws_iam_user_arn"),
 					resource.TestCheckResourceAttrSet("snowflake_api_integration.test_aws_int", "api_aws_external_id"),
@@ -34,14 +36,25 @@ func TestAcc_ApiIntegration(t *testing.T) {
 				),
 			},
 			{
-				Config: apiIntegrationConfigAzure(apiIntName2, []string{"https://apim-hello-world.azure-api.net/"}),
+				Config: apiIntegrationConfigAzure(apiIntNameAzure, []string{"https://apim-hello-world.azure-api.net/"}),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("snowflake_api_integration.test_azure_int", "name", apiIntName2),
+					resource.TestCheckResourceAttr("snowflake_api_integration.test_azure_int", "name", apiIntNameAzure),
 					resource.TestCheckResourceAttr("snowflake_api_integration.test_azure_int", "api_provider", "azure_api_management"),
+					resource.TestCheckResourceAttr("snowflake_api_integration.test_azure_int", "comment", "acceptance test"),
 					resource.TestCheckResourceAttrSet("snowflake_api_integration.test_azure_int", "created_on"),
 					resource.TestCheckResourceAttrSet("snowflake_api_integration.test_azure_int", "azure_multi_tenant_app_name"),
 					resource.TestCheckResourceAttrSet("snowflake_api_integration.test_azure_int", "azure_consent_url"),
 					resource.TestCheckResourceAttrSet("snowflake_api_integration.test_azure_int", "api_key"),
+				),
+			},
+			{
+				Config: apiIntegrationConfigGCP(apiIntNameGCP, []string{"https://gateway-id-123456.uc.gateway.dev/"}),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("snowflake_api_integration.test_gcp_int", "name", apiIntNameGCP),
+					resource.TestCheckResourceAttr("snowflake_api_integration.test_gcp_int", "api_provider", "google_api_gateway"),
+					resource.TestCheckResourceAttr("snowflake_api_integration.test_gcp_int", "comment", "acceptance test"),
+					resource.TestCheckResourceAttrSet("snowflake_api_integration.test_gcp_int", "created_on"),
+					resource.TestCheckResourceAttrSet("snowflake_api_integration.test_gcp_int", "google_audience"),
 				),
 			},
 		},
@@ -56,6 +69,7 @@ func apiIntegrationConfigAWS(name string, prefixes []string) string {
 		api_aws_role_arn = "arn:aws:iam::000000000001:/role/test"
 		api_allowed_prefixes = %q
 		api_key = "12345"
+		comment = "acceptance test"
 		enabled = true
 	}
 	`, name, prefixes)
@@ -70,6 +84,20 @@ func apiIntegrationConfigAzure(name string, prefixes []string) string {
 		azure_ad_application_id = "7890"
 		api_allowed_prefixes = %q
 		api_key = "12345"
+		comment = "acceptance test"
+		enabled = true
+	}
+	`, name, prefixes)
+}
+
+func apiIntegrationConfigGCP(name string, prefixes []string) string {
+	return fmt.Sprintf(`
+	resource "snowflake_api_integration" "test_gcp_int" {
+		name = "%s"
+		api_provider = "google_api_gateway"
+		google_audience = "api-gateway-id-123456.apigateway.gcp-project.cloud.goog"
+		api_allowed_prefixes = %q
+		comment = "acceptance test"
 		enabled = true
 	}
 	`, name, prefixes)
