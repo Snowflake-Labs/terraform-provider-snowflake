273cc31d713fbfa4dbab90295ddb5730ddcb9799
Arkadius Schuchhardt <schuchhardt@auxmoney.com>
2023-04-21 23:17:48 +0200

    feat: added on_all to task_grant

--- pkg/resources/task_grant_acceptance_test.go
+++ pkg/resources/task_grant_acceptance_test.go
@@ -10,18 +10,18 @@ import (
 )
 
 func TestAcc_TaskGrant(t *testing.T) {
-	accName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+	name := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
 
 	resource.Test(t, resource.TestCase{
 		Providers:    providers(),
 		CheckDestroy: nil,
 		Steps: []resource.TestStep{
 			{
-				Config: taskGrantConfig(accName, 8),
+				Config: taskGrantConfig(name, 8, normal),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("snowflake_task_grant.test", "database_name", accName),
-					resource.TestCheckResourceAttr("snowflake_task_grant.test", "schema_name", accName),
-					resource.TestCheckResourceAttr("snowflake_task_grant.test", "task_name", accName),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "database_name", name),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "schema_name", name),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "task_name", name),
 					resource.TestCheckResourceAttr("snowflake_task_grant.test", "with_grant_option", "false"),
 					resource.TestCheckResourceAttr("snowflake_task_grant.test", "privilege", "OPERATE"),
 					resource.TestCheckResourceAttr("snowflake_warehouse.test", "max_concurrency_level", "8"),
@@ -30,11 +30,11 @@ func TestAcc_TaskGrant(t *testing.T) {
 			},
 			// UPDATE MAX_CONCURRENCY_LEVEL
 			{
-				Config: taskGrantConfig(accName, 10),
+				Config: taskGrantConfig(name, 10, normal),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("snowflake_task_grant.test", "database_name", accName),
-					resource.TestCheckResourceAttr("snowflake_task_grant.test", "schema_name", accName),
-					resource.TestCheckResourceAttr("snowflake_task_grant.test", "task_name", accName),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "database_name", name),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "schema_name", name),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "task_name", name),
 					resource.TestCheckResourceAttr("snowflake_task_grant.test", "with_grant_option", "false"),
 					resource.TestCheckResourceAttr("snowflake_task_grant.test", "privilege", "OPERATE"),
 					resource.TestCheckResourceAttr("snowflake_warehouse.test", "max_concurrency_level", "10"),
@@ -54,7 +54,83 @@ func TestAcc_TaskGrant(t *testing.T) {
 	})
 }
 
-func taskGrantConfig(name string, concurrency int32) string {
+func TestAcc_TaskGrant_onAll(t *testing.T) {
+	name := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+
+	resource.Test(t, resource.TestCase{
+		Providers:    providers(),
+		CheckDestroy: nil,
+		Steps: []resource.TestStep{
+			{
+				Config: taskGrantConfig(name, 8, onAll),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "database_name", name),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "schema_name", name),
+					resource.TestCheckNoResourceAttr("snowflake_task_grant.test", "task_name"),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "on_all", "true"),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "with_grant_option", "false"),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "privilege", "OPERATE"),
+					resource.TestCheckResourceAttr("snowflake_warehouse.test", "max_concurrency_level", "8"),
+					resource.TestCheckResourceAttr("snowflake_warehouse.test", "statement_timeout_in_seconds", "86400"),
+				),
+			},
+			// IMPORT
+			{
+				ResourceName:      "snowflake_task_grant.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"enable_multiple_grants", // feature flag attribute not defined in Snowflake, can't be imported
+				},
+			},
+		},
+	})
+}
+
+func TestAcc_TaskGrant_onFuture(t *testing.T) {
+	name := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
+
+	resource.Test(t, resource.TestCase{
+		Providers:    providers(),
+		CheckDestroy: nil,
+		Steps: []resource.TestStep{
+			{
+				Config: taskGrantConfig(name, 8, onFuture),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "database_name", name),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "schema_name", name),
+					resource.TestCheckNoResourceAttr("snowflake_task_grant.test", "task_name"),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "on_future", "true"),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "with_grant_option", "false"),
+					resource.TestCheckResourceAttr("snowflake_task_grant.test", "privilege", "OPERATE"),
+					resource.TestCheckResourceAttr("snowflake_warehouse.test", "max_concurrency_level", "8"),
+					resource.TestCheckResourceAttr("snowflake_warehouse.test", "statement_timeout_in_seconds", "86400"),
+				),
+			},
+			// IMPORT
+			{
+				ResourceName:      "snowflake_task_grant.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"enable_multiple_grants", // feature flag attribute not defined in Snowflake, can't be imported
+				},
+			},
+		},
+	})
+}
+
+func taskGrantConfig(name string, concurrency int32, grantType grantType) string {
+	var taskNameConfig string
+	switch grantType {
+	case normal:
+		taskNameConfig = "task_name \t= snowflake_task.test.name"
+	case onFuture:
+		taskNameConfig = "on_future = true"
+	case onAll:
+		taskNameConfig = "on_all = true"
+	}
+
 	s := `
 resource "snowflake_database" "test" {
   name = "%v"
@@ -75,6 +151,7 @@ resource "snowflake_warehouse" "test" {
   name                         = snowflake_database.test.name
   max_concurrency_level        = %d
   statement_timeout_in_seconds = 86400
+  query_acceleration_max_scale_factor = 0
 }
 
 resource "snowflake_task" "test" {
@@ -91,12 +168,12 @@ resource "snowflake_task" "test" {
 }
 
 resource "snowflake_task_grant" "test" {
-  task_name 	= snowflake_task.test.name
+  %s
   database_name = snowflake_database.test.name
   roles         = [snowflake_role.test.name]
   schema_name   = snowflake_schema.test.name
   privilege 	= "OPERATE"
 }
 `
-	return fmt.Sprintf(s, name, name, concurrency)
+	return fmt.Sprintf(s, name, name, concurrency, taskNameConfig)
 }
