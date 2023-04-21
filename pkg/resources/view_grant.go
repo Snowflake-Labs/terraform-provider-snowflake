657374254a1415c4df4af58c46fcecdf29f0c2b9
Arkadius Schuchhardt <schuchhardt@auxmoney.com>
2023-04-21 22:05:55 +0200

    docs: fix

--- pkg/resources/view_grant.go
+++ pkg/resources/view_grant.go
@@ -67,7 +67,7 @@ var viewGrantSchema = map[string]*schema.Schema{
 	"on_all": {
 		Type:          schema.TypeBool,
 		Optional:      true,
-		Description:   "When this is set to true and a schema_name is provided, apply this grant on all views in the given schema. When this is true and no schema_name is provided apply this grant on all views in the given database. The view_name and shares fields must be unset in order to use on_all. Cannot be used together with on_future. Importing the resource with the on_all=true option is not supported.",
+		Description:   "When this is set to true and a schema_name is provided, apply this grant on all views in the given schema. When this is true and no schema_name is provided apply this grant on all views in the given database. The view_name and shares fields must be unset in order to use on_all. Cannot be used together with on_future.",
 		Default:       false,
 		ForceNew:      true,
 		ConflictsWith: []string{"view_name", "shares"},
