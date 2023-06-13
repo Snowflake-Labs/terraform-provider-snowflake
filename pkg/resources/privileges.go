e4ac8c8d5b9727d67bf29b10b81ca93ae064defe
Johnny Morano <insaniac@moretrix.com>
2023-06-13 08:04:24 +0200

    fix: added privilege constant for `RESOLVE ALL`

--- pkg/resources/privileges.go
+++ pkg/resources/privileges.go
@@ -72,6 +72,7 @@ const (
 	privilegeReferences                  Privilege = "REFERENCES"
 	privilegeReferenceUsage              Privilege = "REFERENCE_USAGE"
 	privilegeReplicate                   Privilege = "REPLICATE"
+	privilegeResolveAll                  Privilege = "RESOLVE ALL"
 	privilegeSelect                      Privilege = "SELECT"
 	privilegeTruncate                    Privilege = "TRUNCATE"
 	privilegeUpdate                      Privilege = "UPDATE"
