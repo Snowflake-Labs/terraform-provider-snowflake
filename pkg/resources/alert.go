74f7f6be04ca232a6c0c599737d6385e90d60a25
Piotr Bosak <piotr.bosak@snowflake.com>
2023-06-20 09:22:39 +0200

    golang ci

--- pkg/resources/alert.go
+++ pkg/resources/alert.go
@@ -304,7 +304,6 @@ func UpdateAlert(d *schema.ResourceData, meta interface{}) error {
 		if err != nil {
 			return fmt.Errorf("error updating alert %v: %w", objectIdentifier.Name(), err)
 		}
-
 	}
 
 	if runUnsetStatement {
