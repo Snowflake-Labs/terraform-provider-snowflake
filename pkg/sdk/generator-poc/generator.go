75e5d0591a8ab758246de12209655048f207961b
Artur Sawicki <artur.sawicki@snowflake.com>
2023-07-24 12:05:25 +0200

    [POC] Set up generator

--- pkg/sdk/generator-poc/generator.go
+++ pkg/sdk/generator-poc/generator.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 var (
@@ -15,11 +18,15 @@ var (
 
 func main() {
 	fmt.Printf("Running generator on %s with args %#v\n", os.Getenv("GOFILE"), os.Args[1:])
+
 	blueprint := loadBlueprint()
 	fmt.Printf("Loaded blueprint %#v.\n", blueprint)
+
+	gen := setUpGenerator(blueprint)
+	fmt.Printf("Generator set up for package \"%s\" with output name \"%s\".\n", gen.outputPackage, gen.outputName)
 }
 
-func loadBlueprint() Blueprint {
+func loadBlueprint() *Blueprint {
 	flag.Parse()
 
 	if len(*blueprintPath) == 0 {
@@ -35,7 +42,7 @@ func loadBlueprint() Blueprint {
 	fmt.Printf("Opened blueprint file %s\n", blueprintFile.Name())
 
 	byteValue, errReadBytes := io.ReadAll(blueprintFile)
-	if errOpenFile != nil {
+	if errReadBytes != nil {
 		log.Panicln(errReadBytes)
 	}
 
@@ -43,9 +50,35 @@ func loadBlueprint() Blueprint {
 	if err := json.Unmarshal(byteValue, &blueprint); err != nil {
 		log.Panicln(err)
 	}
-	return blueprint
+	return &blueprint
 }
 
 type Blueprint struct {
 	Name string
+}
+
+func setUpGenerator(blueprint *Blueprint) *Generator {
+	wd, errWd := os.Getwd()
+	if errWd != nil {
+		log.Panicln(errWd)
+	}
+
+	file := os.Getenv("GOFILE")
+	fileWithoutSuffix, _ := strings.CutSuffix(file, ".go")
+	baseName := fmt.Sprintf("%s_generated.go", fileWithoutSuffix)
+	outputName := filepath.Join(wd, baseName)
+
+	return &Generator{
+		buffer:        bytes.Buffer{},
+		blueprint:     blueprint,
+		outputPackage: os.Getenv("GOPACKAGE"),
+		outputName:    outputName,
+	}
+}
+
+type Generator struct {
+	buffer        bytes.Buffer
+	blueprint     *Blueprint
+	outputPackage string
+	outputName    string
 }
