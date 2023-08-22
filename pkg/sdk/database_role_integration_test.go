be3dd85f263de0e2f4301eef9bd846ed7e302f53
Artur Sawicki <artur.sawicki@snowflake.com>
2023-08-22 10:16:54 +0200

    Fix imports to pass linters
    This is the revert of the intentional format change earlier.

--- pkg/sdk/database_role_integration_test.go
+++ pkg/sdk/database_role_integration_test.go
@@ -2,9 +2,10 @@ package sdk
 
 import (
 	"context"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func TestInt_DatabaseRoles(t *testing.T) {
