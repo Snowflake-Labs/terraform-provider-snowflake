680051ec481643ed6bacd3d0954850f7a84a8572
Artur Sawicki <artur.sawicki@snowflake.com>
2023-10-04 17:31:17 +0200

    Prepare common resources for integration tests

--- pkg/sdk/tasks_gen_integration_test.go
+++ pkg/sdk/tasks_gen_integration_test.go
@@ -1,9 +1,58 @@
 package sdk
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestInt_Tasks(t *testing.T) {
-	// TODO: prepare common resources
+	client := testClient(t)
+	ctx := context.Background()
+
+	database, databaseCleanup := createDatabase(t, client)
+	t.Cleanup(databaseCleanup)
+
+	schema, schemaCleanup := createSchema(t, client, database)
+	t.Cleanup(schemaCleanup)
+
+	sql := "SELECT CURRENT_TIMESTAMP"
+
+	assertTask := func(t *testing.T, task *Task, id SchemaObjectIdentifier) {
+		t.Helper()
+		assert.NotEmpty(t, task.CreatedOn)
+		// TODO: fill out
+	}
+
+	assertTaskTerse := func(t *testing.T, task *Task, id SchemaObjectIdentifier) {
+		t.Helper()
+		assert.NotEmpty(t, task.CreatedOn)
+		// TODO: fill out
+	}
+
+	cleanupTaskProvider := func(id SchemaObjectIdentifier) func() {
+		return func() {
+			err := client.Tasks.Drop(ctx, NewDropTaskRequest(id))
+			require.NoError(t, err)
+		}
+	}
+
+	createTask := func(t *testing.T) *Task {
+		t.Helper()
+		name := randomString(t)
+		id := NewSchemaObjectIdentifier(database.Name, schema.Name, name)
+
+		err := client.Tasks.Create(ctx, NewCreateTaskRequest(id, sql))
+		require.NoError(t, err)
+		t.Cleanup(cleanupTaskProvider(id))
+
+		task, err := client.Tasks.ShowByID(ctx, id)
+		require.NoError(t, err)
+
+		return task
+	}
 
 	t.Run("Create", func(t *testing.T) {
 		// TODO: fill me
