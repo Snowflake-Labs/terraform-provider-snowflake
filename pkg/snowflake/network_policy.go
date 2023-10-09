c23bb9d403153c998e61e70491d528b1c545e215
Jan Cieślak <jan.cieslak@snowflake.com>
2023-10-09 09:44:28 +0200

    Changes after review

--- pkg/snowflake/network_policy.go
+++ pkg/snowflake/network_policy.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/helpers"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -14,24 +13,6 @@ type NetworkPolicyBuilder struct {
 	comment       string
 	allowedIPList string
 	blockedIPList string
-}
-
-// WithComment adds a comment to the NetworkPolicyBuilder.
-func (npb *NetworkPolicyBuilder) WithComment(c string) *NetworkPolicyBuilder {
-	npb.comment = EscapeString(c)
-	return npb
-}
-
-// WithAllowedIPList adds an allowedIpList to the NetworkPolicyBuilder.
-func (npb *NetworkPolicyBuilder) WithAllowedIPList(allowedIps []string) *NetworkPolicyBuilder {
-	npb.allowedIPList = helpers.IPListToSnowflakeString(allowedIps)
-	return npb
-}
-
-// WithBlockedIPList adds a blockedIpList to the NetworkPolicyBuilder.
-func (npb *NetworkPolicyBuilder) WithBlockedIPList(blockedIps []string) *NetworkPolicyBuilder {
-	npb.blockedIPList = helpers.IPListToSnowflakeString(blockedIps)
-	return npb
 }
 
 // NetworkPolicy returns a pointer to a Builder that abstracts the DDL operations for a network policy.
@@ -46,44 +27,6 @@ func NetworkPolicy(name string) *NetworkPolicyBuilder {
 	return &NetworkPolicyBuilder{
 		name: name,
 	}
-}
-
-// Create returns the SQL query that will create a network policy.
-func (npb *NetworkPolicyBuilder) Create() string {
-	createSQL := fmt.Sprintf(`CREATE NETWORK POLICY "%v" ALLOWED_IP_LIST=%v`, npb.name, npb.allowedIPList)
-	if npb.blockedIPList != "" {
-		createSQL += fmt.Sprintf(" BLOCKED_IP_LIST=%v", npb.blockedIPList)
-	}
-	if npb.comment != "" {
-		createSQL += fmt.Sprintf(` COMMENT="%v"`, npb.comment)
-	}
-
-	return createSQL
-}
-
-// Describe returns the SQL query that will describe a network policy.
-func (npb *NetworkPolicyBuilder) Describe() string {
-	return fmt.Sprintf(`DESC NETWORK POLICY "%v"`, npb.name)
-}
-
-// ChangeComment returns the SQL query that will update the comment on the network policy.
-func (npb *NetworkPolicyBuilder) ChangeComment(c string) string {
-	return fmt.Sprintf(`ALTER NETWORK POLICY "%v" SET COMMENT = '%v'`, npb.name, EscapeString(c))
-}
-
-// RemoveComment returns the SQL query that will remove the comment on the network policy.
-func (npb *NetworkPolicyBuilder) RemoveComment() string {
-	return fmt.Sprintf(`ALTER NETWORK POLICY "%v" UNSET COMMENT`, npb.name)
-}
-
-// ChangeIpList returns the SQL query that will update the ip list (of the specified listType) on the network policy.
-func (npb *NetworkPolicyBuilder) ChangeIPList(listType string, ips []string) string {
-	return fmt.Sprintf(`ALTER NETWORK POLICY "%v" SET %v_IP_LIST = %v`, npb.name, listType, helpers.IPListToSnowflakeString(ips))
-}
-
-// Drop returns the SQL query that will drop a network policy.
-func (npb *NetworkPolicyBuilder) Drop() string {
-	return fmt.Sprintf(`DROP NETWORK POLICY "%v"`, npb.name)
 }
 
 // SetOnAccount returns the SQL query that will set the network policy globally on your Snowflake account.
@@ -106,12 +49,6 @@ func (npb *NetworkPolicyBuilder) UnsetOnUser(u string) string {
 	return fmt.Sprintf(`ALTER USER "%v" UNSET NETWORK_POLICY`, u)
 }
 
-// ShowAllNetworkPolicies returns the SQL query that will SHOW *all* network policies in the Snowflake account
-// Snowflake's implementation of SHOW for network policies does *not* support limiting results with LIKE.
-func (npb *NetworkPolicyBuilder) ShowAllNetworkPolicies() string {
-	return `SHOW NETWORK POLICIES`
-}
-
 // ShowOnUser returns the SQL query that will SHOW network policy set on a specific User.
 func (npb *NetworkPolicyBuilder) ShowOnUser(u string) string {
 	return fmt.Sprintf(`SHOW PARAMETERS LIKE 'network_policy' IN USER "%v"`, u)
@@ -122,33 +59,10 @@ func (npb *NetworkPolicyBuilder) ShowOnAccount() string {
 	return `SHOW PARAMETERS LIKE 'network_policy' IN ACCOUNT`
 }
 
-type NetworkPolicyStruct struct {
-	CreatedOn              sql.NullString `db:"created_on"`
-	Name                   sql.NullString `db:"name"`
-	Comment                sql.NullString `db:"comment"`
-	EntriesInAllowedIPList sql.NullString `db:"entries_in_allowed_ip_list"`
-	EntriesInBlockedIPList sql.NullString `db:"entries_in_blocked_ip_list"`
-}
-
 type NetworkPolicyAttachmentStruct struct {
 	Key   sql.NullString `db:"key"`
 	Value sql.NullString `db:"value"`
 	Level sql.NullString `db:"level"`
-}
-
-// ScanNetworkPolicies takes database rows and converts them to a list of NetworkPolicyStruct pointers.
-func ScanNetworkPolicies(rows *sqlx.Rows) ([]*NetworkPolicyStruct, error) {
-	var n []*NetworkPolicyStruct
-
-	for rows.Next() {
-		r := &NetworkPolicyStruct{}
-		err := rows.StructScan(r)
-		if err != nil {
-			return nil, err
-		}
-		n = append(n, r)
-	}
-	return n, nil
 }
 
 func ScanNetworkPolicyAttachment(row *sqlx.Row) (*NetworkPolicyAttachmentStruct, error) {
