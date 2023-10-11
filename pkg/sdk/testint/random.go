49e50fb7e9042f4e972c27deef208cd4482d132f
Artur Sawicki <artur.sawicki@snowflake.com>
2023-10-11 15:47:26 +0200

    Delegate random to sdk exported ones

--- pkg/sdk/testint/random.go
+++ pkg/sdk/testint/random.go
@@ -4,9 +4,6 @@ import (
 	"testing"
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
-	"github.com/brianvoe/gofakeit/v6"
-	"github.com/hashicorp/go-uuid"
-	"github.com/stretchr/testify/require"
 )
 
 // All functions in this file exist also in SDK (they are used also there).
@@ -14,69 +11,53 @@ import (
 // This will be dealt with in subsequent PRs.
 
 func randomUUID(t *testing.T) string {
-	t.Helper()
-	v, err := uuid.GenerateUUID()
-	require.NoError(t, err)
-	return v
+	return sdk.RandomUUID(t)
 }
 
 func randomComment(t *testing.T) string {
-	t.Helper()
-	return gofakeit.Sentence(10)
+	return sdk.RandomComment(t)
 }
 
 func randomBool(t *testing.T) bool {
-	t.Helper()
-	return gofakeit.Bool()
+	return sdk.RandomBool(t)
 }
 
 func randomString(t *testing.T) string {
-	t.Helper()
-	return gofakeit.Password(true, true, true, true, false, 28)
+	return sdk.RandomString(t)
 }
 
 func randomStringN(t *testing.T, num int) string {
-	t.Helper()
-	return gofakeit.Password(true, true, true, true, false, num)
+	return sdk.RandomStringN(t, num)
 }
 
 func randomAlphanumericN(t *testing.T, num int) string {
-	t.Helper()
-	return gofakeit.Password(true, true, true, false, false, num)
+	return sdk.RandomAlphanumericN(t, num)
 }
 
 func randomStringRange(t *testing.T, min, max int) string {
-	t.Helper()
-	if min > max {
-		t.Errorf("min %d is greater than max %d", min, max)
-	}
-	return gofakeit.Password(true, true, true, true, false, randomIntRange(t, min, max))
+	return sdk.RandomStringRange(t, min, max)
 }
 
 func randomIntRange(t *testing.T, min, max int) int {
-	t.Helper()
-	if min > max {
-		t.Errorf("min %d is greater than max %d", min, max)
-	}
-	return gofakeit.IntRange(min, max)
+	return sdk.RandomIntRange(t, min, max)
 }
 
 func randomSchemaObjectIdentifier(t *testing.T) sdk.SchemaObjectIdentifier {
 	t.Helper()
-	return sdk.NewSchemaObjectIdentifier(randomStringN(t, 12), randomStringN(t, 12), randomStringN(t, 12))
+	return sdk.RandomSchemaObjectIdentifier(t)
 }
 
 func randomDatabaseObjectIdentifier(t *testing.T) sdk.DatabaseObjectIdentifier {
 	t.Helper()
-	return sdk.NewDatabaseObjectIdentifier(randomStringN(t, 12), randomStringN(t, 12))
+	return sdk.RandomDatabaseObjectIdentifier(t)
 }
 
 func alphanumericDatabaseObjectIdentifier(t *testing.T) sdk.DatabaseObjectIdentifier {
 	t.Helper()
-	return sdk.NewDatabaseObjectIdentifier(randomAlphanumericN(t, 12), randomAlphanumericN(t, 12))
+	return sdk.AlphanumericDatabaseObjectIdentifier(t)
 }
 
 func randomAccountObjectIdentifier(t *testing.T) sdk.AccountObjectIdentifier {
 	t.Helper()
-	return sdk.NewAccountObjectIdentifier(randomStringN(t, 12))
+	return sdk.RandomAccountObjectIdentifier(t)
 }
