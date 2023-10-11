7ea7f789c59c417ff2d979a60c478694eadb726f
Artur Sawicki <artur.sawicki@snowflake.com>
2023-10-11 17:24:00 +0200

    Update comment

--- pkg/sdk/integration_test_imports.go
+++ pkg/sdk/integration_test_imports.go
@@ -7,13 +7,6 @@ import (
 
 // All the contents of this file were added to be able to use them outside the sdk package (i.e. integration tests package).
 // It was easier to do it that way, so that we do not include big rename changes in the first moving PR.
-// For each of them we will have to decide what do we do:
-// - do we expose the field/method
-// - do we keep the workaround
-// - do we copy the code (e.g. ExecForTests)
-// - do we move the code to other place and use it from both places
-// - something else.
-// This will be handled in subsequent PRs, so that the main difficulty (moving) is already merged.
 
 // ExecForTests is an exact copy of exec (that is unexported), that some integration tests/helpers were using
 // TODO: remove after we have all usages covered by SDK (for now it means implementing stages, tables, and tags)
