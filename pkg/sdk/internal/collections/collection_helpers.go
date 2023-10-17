e119fab78330fb51de1a508540742945440619e8
Artur Sawicki <artur.sawicki@snowflake.com>
2023-10-17 13:44:28 +0200

    Add more logging for failing test

--- pkg/sdk/internal/collections/collection_helpers.go
+++ pkg/sdk/internal/collections/collection_helpers.go
@@ -2,12 +2,14 @@ package collections
 
 import (
 	"errors"
+	"fmt"
 )
 
 var ErrObjectNotFound = errors.New("object does not exist")
 
 func FindOne[T any](collection []T, condition func(T) bool) (*T, error) {
 	for _, o := range collection {
+		fmt.Printf("%v\n", o)
 		if condition(o) {
 			return &o, nil
 		}
