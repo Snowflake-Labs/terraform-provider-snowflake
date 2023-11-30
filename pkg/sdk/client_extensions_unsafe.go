196134cbf91996eabc50bdc586a657fe7ac71900
Artur Sawicki <artur.sawicki@snowflake.com>
2023-11-30 13:56:55 +0100

    feat: Add unsafe_execute resource (#2225)
    * add unsafe exec resource
    
    * Rename resource and make the basic tests pass
    
    * Simplify tests setup
    
    * Rename resource args
    
    * Fix checking db existence
    
    * Add random part to id
    
    * Rename migration
    
    * Remove unnecessary config param
    
    * Remove duplicated checks
    
    * Extract function to generate capitalized name
    
    * Test escaped identifier
    
    * Test unsafe execute with grants
    
    * Add TODO
    
    * Add test with HCL for_each
    
    * Fix before pushing
    
    * Update docs with warning
    
    * Update example usage in docs
    
    * Add negative tests
    
    * Add read (WIP)
    
    * Pass test without query
    
    * Add test for query removed
    
    * Move unsafe client and implementation to SDK
    
    * Fix query unsafe
    
    * Update docs and change query behavior
    
    * Update docs
    
    * Update logs
    
    * Fix test
    
    ---------
    
    Co-authored-by: Jan Cieślak <jan.cieslak@snowflake.com>

---
+++ pkg/sdk/client_extensions_unsafe.go
@@ -0,0 +1,65 @@
+package sdk
+
+import (
+	"context"
+	"database/sql"
+)
+
+func (c *Client) ExecUnsafe(ctx context.Context, sql string) (sql.Result, error) {
+	return c.exec(ctx, sql)
+}
+
+// QueryUnsafe for now only supports single query. For more queries we will have to adjust the behavior. From the gosnowflake driver docs:
+//
+//	 (...) while using the multi-statement feature, pass a Context that specifies the number of statements in the string.
+//		When multiple queries are executed by a single call to QueryContext(), multiple result sets are returned. After you process the first result set, get the next result set (for the next SQL statement) by calling NextResultSet().
+//
+// Therefore, only single resultSet is processed.
+func (c *Client) QueryUnsafe(ctx context.Context, sql string) ([]map[string]*any, error) {
+	rows, err := c.db.QueryContext(ctx, sql)
+	if err != nil {
+		return nil, err
+	}
+	allRows, err := unsafeExecuteProcessRows(rows)
+	if err != nil {
+		return nil, err
+	}
+	return allRows, nil
+}
+
+func unsafeExecuteProcessRows(rows *sql.Rows) ([]map[string]*any, error) {
+	defer rows.Close()
+
+	columnNames, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	processedRows := make([]map[string]*any, 0)
+	for rows.Next() {
+		row, err := unsafeExecuteProcessRow(rows, columnNames)
+		if err != nil {
+			return nil, err
+		}
+		processedRows = append(processedRows, row)
+	}
+
+	return processedRows, nil
+}
+
+func unsafeExecuteProcessRow(rows *sql.Rows, columnNames []string) (map[string]*any, error) {
+	values := make([]any, len(columnNames))
+	for i := range values {
+		values[i] = new(any)
+	}
+
+	err := rows.Scan(values...)
+	if err != nil {
+		return nil, err
+	}
+
+	row := make(map[string]*any)
+	for i, col := range columnNames {
+		row[col] = values[i].(*any)
+	}
+	return row, nil
+}
