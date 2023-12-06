81fcc0fccd0d30279b8711eb8663f556220211d3
Artur Sawicki <artur.sawicki@snowflake.com>
2023-12-06 14:27:52 +0100

    Add test for all params

--- pkg/resources/password_policy_acceptance_test.go
+++ pkg/resources/password_policy_acceptance_test.go
@@ -13,16 +13,28 @@ import (
 
 func TestAcc_PasswordPolicy(t *testing.T) {
 	accName := strings.ToUpper(acctest.RandStringFromCharSet(10, acctest.CharSetAlpha))
-	m := func(minLength int, maxLength int, comment string) map[string]config.Variable {
+	m := func(minLength int, maxLength int, minUpperCaseChars int, minLowerCaseChars int, minNumericChars int, minSpecialChars int, minAgeDays int, maxAgeDays int, maxRetries int, lockoutTimeMins int, history int, comment string) map[string]config.Variable {
 		return map[string]config.Variable{
-			"name":       config.StringVariable(accName),
-			"database":   config.StringVariable(acc.TestDatabaseName),
-			"schema":     config.StringVariable(acc.TestSchemaName),
-			"min_length": config.IntegerVariable(minLength),
-			"max_length": config.IntegerVariable(maxLength),
-			"comment":    config.StringVariable(comment),
+			"name":                 config.StringVariable(accName),
+			"database":             config.StringVariable(acc.TestDatabaseName),
+			"schema":               config.StringVariable(acc.TestSchemaName),
+			"min_length":           config.IntegerVariable(minLength),
+			"max_length":           config.IntegerVariable(maxLength),
+			"min_upper_case_chars": config.IntegerVariable(minUpperCaseChars),
+			"min_lower_case_chars": config.IntegerVariable(minLowerCaseChars),
+			"min_numeric_chars":    config.IntegerVariable(minNumericChars),
+			"min_special_chars":    config.IntegerVariable(minSpecialChars),
+			"min_age_days":         config.IntegerVariable(minAgeDays),
+			"max_age_days":         config.IntegerVariable(maxAgeDays),
+			"max_retries":          config.IntegerVariable(maxRetries),
+			"lockout_time_mins":    config.IntegerVariable(lockoutTimeMins),
+			"history":              config.IntegerVariable(history),
+			"comment":              config.StringVariable(comment),
 		}
 	}
+	variables1 := m(10, 30, 2, 3, 4, 5, 6, 7, 8, 9, 10, "this is a test resource")
+	variables2 := m(20, 50, 1, 2, 3, 4, 5, 6, 7, 8, 9, "this is a test resource")
+	variables3 := m(20, 50, 1, 2, 3, 4, 5, 6, 7, 8, 9, "")
 
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: acc.TestAccProtoV6ProviderFactories,
@@ -34,33 +46,51 @@ func TestAcc_PasswordPolicy(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				ConfigDirectory: config.TestNameDirectory(),
-				ConfigVariables: m(10, 30, "this is a test resource"),
+				ConfigVariables: variables1,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "name", accName),
 					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_length", "10"),
 					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "max_length", "30"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_upper_case_chars", "2"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_lower_case_chars", "3"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_numeric_chars", "4"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_special_chars", "5"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_age_days", "6"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "max_age_days", "7"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "max_retries", "8"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "lockout_time_mins", "9"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "history", "10"),
 					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "comment", "this is a test resource"),
 				),
 			},
 			{
 				ConfigDirectory: config.TestNameDirectory(),
-				ConfigVariables: m(20, 50, "this is a test resource"),
+				ConfigVariables: variables2,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_length", "20"),
 					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "max_length", "50"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_upper_case_chars", "1"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_lower_case_chars", "2"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_numeric_chars", "3"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_special_chars", "4"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "min_age_days", "5"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "max_age_days", "6"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "max_retries", "7"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "lockout_time_mins", "8"),
+					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "history", "9"),
 					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "comment", "this is a test resource"),
 				),
 			},
 			{
 				ConfigDirectory: config.TestNameDirectory(),
-				ConfigVariables: m(20, 50, ""),
+				ConfigVariables: variables3,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("snowflake_password_policy.pa", "comment", ""),
 				),
 			},
 			{
 				ConfigDirectory:   config.TestNameDirectory(),
-				ConfigVariables:   m(20, 50, ""),
+				ConfigVariables:   variables3,
 				ResourceName:      "snowflake_password_policy.pa",
 				ImportState:       true,
 				ImportStateVerify: true,
