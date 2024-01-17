f968db11a9b41ffa594170415c4edd9376f574e7
Artur Sawicki <artur.sawicki@snowflake.com>
2024-01-17 10:56:19 +0100

    feat: Add managed account to the SDK (#2357)
    Add managed account to the SDK:
    - used generator
    - changed no quotes to single quotes for `ADMIN_NAME`
    
    References
    * [CREATE MANAGED
    ACCOUNT](https://docs.snowflake.com/en/sql-reference/sql/create-managed-account)
    * [DROP MANAGED
    ACCOUNT](https://docs.snowflake.com/en/sql-reference/sql/drop-managed-account)
    * [SHOW MANAGED
    ACCOUNTS](https://docs.snowflake.com/en/sql-reference/sql/show-managed-accounts)

---
+++ pkg/sdk/managed_accounts_gen.go
@@ -0,0 +1,66 @@
+package sdk
+
+import (
+	"context"
+	"database/sql"
+)
+
+type ManagedAccounts interface {
+	Create(ctx context.Context, request *CreateManagedAccountRequest) error
+	Drop(ctx context.Context, request *DropManagedAccountRequest) error
+	Show(ctx context.Context, request *ShowManagedAccountRequest) ([]ManagedAccount, error)
+	ShowByID(ctx context.Context, id AccountObjectIdentifier) (*ManagedAccount, error)
+}
+
+// CreateManagedAccountOptions is based on https://docs.snowflake.com/en/sql-reference/sql/create-managed-account.
+type CreateManagedAccountOptions struct {
+	create                     bool                       `ddl:"static" sql:"CREATE"`
+	managedAccount             bool                       `ddl:"static" sql:"MANAGED ACCOUNT"`
+	name                       AccountObjectIdentifier    `ddl:"identifier"`
+	CreateManagedAccountParams CreateManagedAccountParams `ddl:"list,no_parentheses"`
+}
+
+type CreateManagedAccountParams struct {
+	AdminName     string  `ddl:"parameter,single_quotes" sql:"ADMIN_NAME"`
+	AdminPassword string  `ddl:"parameter,single_quotes" sql:"ADMIN_PASSWORD"`
+	typeProvider  string  `ddl:"static" sql:"TYPE = READER"`
+	Comment       *string `ddl:"parameter,single_quotes" sql:"COMMENT"`
+}
+
+// DropManagedAccountOptions is based on https://docs.snowflake.com/en/sql-reference/sql/drop-managed-account.
+type DropManagedAccountOptions struct {
+	drop           bool                    `ddl:"static" sql:"DROP"`
+	managedAccount bool                    `ddl:"static" sql:"MANAGED ACCOUNT"`
+	name           AccountObjectIdentifier `ddl:"identifier"`
+}
+
+// ShowManagedAccountOptions is based on https://docs.snowflake.com/en/sql-reference/sql/show-managed-accounts.
+type ShowManagedAccountOptions struct {
+	show            bool  `ddl:"static" sql:"SHOW"`
+	managedAccounts bool  `ddl:"static" sql:"MANAGED ACCOUNTS"`
+	Like            *Like `ddl:"keyword" sql:"LIKE"`
+}
+
+type managedAccountDBRow struct {
+	Name              string         `db:"name"`
+	Cloud             string         `db:"cloud"`
+	Region            string         `db:"region"`
+	Locator           string         `db:"locator"`
+	CreatedOn         string         `db:"created_on"`
+	Url               string         `db:"url"`
+	AccountLocatorUrl string         `db:"account_locator_url"`
+	IsReader          bool           `db:"is_reader"`
+	Comment           sql.NullString `db:"comment"`
+}
+
+type ManagedAccount struct {
+	Name              string
+	Cloud             string
+	Region            string
+	Locator           string
+	CreatedOn         string
+	URL               string
+	AccountLocatorURL string
+	IsReader          bool
+	Comment           string
+}
---
+++ pkg/sdk/managed_accounts_gen_test.go
@@ -0,0 +1,104 @@
+package sdk
+
+import "testing"
+
+func TestManagedAccounts_Create(t *testing.T) {
+	id := RandomAccountObjectIdentifier()
+
+	// Minimal valid CreateManagedAccountOptions
+	defaultOpts := func() *CreateManagedAccountOptions {
+		return &CreateManagedAccountOptions{
+			name: id,
+			CreateManagedAccountParams: CreateManagedAccountParams{
+				AdminName:     "admin",
+				AdminPassword: "password",
+			},
+		}
+	}
+
+	t.Run("validation: nil options", func(t *testing.T) {
+		var opts *CreateManagedAccountOptions = nil
+		assertOptsInvalidJoinedErrors(t, opts, ErrNilOptions)
+	})
+
+	t.Run("validation: valid identifier for [opts.name]", func(t *testing.T) {
+		opts := defaultOpts()
+		opts.name = NewAccountObjectIdentifier("")
+		assertOptsInvalidJoinedErrors(t, opts, ErrInvalidObjectIdentifier)
+	})
+
+	t.Run("validation: [opts.CreateManagedAccountParams.AdminName] should be set", func(t *testing.T) {
+		opts := defaultOpts()
+		opts.CreateManagedAccountParams.AdminName = ""
+		assertOptsInvalidJoinedErrors(t, opts, errNotSet("CreateManagedAccountOptions.CreateManagedAccountParams", "AdminName"))
+	})
+
+	t.Run("validation: [opts.CreateManagedAccountParams.AdminPassword] should be set", func(t *testing.T) {
+		opts := defaultOpts()
+		opts.CreateManagedAccountParams.AdminPassword = ""
+		assertOptsInvalidJoinedErrors(t, opts, errNotSet("CreateManagedAccountOptions.CreateManagedAccountParams", "AdminPassword"))
+	})
+
+	t.Run("basic", func(t *testing.T) {
+		opts := defaultOpts()
+		assertOptsValidAndSQLEquals(t, opts, "CREATE MANAGED ACCOUNT %s ADMIN_NAME = 'admin', ADMIN_PASSWORD = 'password', TYPE = READER", id.FullyQualifiedName())
+	})
+
+	t.Run("all options", func(t *testing.T) {
+		opts := defaultOpts()
+		opts.CreateManagedAccountParams.Comment = String("comment")
+		assertOptsValidAndSQLEquals(t, opts, "CREATE MANAGED ACCOUNT %s ADMIN_NAME = 'admin', ADMIN_PASSWORD = 'password', TYPE = READER, COMMENT = 'comment'", id.FullyQualifiedName())
+	})
+}
+
+func TestManagedAccounts_Drop(t *testing.T) {
+	id := RandomAccountObjectIdentifier()
+
+	// Minimal valid DropManagedAccountOptions
+	defaultOpts := func() *DropManagedAccountOptions {
+		return &DropManagedAccountOptions{
+			name: id,
+		}
+	}
+
+	t.Run("validation: nil options", func(t *testing.T) {
+		var opts *DropManagedAccountOptions = nil
+		assertOptsInvalidJoinedErrors(t, opts, ErrNilOptions)
+	})
+
+	t.Run("validation: valid identifier for [opts.name]", func(t *testing.T) {
+		opts := defaultOpts()
+		opts.name = NewAccountObjectIdentifier("")
+		assertOptsInvalidJoinedErrors(t, opts, ErrInvalidObjectIdentifier)
+	})
+
+	t.Run("basic", func(t *testing.T) {
+		opts := defaultOpts()
+		assertOptsValidAndSQLEquals(t, opts, "DROP MANAGED ACCOUNT %s", id.FullyQualifiedName())
+	})
+}
+
+func TestManagedAccounts_Show(t *testing.T) {
+	// Minimal valid ShowManagedAccountOptions
+	defaultOpts := func() *ShowManagedAccountOptions {
+		return &ShowManagedAccountOptions{}
+	}
+
+	t.Run("validation: nil options", func(t *testing.T) {
+		var opts *ShowManagedAccountOptions = nil
+		assertOptsInvalidJoinedErrors(t, opts, ErrNilOptions)
+	})
+
+	t.Run("basic", func(t *testing.T) {
+		opts := defaultOpts()
+		assertOptsValidAndSQLEquals(t, opts, "SHOW MANAGED ACCOUNTS")
+	})
+
+	t.Run("all options", func(t *testing.T) {
+		opts := defaultOpts()
+		opts.Like = &Like{
+			Pattern: String("myaccount"),
+		}
+		assertOptsValidAndSQLEquals(t, opts, "SHOW MANAGED ACCOUNTS LIKE 'myaccount'")
+	})
+}
