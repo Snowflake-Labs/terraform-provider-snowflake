a0a930b381ffe93e6e6c31a197f651b520df7d26
Raül Bonet <rbonet@hackerone.com>
2024-01-21 17:46:34 +0100

    feat: implement validate()

--- pkg/sdk/policy_references_validation.go
+++ pkg/sdk/policy_references_validation.go
@@ -1,6 +1,30 @@
 package sdk
 
+import (
+	"errors"
+)
+
+var _ validatable = new(getForEntityPolicyReferenceOptions)
+
 func (opts *getForEntityPolicyReferenceOptions) validate() error {
-	// TODO
-	return nil
+	if opts == nil {
+		return errors.Join(ErrNilOptions)
+	}
+	var errs []error
+	if !opts.select_ {
+		errs = append(errs, errors.New("select_ must be true"))
+	}
+	if !opts.asterisk {
+		errs = append(errs, errors.New("asterisk must be true"))
+	}
+	if !opts.from {
+		errs = append(errs, errors.New("from must be true"))
+	}
+	if !opts.tableFunction {
+		errs = append(errs, errors.New("tableFunction must be true"))
+	}
+	if !opts.endTableFunction {
+		errs = append(errs, errors.New("endTableFunction must be true"))
+	}
+	return errors.Join(errs...)
 }
