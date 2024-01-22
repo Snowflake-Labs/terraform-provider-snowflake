d42b7e6f0019bf26941a53a0a45f5ce82dff2587
Artur Sawicki <artur.sawicki@snowflake.com>
2024-01-22 14:11:07 +0100

    Start materialized view definition

---
+++ pkg/sdk/materialized_views_def.go
@@ -0,0 +1,49 @@
+package sdk
+
+import g "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk/poc/generator"
+
+//go:generate go run ./poc/main.go
+
+var materializedViewColumn = g.NewQueryStruct("MaterializedViewColumn").
+	Text("Name", g.KeywordOptions().DoubleQuotes().Required()).
+	OptionalTextAssignment("COMMENT", g.ParameterOptions().SingleQuotes().NoEquals())
+
+var materializedViewColumnMaskingPolicy = g.NewQueryStruct("MaterializedViewColumnMaskingPolicy").
+	Text("Name", g.KeywordOptions().Required()).
+	Identifier("MaskingPolicy", g.KindOfT[SchemaObjectIdentifier](), g.IdentifierOptions().SQL("MASKING POLICY").Required()).
+	NamedListWithParens("USING", g.KindOfT[string](), nil). // TODO: double quotes here?
+	OptionalTags()
+
+var materializedViewRowAccessPolicy = g.NewQueryStruct("MaterializedViewRowAccessPolicy").
+	Identifier("RowAccessPolicy", g.KindOfT[SchemaObjectIdentifier](), g.IdentifierOptions().SQL("ROW ACCESS POLICY").Required()).
+	NamedListWithParens("ON", g.KindOfT[string](), g.KeywordOptions().Required()). // TODO: double quotes here?
+	WithValidation(g.ValidIdentifier, "RowAccessPolicy")
+
+var MaterializedViewsDef = g.NewInterface(
+	"MaterializedViews",
+	"MaterializedView",
+	g.KindOfT[SchemaObjectIdentifier](),
+).
+	CreateOperation(
+		"https://docs.snowflake.com/en/sql-reference/sql/create-materialized-view",
+		g.NewQueryStruct("CreateMaterializedView").
+			Create().
+			OrReplace().
+			OptionalSQL("SECURE").
+			SQL("MATERIALIZED VIEW").
+			IfNotExists().
+			Name().
+			OptionalCopyGrants().
+			ListQueryStructField("Columns", materializedViewColumn, g.ListOptions().Parentheses()).
+			ListQueryStructField("ColumnsMaskingPolicies", materializedViewColumnMaskingPolicy, g.ListOptions().NoParentheses().NoEquals()).
+			OptionalTextAssignment("COMMENT", g.ParameterOptions().SingleQuotes()).
+			// In the current docs ROW ACCESS POLICY and TAG are specified twice.
+			// It is a mistake probably so here they are present only once.
+			OptionalQueryStructField("RowAccessPolicy", materializedViewRowAccessPolicy, g.KeywordOptions()).
+			OptionalTags().
+			NamedListWithParens("CLUSTER BY", g.KindOfT[string](), g.KeywordOptions()).
+			SQL("AS").
+			Text("sql", g.KeywordOptions().NoQuotes().Required()).
+			WithValidation(g.ValidIdentifier, "name").
+			WithValidation(g.ConflictingFields, "OrReplace", "IfNotExists"),
+	)
--- pkg/sdk/poc/main.go
+++ pkg/sdk/poc/main.go
@@ -31,6 +31,7 @@ var definitionMapping = map[string]*generator.Interface{
 	"storage_integration_def.go":  sdk.StorageIntegrationDef,
 	"managed_accounts_def.go":     sdk.ManagedAccountsDef,
 	"row_access_policies_def.go":  sdk.RowAccessPoliciesDef,
+	"materialized_views_def.go":   sdk.MaterializedViewsDef,
 }
 
 func main() {
