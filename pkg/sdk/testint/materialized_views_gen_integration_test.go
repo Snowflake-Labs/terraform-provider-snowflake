78c0c3bf51ef8216c69be3e6eed72955d6ffb00f
Artur Sawicki <artur.sawicki@snowflake.com>
2024-01-23 13:21:54 +0100

    Prepare integration test list

--- pkg/sdk/testint/materialized_views_gen_integration_test.go
+++ pkg/sdk/testint/materialized_views_gen_integration_test.go
@@ -1,31 +1,68 @@
 package testint
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestInt_MaterializedViews(t *testing.T) {
-	// TODO: prepare common resources
+	client := testClient(t)
+	ctx := testContext(t)
 
-	t.Run("Create", func(t *testing.T) {
+	table, tableCleanup := createTable(t, client, testDb(t), testSchema(t))
+	t.Cleanup(tableCleanup)
+
+	sql := fmt.Sprintf("SELECT id FROM %s", table.ID().FullyQualifiedName())
+
+	t.Run("create materialized view: no optionals", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Alter", func(t *testing.T) {
+	t.Run("create materialized view: complete case", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Drop", func(t *testing.T) {
+	t.Run("drop materialized view: existing", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Show", func(t *testing.T) {
+	t.Run("drop view: non-existing", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("ShowByID", func(t *testing.T) {
+	t.Run("alter materialized view: rename", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Describe", func(t *testing.T) {
+	t.Run("alter materialized view: set cluster by", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("alter materialized view: recluster suspend and resume", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("alter materialized view: suspend and resume", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("alter materialized view: set and unset values", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("show materialized view: default", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("show materialized view: with options", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("describe materialized view", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("describe materialized view: non-existing", func(t *testing.T) {
 		// TODO: fill me
 	})
 }
