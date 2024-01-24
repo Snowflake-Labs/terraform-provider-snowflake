4798a53b9daa7b281516f8cb7066724cff972ac2
Artur Sawicki <artur.sawicki@snowflake.com>
2024-01-24 10:13:34 +0100

    List integration tests to implement

--- pkg/sdk/testint/api_integrations_gen_integration_test.go
+++ pkg/sdk/testint/api_integrations_gen_integration_test.go
@@ -5,27 +5,55 @@ import "testing"
 func TestInt_ApiIntegrations(t *testing.T) {
 	// TODO: prepare common resources
 
-	t.Run("Create", func(t *testing.T) {
+	t.Run("create api integration: aws", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Alter", func(t *testing.T) {
+	t.Run("create api integration: azure", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Drop", func(t *testing.T) {
+	t.Run("create api integration: google", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Show", func(t *testing.T) {
+	t.Run("alter api integration: aws", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("ShowByID", func(t *testing.T) {
+	t.Run("alter api integration: azure", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Describe", func(t *testing.T) {
+	t.Run("alter api integration: google", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("drop view: existing", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("drop view: non-existing", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("show api integration: default", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("show api integration: with options", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("describe api integration: aws", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("describe api integration: azure", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("describe api integration: google", func(t *testing.T) {
 		// TODO: fill me
 	})
 }
