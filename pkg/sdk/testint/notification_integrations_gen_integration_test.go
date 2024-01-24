7f5cb8c1e452f6ce8d9de89de622e0ac9cefa7ea
Artur Sawicki <artur.sawicki@snowflake.com>
2024-01-24 15:02:59 +0100

    List integration tests to implement

--- pkg/sdk/testint/notification_integrations_gen_integration_test.go
+++ pkg/sdk/testint/notification_integrations_gen_integration_test.go
@@ -5,27 +5,71 @@ import "testing"
 func TestInt_NotificationIntegrations(t *testing.T) {
 	// TODO: prepare common resources
 
-	t.Run("Create", func(t *testing.T) {
+	t.Run("create and describe notification integration - auto google", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Alter", func(t *testing.T) {
+	t.Run("create and describe notification integration - auto azure", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Drop", func(t *testing.T) {
+	t.Run("create and describe notification integration - push amazon", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Show", func(t *testing.T) {
+	t.Run("create and describe notification integration - push google", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("ShowByID", func(t *testing.T) {
+	t.Run("create and describe notification integration - push azure", func(t *testing.T) {
 		// TODO: fill me
 	})
 
-	t.Run("Describe", func(t *testing.T) {
+	t.Run("create and describe notification integration - email", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("alter notification integration: auto", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("alter notification integration: push amazon", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("alter notification integration: push google", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("alter notification integration: push azure", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("alter notification integration: email", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("alter notification integration: set and unset tags", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("drop notification integration: existing", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("drop notification integration: non-existing", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("show notification integration: default", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("show notification integration: with options", func(t *testing.T) {
+		// TODO: fill me
+	})
+
+	t.Run("describe notification integration: non-existing", func(t *testing.T) {
 		// TODO: fill me
 	})
 }
