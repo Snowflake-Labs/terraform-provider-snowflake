0385650f9ea7d1708b65139f2eeeacc373d5c9f8
Artur Sawicki <artur.sawicki@snowflake.com>
2024-02-08 17:41:22 +0100

    fix: Fix task related issues (#2479)
    Related issues: #2036 #2207 #2346.
    
    - (#2207) The reason was the change in parsing the `SHOW TASKS` response
    for `after` parameter. In earlier versions, the parsing was incorrect
    for the identifiers containing dot characters. The new one fixed the dot
    case but Snowflake identifiers surprised us one more time, and the basic
    case, without any `"` was not supported correctly.
    - (#2036) There is no way to unset `when` currently in Snowflake. We
    don't want to always `ForceNew` for when change. For that reason we run
    forceNew conditionally using `customdiff.ForceNewIfChange`.
    - (#2346) Waiting for answers but maybe this fix will also solve this
    issues.

---
+++ pkg/resources/testdata/TestAcc_Task_issue2207/1/variables.tf
@@ -0,0 +1,23 @@
+variable "database" {
+  type = string
+}
+
+variable "schema" {
+  type = string
+}
+
+variable "warehouse" {
+  type = string
+}
+
+variable "root_name" {
+  type = string
+}
+
+variable "child_name" {
+  type = string
+}
+
+variable "comment" {
+  type = string
+}
