72fe7aeda640dd3500a62ca0b07740dc8dbcd4fe
Jan Cieślak <jan.cieslak@snowflake.com>
2024-02-13 08:22:16 +0100

    fix... again

--- pkg/resources/testdata/TestAcc_GrantPrivilegesToShare/OnDatabase_NoGrant/test.tf
+++ pkg/resources/testdata/TestAcc_GrantPrivilegesToShare/OnDatabase_NoGrant/test.tf
@@ -1,8 +1,8 @@
 resource "snowflake_share" "test" {
+  depends_on = [snowflake_database.test]
   name = var.to_share
 }
 
 resource "snowflake_database" "test" {
-  depends_on = [snowflake_share.test]
   name       = var.database
 }
