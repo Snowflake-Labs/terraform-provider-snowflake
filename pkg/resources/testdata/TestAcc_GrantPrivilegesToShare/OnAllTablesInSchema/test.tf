5bdc85c40df8e8c7d071153e09cf2620c60f103d
Artur Sawicki <artur.sawicki@snowflake.com>
2024-02-14 13:06:22 +0100

    Run make pre-push

--- pkg/resources/testdata/TestAcc_GrantPrivilegesToShare/OnAllTablesInSchema/test.tf
+++ pkg/resources/testdata/TestAcc_GrantPrivilegesToShare/OnAllTablesInSchema/test.tf
@@ -1,10 +1,10 @@
 resource "snowflake_share" "test" {
-  name = var.to_share
+  name       = var.to_share
   depends_on = [snowflake_database.test]
 }
 
 resource "snowflake_database" "test" {
-  name       = var.database
+  name = var.database
 }
 
 resource "snowflake_schema" "test" {
--- pkg/resources/testdata/TestAcc_GrantPrivilegesToShare/OnTable_NoGrant/test.tf
+++ pkg/resources/testdata/TestAcc_GrantPrivilegesToShare/OnTable_NoGrant/test.tf
@@ -1,10 +1,10 @@
 resource "snowflake_share" "test" {
   depends_on = [snowflake_database.test]
-  name = var.to_share
+  name       = var.to_share
 }
 
 resource "snowflake_database" "test" {
-  name       = var.database
+  name = var.database
 }
 
 resource "snowflake_schema" "test" {
