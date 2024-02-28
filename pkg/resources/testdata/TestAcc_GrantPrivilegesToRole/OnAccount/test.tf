efc3f70fefad59019a46618f44c8bb41b58a2f46
Artur Sawicki <artur.sawicki@snowflake.com>
2024-02-28 14:51:10 +0100

    Fix formatting

--- pkg/resources/testdata/TestAcc_GrantPrivilegesToRole/OnAccount/test.tf
+++ pkg/resources/testdata/TestAcc_GrantPrivilegesToRole/OnAccount/test.tf
@@ -1,5 +1,5 @@
 resource "snowflake_grant_privileges_to_role" "test" {
-  role_name = var.name
+  role_name         = var.name
   privileges        = var.privileges
   on_account        = true
   with_grant_option = var.with_grant_option
