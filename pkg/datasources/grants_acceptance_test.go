93907ae70a7736bfd2fd61392a1bcb1fbb5a0ca2
Artur Sawicki <artur.sawicki@snowflake.com>
2024-03-28 17:46:09 +0100

    Prepare list of tests

--- pkg/datasources/grants_acceptance_test.go
+++ pkg/datasources/grants_acceptance_test.go
@@ -9,6 +9,33 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
+// TODO: tests (examples from the correct ones):
+// - on - account
+// - on - db object
+// - on - schema object
+// - on - invalid config - no attribute
+// - on - invalid config - missing object type or name
+// - to - application
+// - to - application role
+// - to - role
+// - to - user
+// - to - share
+// - to - share with application package
+// - to - invalid config - no attribute
+// - to - invalid config - share name missing
+// - of - role
+// - of - application role
+// - of - share
+// - of - invalid config - no attribute
+// - future in - database
+// - future in - schema (both db and sc present)
+// - future in - schema (only sc present)
+// - future in - invalid config - no attribute
+// - future in - invalid config - schema with no schema name
+// - future to - role
+// - future to - database role
+// - future to - invalid config - no attribute
+// - future to - invalid config - database role id invalid
 func TestAcc_Grants(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: acc.TestAccProtoV6ProviderFactories,
