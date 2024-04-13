06e559994d5a7155916bf48c61180218e31666a0
Scott Winkler <scott.winkler@snowflake.com>
2024-04-12 18:41:54 -0700

    randomize name

--- pkg/sdk/testint/application_packages_integration_test.go
+++ pkg/sdk/testint/application_packages_integration_test.go
@@ -208,7 +208,7 @@ func TestInt_ApplicationPackagesVersionAndReleaseDirective(t *testing.T) {
 	createApplicationPackageHandle := func(t *testing.T) *sdk.ApplicationPackage {
 		t.Helper()
 
-		id := sdk.NewAccountObjectIdentifier("snowflake_package_test")
+		id := sdk.NewAccountObjectIdentifier(random.AlphaN(8))
 		request := sdk.NewCreateApplicationPackageRequest(id).WithDistribution(sdk.DistributionPointer(sdk.DistributionInternal))
 		err := client.ApplicationPackages.Create(ctx, request)
 		require.NoError(t, err)
