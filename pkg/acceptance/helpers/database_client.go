3e5e190a2c5e741e9cca0f587b5c473fdc416473
Artur Sawicki <artur.sawicki@snowflake.com>
2024-04-18 10:43:21 +0200

    Changed drop to if exists and add cleanup

--- pkg/acceptance/helpers/database_client.go
+++ pkg/acceptance/helpers/database_client.go
@@ -27,6 +27,11 @@ func (d *DatabaseClient) CreateDatabase(t *testing.T) (*sdk.Database, func()) {
 	return d.CreateDatabaseWithOptions(t, sdk.RandomAccountObjectIdentifier(), &sdk.CreateDatabaseOptions{})
 }
 
+func (d *DatabaseClient) CreateDatabaseWithName(t *testing.T, name string) (*sdk.Database, func()) {
+	t.Helper()
+	return d.CreateDatabaseWithOptions(t, sdk.NewAccountObjectIdentifier(name), &sdk.CreateDatabaseOptions{})
+}
+
 func (d *DatabaseClient) CreateDatabaseWithOptions(t *testing.T, id sdk.AccountObjectIdentifier, opts *sdk.CreateDatabaseOptions) (*sdk.Database, func()) {
 	t.Helper()
 	ctx := context.Background()
@@ -35,7 +40,7 @@ func (d *DatabaseClient) CreateDatabaseWithOptions(t *testing.T, id sdk.AccountO
 	database, err := d.client().ShowByID(ctx, id)
 	require.NoError(t, err)
 	return database, func() {
-		err := d.client().Drop(ctx, id, nil)
+		err := d.client().Drop(ctx, id, &sdk.DropDatabaseOptions{IfExists: sdk.Bool(true)})
 		require.NoError(t, err)
 		err = d.context.client.Sessions.UseSchema(ctx, sdk.NewDatabaseObjectIdentifier(d.context.database, d.context.schema))
 		require.NoError(t, err)
