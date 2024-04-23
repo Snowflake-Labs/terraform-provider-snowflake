db0aedc6ef120ebc82d88f42bb4dec26e3ed95e2
Artur Sawicki <artur.sawicki@snowflake.com>
2024-04-23 16:11:39 +0200

    Copy methods to prepared stage client

--- pkg/acceptance/helpers/stage_client.go
+++ pkg/acceptance/helpers/stage_client.go
@@ -1,7 +1,14 @@
 package helpers
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
+	"github.com/stretchr/testify/require"
 )
 
 type StageClient struct {
@@ -16,4 +23,86 @@ func NewStageClient(context *TestClientContext) *StageClient {
 
 func (c *StageClient) client() sdk.Stages {
 	return c.context.client.Stages
+}
+
+func (c *StageClient) CreateStageWithDirectory(t *testing.T, database *sdk.Database, schema *sdk.Schema, name string) (*sdk.Stage, func()) {
+	t.Helper()
+	id := sdk.NewSchemaObjectIdentifier(database.Name, schema.Name, name)
+	return c.CreateStageWithOptions(t, id, func(request *sdk.CreateInternalStageRequest) *sdk.CreateInternalStageRequest {
+		return request.WithDirectoryTableOptions(sdk.NewInternalDirectoryTableOptionsRequest().WithEnable(sdk.Bool(true)))
+	})
+}
+
+func (c *StageClient) CreateStage(t *testing.T, id sdk.SchemaObjectIdentifier) (*sdk.Stage, func()) {
+	t.Helper()
+	return c.CreateStageWithOptions(t, id, func(request *sdk.CreateInternalStageRequest) *sdk.CreateInternalStageRequest { return request })
+}
+
+func (c *StageClient) CreateStageWithURL(t *testing.T, id sdk.SchemaObjectIdentifier, url string) (*sdk.Stage, func()) {
+	t.Helper()
+	ctx := context.Background()
+	err := c.client().CreateOnS3(ctx, sdk.NewCreateOnS3StageRequest(id).
+		WithExternalStageParams(sdk.NewExternalS3StageParamsRequest(url)))
+	require.NoError(t, err)
+
+	stage, err := c.client().ShowByID(ctx, id)
+	require.NoError(t, err)
+
+	return stage, c.DropStageFunc(t, id)
+}
+
+func (c *StageClient) CreateStageWithOptions(t *testing.T, id sdk.SchemaObjectIdentifier, reqMapping func(*sdk.CreateInternalStageRequest) *sdk.CreateInternalStageRequest) (*sdk.Stage, func()) {
+	t.Helper()
+	ctx := context.Background()
+	err := c.client().CreateInternal(ctx, reqMapping(sdk.NewCreateInternalStageRequest(id)))
+	require.NoError(t, err)
+
+	stage, err := c.client().ShowByID(ctx, id)
+	require.NoError(t, err)
+
+	return stage, c.DropStageFunc(t, id)
+}
+
+func (c *StageClient) DropStageFunc(t *testing.T, id sdk.SchemaObjectIdentifier) func() {
+	t.Helper()
+	ctx := context.Background()
+
+	return func() {
+		err := c.client().Drop(ctx, sdk.NewDropStageRequest(id).WithIfExists(sdk.Bool(true)))
+		require.NoError(t, err)
+	}
+}
+
+func (c *StageClient) PutOnStage(t *testing.T, stage *sdk.Stage, filename string) {
+	t.Helper()
+	ctx := context.Background()
+
+	path, err := filepath.Abs("./testdata/" + filename)
+	require.NoError(t, err)
+	absPath := "file://" + path
+
+	_, err = c.context.client.ExecForTests(ctx, fmt.Sprintf(`PUT '%s' @%s AUTO_COMPRESS = FALSE`, absPath, stage.ID().FullyQualifiedName()))
+	require.NoError(t, err)
+}
+
+func (c *StageClient) PutOnStageWithContent(t *testing.T, id sdk.SchemaObjectIdentifier, filename string, content string) {
+	t.Helper()
+	ctx := context.Background()
+
+	tf := fmt.Sprintf("/tmp/%s", filename)
+	f, err := os.Create(tf)
+	require.NoError(t, err)
+	if content != "" {
+		_, err = f.Write([]byte(content))
+		require.NoError(t, err)
+	}
+	f.Close()
+	defer os.Remove(f.Name())
+
+	_, err = c.context.client.ExecForTests(ctx, fmt.Sprintf(`PUT file://%s @%s AUTO_COMPRESS = FALSE OVERWRITE = TRUE`, f.Name(), id.FullyQualifiedName()))
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		_, err = c.context.client.ExecForTests(ctx, fmt.Sprintf(`REMOVE @%s/%s`, id.FullyQualifiedName(), filename))
+		require.NoError(t, err)
+	})
 }
