7812cc8cf7a6b0eeea9cdba4dfe6d72846411e5a
Artur Sawicki <artur.sawicki@snowflake.com>
2024-04-24 14:46:35 +0200

    Prepare failover group helper methods

--- pkg/acceptance/helpers/failover_group_client.go
+++ pkg/acceptance/helpers/failover_group_client.go
@@ -1,7 +1,11 @@
 package helpers
 
 import (
+	"context"
+	"testing"
+
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
+	"github.com/stretchr/testify/require"
 )
 
 type FailoverGroupClient struct {
@@ -16,4 +20,39 @@ func NewFailoverGroupClient(context *TestClientContext) *FailoverGroupClient {
 
 func (c *FailoverGroupClient) client() sdk.FailoverGroups {
 	return c.context.client.FailoverGroups
+}
+
+func (c *FailoverGroupClient) CreateFailoverGroup(t *testing.T) (*sdk.FailoverGroup, func()) {
+	t.Helper()
+	objectTypes := []sdk.PluralObjectType{sdk.PluralObjectTypeRoles}
+	currentAccount, err := c.context.client.ContextFunctions.CurrentAccount(context.Background())
+	require.NoError(t, err)
+	accountID := sdk.NewAccountIdentifierFromAccountLocator(currentAccount)
+	allowedAccounts := []sdk.AccountIdentifier{accountID}
+	return c.CreateFailoverGroupWithOptions(t, objectTypes, allowedAccounts, nil)
+}
+
+func (c *FailoverGroupClient) CreateFailoverGroupWithOptions(t *testing.T, objectTypes []sdk.PluralObjectType, allowedAccounts []sdk.AccountIdentifier, opts *sdk.CreateFailoverGroupOptions) (*sdk.FailoverGroup, func()) {
+	t.Helper()
+	ctx := context.Background()
+
+	id := sdk.RandomAlphanumericAccountObjectIdentifier()
+
+	err := c.client().Create(ctx, id, objectTypes, allowedAccounts, opts)
+	require.NoError(t, err)
+
+	failoverGroup, err := c.client().ShowByID(ctx, id)
+	require.NoError(t, err)
+
+	return failoverGroup, c.DropFailoverGroupFunc(t, id)
+}
+
+func (c *FailoverGroupClient) DropFailoverGroupFunc(t *testing.T, id sdk.AccountObjectIdentifier) func() {
+	t.Helper()
+	ctx := context.Background()
+
+	return func() {
+		err := c.client().Drop(ctx, id, &sdk.DropFailoverGroupOptions{IfExists: sdk.Bool(true)})
+		require.NoError(t, err)
+	}
 }
