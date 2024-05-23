c29fbf13674dd8441db0da4ff5d309fa0f6e925f
Jan Cieślak <jan.cieslak@snowflake.com>
2024-05-23 09:51:14 +0200

    chore: accept non-pointer values in the generated builder methods (#2816)
    Accept non-pointer values in the generated builder methods. Now the
    nested requests have to be explicitly unreferenced, but two additional
    cases could be added to resolve this "issue". There's only a special
    case for strings which shouldn't be set whenever they're optional and
    the input value is equal to an empty string, because:
    1. that would be a rare case to have an empty string in Snowflake.
    2. empty strings are treated as non-set values in Terraform.
    
    The improved generator was tested and used as an example on the external
    table.

--- pkg/sdk/dto-builder-generator/main.go
+++ pkg/sdk/dto-builder-generator/main.go
@@ -204,8 +204,15 @@ func (gen *Generator) generateBuilderMethods(d *structDef) {
 	}
 
 	for _, field := range optionalFields {
-		gen.printf("func (s *%s) With%s(%s %s) *%s {\n", d.name, toTitle(field.name), field.name, field.typeString, d.name)
-		gen.printf("s.%s = %s\n", field.name, field.name)
+		gen.printf("func (s *%s) With%s(%s %s) *%s {\n", d.name, toTitle(field.name), field.name, strings.TrimLeft(field.typeString, "*"), d.name)
+
+		switch {
+		case strings.HasPrefix(field.typeString, "*"):
+			// If the target field is a pointer, assign the address of input field because right now we always pass them by value
+			gen.printf("s.%s = &%s\n", field.name, field.name)
+		default:
+			gen.printf("s.%s = %s\n", field.name, field.name)
+		}
 		gen.printf("return s\n")
 		gen.printf("}\n\n")
 	}
