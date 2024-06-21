7057a14a1f9a72cb2b06f77afb563a538e99c57e
Artur Sawicki <artur.sawicki@snowflake.com>
2024-06-21 13:34:54 +0200

    Remove from snowflake package

--- pkg/snowflake/escaping.go
+++ pkg/snowflake/escaping.go
@@ -2,7 +2,6 @@ package snowflake
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 )
 
@@ -11,13 +10,6 @@ import (
 func EscapeString(in string) string {
 	out := strings.ReplaceAll(in, `\`, `\\`)
 	out = strings.ReplaceAll(out, `'`, `\'`)
-	return out
-}
-
-// UnescapeString reverses EscapeString.
-func UnescapeString(in string) string {
-	out := strings.ReplaceAll(in, `\\`, `\`)
-	out = strings.ReplaceAll(out, `\'`, `'`)
 	return out
 }
 
@@ -33,20 +25,4 @@ func UnescapeSnowflakeString(in string) string {
 	out = strings.TrimSuffix(out, `'`)
 	out = strings.ReplaceAll(out, `''`, `'`)
 	return out
-}
-
-// AddressEscape wraps a name inside double quotes only if required by Snowflake.
-func AddressEscape(in ...string) string {
-	quoteCheck := regexp.MustCompile(`[^A-Z0-9_]`)
-	address := make([]string, len(in))
-
-	for i, n := range in {
-		if quoteCheck.MatchString(n) {
-			address[i] = fmt.Sprintf(`"%s"`, strings.ReplaceAll(n, `"`, `\"`))
-		} else {
-			address[i] = n
-		}
-	}
-
-	return strings.Join(address, ".")
 }
