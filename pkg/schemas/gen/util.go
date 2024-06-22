47dffcde65a9577e8aa3d9d9d6f4d8b855d58001
Artur Sawicki <artur.sawicki@snowflake.com>
2024-06-22 12:22:23 +0200

    Test column output

--- pkg/schemas/gen/util.go
+++ pkg/schemas/gen/util.go
@@ -8,18 +8,20 @@ import (
 var splitOnTheWordsBeginnings = regexp.MustCompile(`(.)([A-Z][a-z]+)`)
 var splitRemainingWordBreaks = regexp.MustCompile("([a-z0-9])([A-Z]+)")
 
+// TODO: describe
 func ToSnakeCase(str string) string {
 	wordsSplit := splitOnTheWordsBeginnings.ReplaceAllString(str, "${1}_${2}")
 	return strings.ToLower(splitRemainingWordBreaks.ReplaceAllString(wordsSplit, "${1}_${2}"))
 }
 
-// TODO: test and describe
+// TODO: describe
 func ColumnOutput(columnWidth int, columns ...string) string {
 	var sb strings.Builder
 	for i, column := range columns {
 		sb.WriteString(column)
 		if i != len(columns)-1 {
-			sb.WriteString(strings.Repeat(" ", columnWidth-len(column)))
+			spaces := max(columnWidth-len(column), 1)
+			sb.WriteString(strings.Repeat(" ", spaces))
 		}
 	}
 	return sb.String()
