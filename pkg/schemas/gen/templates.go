585c48f1a919bdac8f585d99c7a36ac8d149c50f
Artur Sawicki <artur.sawicki@snowflake.com>
2024-06-23 19:11:09 +0200

    Change comment and remove unused

--- pkg/schemas/gen/templates.go
+++ pkg/schemas/gen/templates.go
@@ -16,16 +16,12 @@ var (
 	//go:embed templates/schema.tmpl
 	schemaTemplateContent string
 	SchemaTemplate, _     = template.New("schemaTemplate").Funcs(template.FuncMap{
-		"uppercase":            func(in string) string { return strings.ToUpper(in) },
-		"lowercase":            func(in string) string { return strings.ToLower(in) },
 		"firstLetterLowercase": func(in string) string { return strings.ToLower(in[:1]) + in[1:] },
 	}).Parse(schemaTemplateContent)
 
 	//go:embed templates/to_schema_mapper.tmpl
 	toSchemaMapperTemplateContent string
 	ToSchemaMapperTemplate, _     = template.New("toSchemaMapperTemplate").Funcs(template.FuncMap{
-		"uppercase":            func(in string) string { return strings.ToUpper(in) },
-		"lowercase":            func(in string) string { return strings.ToLower(in) },
 		"firstLetterLowercase": func(in string) string { return strings.ToLower(in[:1]) + in[1:] },
 		"runMapper":            func(mapper Mapper, in ...string) string { return mapper(strings.Join(in, "")) },
 	}).Parse(toSchemaMapperTemplateContent)
