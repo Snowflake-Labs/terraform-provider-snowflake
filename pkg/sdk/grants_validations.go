8e4e7535f2814da4bf7326c66a3e581e064bdc51
Artur Sawicki <artur.sawicki@snowflake.com>
2024-07-02 12:36:59 +0200

    Add tests for cortex grants

--- pkg/sdk/grants_validations.go
+++ pkg/sdk/grants_validations.go
@@ -118,6 +118,10 @@ var invalidGrantToFutureObjectTypes = []ObjectType{
 	ObjectTypeStreamlit, // added because of https://github.com/Snowflake-Labs/terraform-provider-snowflake/issues/2656
 }
 
+var invalidGrantPluralObjectTypes = []ObjectType{
+	ObjectTypeCortexSearchService, // added because it is not working currently
+}
+
 var (
 	ValidGrantOwnershipObjectTypesString       = make([]string, len(validGrantOwnershipObjectTypes))
 	ValidGrantOwnershipPluralObjectTypesString = make([]string, len(validGrantOwnershipObjectTypes))
@@ -134,7 +138,7 @@ func init() {
 	for i, objectType := range validGrantToObjectTypes {
 		ValidGrantToObjectTypesString[i] = objectType.String()
 		ValidGrantToPluralObjectTypesString[i] = objectType.Plural().String()
-		if !slices.Contains(invalidGrantToFutureObjectTypes, objectType) {
+		if !slices.Contains(invalidGrantToFutureObjectTypes, objectType) && !slices.Contains(invalidGrantPluralObjectTypes, objectType) {
 			ValidGrantToFuturePluralObjectTypesString = append(ValidGrantToFuturePluralObjectTypesString, objectType.Plural().String())
 		}
 	}
--- pkg/sdk/testint/grants_integration_test.go
+++ pkg/sdk/testint/grants_integration_test.go
@@ -195,6 +195,68 @@ func TestInt_GrantAndRevokePrivilegesToAccountRole(t *testing.T) {
 		assert.Equal(t, 0, len(grants))
 	})
 
+	t.Run("on schema object: cortex search service", func(t *testing.T) {
+		roleTest, roleCleanup := testClientHelper().Role.CreateRole(t)
+		t.Cleanup(roleCleanup)
+		table, tableTestCleanup := testClientHelper().Table.CreateTableWithPredefinedColumns(t)
+		t.Cleanup(tableTestCleanup)
+		cortex, cortexCleanup := testClientHelper().CortexSearchService.CreateCortexSearchService(t, table.ID())
+		t.Cleanup(cortexCleanup)
+
+		privileges := &sdk.AccountRoleGrantPrivileges{
+			SchemaObjectPrivileges: []sdk.SchemaObjectPrivilege{sdk.SchemaObjectPrivilegeUsage},
+		}
+		on := &sdk.AccountRoleGrantOn{
+			SchemaObject: &sdk.GrantOnSchemaObject{
+				SchemaObject: &sdk.Object{
+					ObjectType: sdk.ObjectTypeCortexSearchService,
+					Name:       cortex.ID(),
+				},
+			},
+		}
+		err := client.Grants.GrantPrivilegesToAccountRole(ctx, privileges, on, roleTest.ID(), nil)
+		require.NoError(t, err)
+		grants, err := client.Grants.Show(ctx, &sdk.ShowGrantOptions{
+			To: &sdk.ShowGrantsTo{
+				Role: roleTest.ID(),
+			},
+		})
+		require.NoError(t, err)
+		selectPrivilege, err := collections.FindOne[sdk.Grant](grants, func(g sdk.Grant) bool { return g.Privilege == sdk.SchemaObjectPrivilegeUsage.String() })
+		require.NoError(t, err)
+		assert.Equal(t, cortex.ID().FullyQualifiedName(), selectPrivilege.Name.FullyQualifiedName())
+
+		// now revoke and verify that the grant(s) are gone
+		err = client.Grants.RevokePrivilegesFromAccountRole(ctx, privileges, on, roleTest.ID(), nil)
+		require.NoError(t, err)
+		grants, err = client.Grants.Show(ctx, &sdk.ShowGrantOptions{
+			To: &sdk.ShowGrantsTo{
+				Role: roleTest.ID(),
+			},
+		})
+		require.NoError(t, err)
+		assert.Equal(t, 0, len(grants))
+	})
+
+	t.Run("on schema object: cortex search service", func(t *testing.T) {
+		roleTest, roleCleanup := testClientHelper().Role.CreateRole(t)
+		t.Cleanup(roleCleanup)
+
+		privileges := &sdk.AccountRoleGrantPrivileges{
+			SchemaObjectPrivileges: []sdk.SchemaObjectPrivilege{sdk.SchemaObjectPrivilegeUsage},
+		}
+		on := &sdk.AccountRoleGrantOn{
+			SchemaObject: &sdk.GrantOnSchemaObject{
+				All: &sdk.GrantOnSchemaObjectIn{
+					PluralObjectType: sdk.PluralObjectTypeCortexSearchServices,
+					InSchema:         sdk.Pointer(testSchema(t).ID()),
+				},
+			},
+		}
+		err := client.Grants.GrantPrivilegesToAccountRole(ctx, privileges, on, roleTest.ID(), nil)
+		require.ErrorContains(t, err, "unexpected 'SEARCH'")
+	})
+
 	t.Run("on future schema object", func(t *testing.T) {
 		roleTest, roleCleanup := testClientHelper().Role.CreateRole(t)
 		t.Cleanup(roleCleanup)
@@ -522,6 +584,59 @@ func TestInt_GrantAndRevokePrivilegesToDatabaseRole(t *testing.T) {
 		err = client.Grants.RevokePrivilegesFromDatabaseRole(ctx, privileges, on, databaseRoleId, nil)
 		require.NoError(t, err)
 
+		returnedGrants, err = client.Grants.Show(ctx, &sdk.ShowGrantOptions{
+			To: &sdk.ShowGrantsTo{
+				DatabaseRole: databaseRoleId,
+			},
+		})
+		require.NoError(t, err)
+		assert.Equal(t, 1, len(returnedGrants))
+		assert.Equal(t, sdk.AccountObjectPrivilegeUsage.String(), returnedGrants[0].Privilege)
+	})
+
+	t.Run("on schema object: cortex search service", func(t *testing.T) {
+		databaseRole, databaseRoleCleanup := testClientHelper().DatabaseRole.CreateDatabaseRole(t)
+		t.Cleanup(databaseRoleCleanup)
+		databaseRoleId := testClientHelper().Ids.NewDatabaseObjectIdentifier(databaseRole.Name)
+		table, tableTestCleanup := testClientHelper().Table.CreateTableWithPredefinedColumns(t)
+		t.Cleanup(tableTestCleanup)
+		cortex, cortexCleanup := testClientHelper().CortexSearchService.CreateCortexSearchService(t, table.ID())
+		t.Cleanup(cortexCleanup)
+
+		privileges := &sdk.DatabaseRoleGrantPrivileges{
+			SchemaObjectPrivileges: []sdk.SchemaObjectPrivilege{sdk.SchemaObjectPrivilegeUsage},
+		}
+		on := &sdk.DatabaseRoleGrantOn{
+			SchemaObject: &sdk.GrantOnSchemaObject{
+				SchemaObject: &sdk.Object{
+					ObjectType: sdk.ObjectTypeCortexSearchService,
+					Name:       cortex.ID(),
+				},
+			},
+		}
+		err := client.Grants.GrantPrivilegesToDatabaseRole(ctx, privileges, on, databaseRoleId, nil)
+		require.NoError(t, err)
+		returnedGrants, err := client.Grants.Show(ctx, &sdk.ShowGrantOptions{
+			To: &sdk.ShowGrantsTo{
+				DatabaseRole: databaseRoleId,
+			},
+		})
+		require.NoError(t, err)
+		// Expecting two grants because database role has usage on database by default
+		require.LessOrEqual(t, 2, len(returnedGrants))
+		usagePrivilege, err := collections.FindOne[sdk.Grant](returnedGrants, func(g sdk.Grant) bool { return g.GrantedOn == sdk.ObjectTypeDatabase })
+		require.NoError(t, err)
+		assert.Equal(t, sdk.ObjectTypeDatabaseRole, usagePrivilege.GrantedTo)
+
+		selectPrivilege, err := collections.FindOne[sdk.Grant](returnedGrants, func(g sdk.Grant) bool { return g.GrantedOn == sdk.ObjectTypeCortexSearchService })
+		require.NoError(t, err)
+		assert.Equal(t, sdk.SchemaObjectPrivilegeUsage.String(), selectPrivilege.Privilege)
+		assert.Equal(t, sdk.ObjectTypeDatabaseRole, selectPrivilege.GrantedTo)
+		assert.Equal(t, cortex.ID().FullyQualifiedName(), selectPrivilege.Name.FullyQualifiedName())
+
+		// now revoke and verify that the new grant is gone
+		err = client.Grants.RevokePrivilegesFromDatabaseRole(ctx, privileges, on, databaseRoleId, nil)
+		require.NoError(t, err)
 		returnedGrants, err = client.Grants.Show(ctx, &sdk.ShowGrantOptions{
 			To: &sdk.ShowGrantsTo{
 				DatabaseRole: databaseRoleId,
@@ -1147,6 +1262,25 @@ func TestInt_GrantOwnership(t *testing.T) {
 		pipeExecutionState, err = client.SystemFunctions.PipeStatus(pipe.ID())
 		require.NoError(t, err)
 		require.Equal(t, sdk.PausedPipeExecutionState, pipeExecutionState)
+	})
+
+	t.Run("on cortex - with ownership", func(t *testing.T) {
+		role, roleCleanup := testClientHelper().Role.CreateRole(t)
+		t.Cleanup(roleCleanup)
+		table, tableTestCleanup := testClientHelper().Table.CreateTableWithPredefinedColumns(t)
+		t.Cleanup(tableTestCleanup)
+		cortex, cortexCleanup := testClientHelper().CortexSearchService.CreateCortexSearchService(t, table.ID())
+		t.Cleanup(cortexCleanup)
+
+		err := client.Grants.GrantOwnership(
+			ctx,
+			ownershipGrantOnObject(sdk.ObjectTypeCortexSearchService, cortex.ID()),
+			sdk.OwnershipGrantTo{
+				AccountRoleName: sdk.Pointer(role.ID()),
+			},
+			new(sdk.GrantOwnershipOptions),
+		)
+		require.ErrorContains(t, err, "Invalid object type 'CORTEX_SEARCH_SERVICE' for privilege 'OWNERSHIP'")
 	})
 
 	t.Run("on pipe - with operate and monitor privileges granted", func(t *testing.T) {
