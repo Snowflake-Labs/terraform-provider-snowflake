4e50d808ee51c1b08dfbd700ab0877121e850c2e
Artur Sawicki <artur.sawicki@snowflake.com>
2024-07-21 21:11:24 +0200

    Add assertion through method

--- pkg/acceptance/bettertestspoc/gen/model.go
+++ pkg/acceptance/bettertestspoc/gen/model.go
@@ -1,16 +1,23 @@
 package gen
 
 import (
+	"os"
 	"strings"
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/gencommons"
 )
+
+// TODO: extract to commons?
+type PreambleModel struct {
+	PackageName string
+}
 
 type SnowflakeObjectAssertionsModel struct {
 	Name    string
 	SdkType string
 	IdType  string
 	Fields  []SnowflakeObjectFieldAssertion
+	PreambleModel
 }
 
 func (m SnowflakeObjectAssertionsModel) SomeFunc() {
@@ -30,11 +37,15 @@ func ModelFromSdkObjectDetails(sdkObject gencommons.SdkObjectDetails) SnowflakeO
 		fields[idx] = MapToSnowflakeObjectFieldAssertion(field)
 	}
 
+	packageWithGenerateDirective := os.Getenv("GOPACKAGE")
 	return SnowflakeObjectAssertionsModel{
 		Name:    name,
 		SdkType: sdkObject.Name,
 		IdType:  sdkObject.IdType,
 		Fields:  fields,
+		PreambleModel: PreambleModel{
+			PackageName: packageWithGenerateDirective,
+		},
 	}
 }
 
