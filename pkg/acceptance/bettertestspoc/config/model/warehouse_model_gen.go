8f2b5d9a71fcc72e42a3c186a6113978741f85cc
Artur Sawicki <artur.sawicki@snowflake.com>
2024-07-23 22:03:45 +0200

    Remove unnecessary sdk import

--- pkg/acceptance/bettertestspoc/config/model/warehouse_model_gen.go
+++ pkg/acceptance/bettertestspoc/config/model/warehouse_model_gen.go
@@ -7,11 +7,7 @@ import (
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/resources"
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 )
-
-// to ensure sdk package is used
-var _ = sdk.Object{}
 
 type WarehouseModel struct {
 	AutoResume                      tfconfig.Variable `json:"auto_resume,omitempty"`
