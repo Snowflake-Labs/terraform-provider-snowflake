f70e40e82a7f22e86276ab55ddf2584882229ccd
Jakub Michalak <jakub.michalak@snowflake.com>
2024-07-24 14:16:17 +0200

    feat: Rework schema datasource (#2954)
    <!-- Feel free to delete comments as you fill this in -->
    Rework schema datasource:
    - add acceptance tests
    - add entry to the migration guide
    <!-- summary of changes -->
    
    ## Test Plan
    <!-- detail ways in which this PR has been tested or needs to be tested
    -->
    * [x] acceptance tests
    
    ## References
    <!-- issues documentation links, etc  -->
    https://docs.snowflake.com/en/sql-reference/sql/show-schemas

---
+++ pkg/datasources/testdata/TestAcc_Schemas/limit/variables.tf
@@ -0,0 +1,23 @@
+variable "name_1" {
+  type = string
+}
+
+variable "name_2" {
+  type = string
+}
+
+variable "name_3" {
+  type = string
+}
+
+variable "database" {
+  type = string
+}
+
+variable "rows" {
+  type = number
+}
+
+variable "from" {
+  type = string
+}
