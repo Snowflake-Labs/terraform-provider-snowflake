10a3e7b993ba1fdae16f4694fc2d8846b68312ad
Artur Sawicki <artur.sawicki@snowflake.com>
2024-09-02 11:50:49 +0200

    Pass really basic test

--- pkg/datasources/users_acceptance_test.go
+++ pkg/datasources/users_acceptance_test.go
@@ -1,56 +1,73 @@
 package datasources_test
 
 import (
-	"fmt"
 	"testing"
 
 	acc "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance"
 
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config/model"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/helpers/random"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testenvs"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/resources"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
-func TestAcc_Users(t *testing.T) {
-	userName := acc.TestClient().Ids.Alpha()
+func TestAcc_Users_Complete(t *testing.T) {
+	_ = testenvs.GetOrSkipTest(t, testenvs.ConfigureClientOnce)
+	id := acc.TestClient().Ids.RandomAccountObjectIdentifier()
+
+	comment := random.Comment()
+	pass := random.Password()
+	key1, _ := random.GenerateRSAPublicKey(t)
+	key2, _ := random.GenerateRSAPublicKey(t)
+
+	userModelAllAttributes := model.User("u", id.Name()).
+		WithPassword(pass).
+		WithLoginName(id.Name() + "_login").
+		WithDisplayName("Display Name").
+		WithFirstName("Jan").
+		WithMiddleName("Jakub").
+		WithLastName("Testowski").
+		WithEmail("fake@email.com").
+		WithMustChangePassword("true").
+		WithDisabled("false").
+		WithDaysToExpiry(8).
+		WithMinsToUnlock(9).
+		WithDefaultWarehouse("some_warehouse").
+		WithDefaultNamespace("some.namespace").
+		WithDefaultRole("some_role").
+		WithDefaultSecondaryRolesStringList("ALL").
+		WithMinsToBypassMfa(10).
+		WithRsaPublicKey(key1).
+		WithRsaPublicKey2(key2).
+		WithComment(comment).
+		WithDisableMfa("true")
+
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: acc.TestAccProtoV6ProviderFactories,
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
 			tfversion.RequireAbove(tfversion.Version1_5_0),
 		},
+		CheckDestroy: acc.CheckDestroy(t, resources.User),
 		Steps: []resource.TestStep{
 			{
-				Config: users(userName),
+				Config: config.FromModel(t, userModelAllAttributes) + datasourceWithLike(),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.snowflake_users.u", "users.#"),
-					resource.TestCheckResourceAttr("data.snowflake_users.u", "users.#", "1"),
-					resource.TestCheckResourceAttr("data.snowflake_users.u", "users.0.name", userName),
-					resource.TestCheckResourceAttr("data.snowflake_users.u", "users.0.disabled", "false"),
+					resource.TestCheckResourceAttr("data.snowflake_users.test", "users.#", "1"),
+
+					resource.TestCheckResourceAttr("data.snowflake_users.test", "users.0.show_output.0.name", id.Name()),
 				),
 			},
 		},
 	})
 }
 
-func users(userName string) string {
-	return fmt.Sprintf(`
-	resource "snowflake_user" "u" {
-		name = "%s"
-		comment = "test comment"
-		login_name = "%s_login"
-		display_name = "Display Name"
-		first_name = "Alex"
-		last_name = "Kita"
-		email = "fake@email.com"
-		disabled = false
-		default_warehouse="foo"
-		default_role="FOO"
-		default_secondary_roles = ["ALL"]
-		default_namespace="FOO"
+func datasourceWithLike() string {
+	return `
+	data "snowflake_users" "test" {
+		like = snowflake_user.u.name
 	}
-
-	data snowflake_users "u" {
-		pattern = "%s"
-		depends_on = [snowflake_user.u]
-	}
-	`, userName, userName, userName)
+`
 }
