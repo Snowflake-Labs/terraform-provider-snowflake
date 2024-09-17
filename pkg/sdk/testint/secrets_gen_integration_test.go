aa645112a119676b5ab9ccc23c8f4f614717210f
Filip Budzyński <filip.budzynski@snowflake.com>
2024-09-17 11:03:55 +0200

    init for integration tests

--- pkg/sdk/testint/secrets_gen_integration_test.go
+++ pkg/sdk/testint/secrets_gen_integration_test.go
@@ -13,23 +13,23 @@ func TestInt_Secrets(t *testing.T) {
 	client := testClient(t)
 	ctx := testContext(t)
 
-	cleanupSecret := func(id sdk.SchemaObjectIdentifier) {
-		t.Cleanup(func() {
+	cleanupSecret := func(id sdk.SchemaObjectIdentifier) func() {
+		return func() {
 			err := client.Secrets.Drop(ctx, sdk.NewDropSecretRequest(id).WithIfExists(true))
-			assert.NoError(t, err)
-		})
+			require.NoError(t, err)
+		}
 	}
 
-	assertSecret := func(t *testing.T, s *sdk.Secret, id sdk.SchemaObjectIdentifier, secretType, comment string) {
+	assertSecret := func(t *testing.T, s *sdk.Secret, expectedId sdk.SchemaObjectIdentifier, expectedSecretType, expectedComment string) {
 		t.Helper()
+		assert.Equal(t, expectedId.Name(), s.Name)
+		assert.Equal(t, expectedSecretType, s.SecretType)
+		assert.Equal(t, expectedComment, s.Comment)
 		assert.NotEmpty(t, s.CreatedOn)
 		assert.NotEmpty(t, s.DatabaseName)
 		assert.NotEmpty(t, s.SchemaName)
 		assert.NotEmpty(t, s.OwnerRoleType)
 		assert.NotEmpty(t, s.Owner)
-		assert.Equal(t, id.Name(), s.Name)
-		assert.Equal(t, secretType, s.SecretType)
-		assert.Equal(t, comment, s.Comment)
 	}
 
 	type secretDetails struct {
@@ -56,18 +56,21 @@ func TestInt_Secrets(t *testing.T) {
 	_ = assertBasicAuth
 
 	/*
-		createOAuthClientCredentialsFlowSecret := func(id sdk.SchemaObjectIdentifier, si sdk.CreateApiAuthenticationWithClientCredentialsFlowSecurityIntegrationRequest) {
-			//TODO
-		}
+			createOAuthClientCredentialsFlowSecret := func(id sdk.SchemaObjectIdentifier, si sdk.CreateApiAuthenticationWithClientCredentialsFlowSecurityIntegrationRequest) {
+				//TODO
+			}
 
-		createBasicAuthenticationSecret := func(t *testing.T, id sdk.SchemaObjectIdentifier, username, password string) *sdk.Secret {
+		createBasicAuthenticationSecret := func(t *testing.T, username, password string) *sdk.Secret {
 			t.Helper()
+			id := testClientHelper().Ids.RandomSchemaObjectIdentifier()
 
 			err := client.Secrets.CreateWithBasicAuthentication(ctx, sdk.NewCreateWithBasicAuthenticationSecretRequest(id, username, password))
 			require.NoError(t, err)
-			cleanupSecret(id)
+			t.Cleanup(cleanupSecret(id))
 
 			secret, err := client.Secrets.ShowByID(ctx, id)
+			require.NoError(t, err)
+
 			return secret
 		}
 	*/
@@ -83,31 +86,32 @@ func TestInt_Secrets(t *testing.T) {
 	t.Run("Create With Basic Authentication", func(t *testing.T) {
 		id := testClientHelper().Ids.RandomSchemaObjectIdentifier()
 		comment := random.Comment()
-
 		request := sdk.NewCreateWithBasicAuthenticationSecretRequest(id, "foo", "foo").WithComment(comment).WithIfNotExists(true)
-		// todo: secrets = nil
+
 		err := client.Secrets.CreateWithBasicAuthentication(ctx, request)
 		require.NoError(t, err)
-		cleanupSecret(id)
+		t.Cleanup(cleanupSecret(id))
 
 		secret, err := client.Secrets.ShowByID(ctx, id)
 		require.NoError(t, err)
-		assertSecret(t, secret, id, "PASSWORD", comment)
-		/*
-			details, err := client.Secrets.Describe(ctx, id)
 
-			assertBasicAuth(details, secretDetails{
-				Name:                        id.FullyQualifiedName(),
-				Comment:                     sql.NullString{String: comment},
-				SecretType:                  "PASSWORD",
-				Username:                    sql.NullString{String: "foo"},
-				OauthAccessTokenExpiryTime:  sql.NullString{},
-				OauthRefreshTokenExpiryTime: sql.NullString{},
-				OauthScopes:                 sql.NullString{},
-				IntegrationName:             sql.NullString{},
-			})
-		*/
+		assertSecret(t, secret, id, "PASSWORD", comment)
 	})
+
+	/*
+		details, err := client.Secrets.Describe(ctx, id)
+
+		assertBasicAuth(details, secretDetails{
+			Name:                        id.FullyQualifiedName(),
+			Comment:                     sql.NullString{String: comment},
+			SecretType:                  "PASSWORD",
+			Username:                    sql.NullString{String: "foo"},
+			OauthAccessTokenExpiryTime:  sql.NullString{},
+			OauthRefreshTokenExpiryTime: sql.NullString{},
+			OauthScopes:                 sql.NullString{},
+			IntegrationName:             sql.NullString{},
+		})
+	*/
 
 	t.Run("CreateWithGenericString", func(t *testing.T) {
 		// TODO: fill me
