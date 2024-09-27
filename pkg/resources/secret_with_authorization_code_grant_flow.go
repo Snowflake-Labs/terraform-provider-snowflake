46e2181e946531c060b50b5ee1240138adf18ae9
Filip Budzyński <filip.budzynski@snowflake.com>
2024-09-27 10:54:23 +0200

    Secret with authorization code acceptance test, diff suppress

--- pkg/resources/secret_with_authorization_code_grant_flow.go
+++ pkg/resources/secret_with_authorization_code_grant_flow.go
@@ -6,64 +6,35 @@ import (
 	"fmt"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/helpers"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/provider"
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/schemas"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"time"
 )
 
-var secretAuthorizationCodeSchema = map[string]*schema.Schema{
-	"name": {
-		Type:             schema.TypeString,
-		Required:         true,
-		Description:      blocklistedCharactersFieldDescription("String that specifies the identifier (i.e. name) for the secret, must be unique in your schema."),
-		DiffSuppressFunc: suppressIdentifierQuoting,
-	},
-	"database": {
-		Type:             schema.TypeString,
-		Required:         true,
-		Description:      blocklistedCharactersFieldDescription("The database in which to create the secret"),
-		ForceNew:         true,
-		DiffSuppressFunc: suppressIdentifierQuoting,
-	},
-	"schema": {
-		Type:             schema.TypeString,
-		Required:         true,
-		Description:      blocklistedCharactersFieldDescription("The schema in which to create the secret."),
-		ForceNew:         true,
-		DiffSuppressFunc: suppressIdentifierQuoting,
-	},
-	"oauth_refresh_token": {
-		Type:        schema.TypeString,
-		Required:    true,
-		Description: "Specifies the token as a string that is used to obtain a new access token from the OAuth authorization server when the access token expires.",
-	},
-	"oauth_refresh_token_expiry_time": {
-		Type:        schema.TypeString,
-		Required:    true,
-		Description: "Specifies the timestamp as a string when the OAuth refresh token expires. Accepted formats: YYYY-MM-DD, YYYY-MM-DD HH:MI:SS",
-	},
-	"api_authentication": {
-		Type:             schema.TypeString,
-		ValidateDiagFunc: IsValidIdentifier[sdk.AccountObjectIdentifier](),
-		Required:         true,
-		Description:      "Specifies the name value of the Snowflake security integration that connects Snowflake to an external service when setting Type to OAUTH2.",
-	},
-	"comment": {
-		Type:        schema.TypeString,
-		Optional:    true,
-		Description: "Specifies a comment for the secret.",
-	},
-	ShowOutputAttributeName: {
-		Type:        schema.TypeList,
-		Computed:    true,
-		Description: "Outputs the result of `SHOW SECRET` for the given secret.",
-		Elem: &schema.Resource{
-			Schema: schemas.ShowSecretSchema,
+var secretAuthorizationCodeSchema = func() map[string]*schema.Schema {
+	secretAuthorizationCode := map[string]*schema.Schema{
+		"oauth_refresh_token": {
+			Type:        schema.TypeString,
+			Required:    true,
+			Description: "Specifies the token as a string that is used to obtain a new access token from the OAuth authorization server when the access token expires.",
 		},
-	},
-	FullyQualifiedNameAttributeName: schemas.FullyQualifiedNameSchema,
-}
+		"oauth_refresh_token_expiry_time": {
+			Type:        schema.TypeString,
+			Required:    true,
+			Description: "Specifies the timestamp as a string when the OAuth refresh token expires. Accepted formats: YYYY-MM-DD, YYYY-MM-DD HH:MI:SS",
+			// has to be ignored since snowflake changes the date format to different one than is required in the input
+			//DiffSuppressFunc: IgnoreAfterCreation,
+		},
+		"api_authentication": {
+			Type:             schema.TypeString,
+			ValidateDiagFunc: IsValidIdentifier[sdk.AccountObjectIdentifier](),
+			Required:         true,
+			Description:      "Specifies the name value of the Snowflake security integration that connects Snowflake to an external service when setting Type to OAUTH2.",
+		},
+	}
+	return helpers.MergeMaps(secretCommonSchema, secretAuthorizationCode)
+}()
 
 func SecretWithAuthorizationCode() *schema.Resource {
 	return &schema.Resource{
@@ -81,11 +52,9 @@ func SecretWithAuthorizationCode() *schema.Resource {
 
 func CreateContextSecretWithAuthorizationCode(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*provider.Context).Client
-	databaseName := d.Get("database").(string)
-	schemaName := d.Get("schema").(string)
-	name := d.Get("name").(string)
+	commonCreate := handleSecretCreate(d)
 
-	id := sdk.NewSchemaObjectIdentifier(databaseName, schemaName, name)
+	id := sdk.NewSchemaObjectIdentifier(commonCreate.database, commonCreate.schema, commonCreate.name)
 
 	apiIntegrationString := d.Get("api_authentication").(string)
 	apiIntegration, err := sdk.ParseAccountObjectIdentifier(apiIntegrationString)
@@ -138,26 +107,17 @@ func ReadContextSecretWithAuthorizationCode(ctx context.Context, d *schema.Resou
 		}
 	}
 
+	if err := handleSecretRead(d, id, secret); err != nil {
+		return diag.FromErr(err)
+	}
 	secretDescription, err := client.Secrets.Describe(ctx, id)
 	if err != nil {
 		return diag.FromErr(err)
 	}
-	if err = d.Set("name", secretDescription.Name); err != nil {
-		return diag.FromErr(err)
-	}
-	if err = d.Set("database", secretDescription.DatabaseName); err != nil {
-		return diag.FromErr(err)
-	}
-	if err = d.Set("schema", secretDescription.SchemaName); err != nil {
-		return diag.FromErr(err)
-	}
-	if err = d.Set("oauth_refresh_token_expiry_time", secretDescription.OauthRefreshTokenExpiryTime); err != nil {
+	if err = d.Set("oauth_refresh_token_expiry_time", secretDescription.OauthRefreshTokenExpiryTime.String()); err != nil {
 		return diag.FromErr(err)
 	}
 	if err = d.Set("api_authentication", secretDescription.IntegrationName); err != nil {
-		return diag.FromErr(err)
-	}
-	if err = d.Set("comment", secretDescription.Comment); err != nil {
 		return diag.FromErr(err)
 	}
 	return nil
@@ -168,6 +128,12 @@ func UpdateContextSecretWithAuthorizationCode(ctx context.Context, d *schema.Res
 	id, err := sdk.ParseSchemaObjectIdentifier(d.Id())
 	if err != nil {
 		return diag.FromErr(err)
+	}
+
+	if request := handleSecretUpdate(id, d); request != nil {
+		if err := client.Secrets.Alter(ctx, request); err != nil {
+			return diag.FromErr(err)
+		}
 	}
 
 	if d.HasChange("oauth_refresh_token") {
@@ -183,27 +149,12 @@ func UpdateContextSecretWithAuthorizationCode(ctx context.Context, d *schema.Res
 	}
 
 	if d.HasChange("oauth_refresh_token_expiry_time") {
-		refreshTokenExpiryTime := d.Get("oauth_refresh_token_expiry_time").(string)
+		refreshTokenExpiryTime := d.Get("oauth_refresh_token_expiry_time").(time.Time)
 
 		request := sdk.NewAlterSecretRequest(id)
-		setRequest := sdk.NewSetForOAuthAuthorizationFlowRequest().WithOauthRefreshTokenExpiryTime(refreshTokenExpiryTime)
+		setRequest := sdk.NewSetForOAuthAuthorizationFlowRequest().WithOauthRefreshTokenExpiryTime(refreshTokenExpiryTime.Format(time.DateOnly))
 		request.WithSet(*sdk.NewSecretSetRequest().WithSetForOAuthAuthorizationFlow(*setRequest))
 
-		if err := client.Secrets.Alter(ctx, request); err != nil {
-			return diag.FromErr(err)
-		}
-	}
-
-	if d.HasChange("comment") {
-		comment := d.Get("comment").(string)
-		request := sdk.NewAlterSecretRequest(id)
-		if len(comment) == 0 {
-			unsetRequest := sdk.NewSecretUnsetRequest().WithComment(*sdk.Bool(true))
-			request.WithUnset(*unsetRequest)
-		} else {
-			setRequest := sdk.NewSecretSetRequest().WithComment(comment)
-			request.WithSet(*setRequest)
-		}
 		if err := client.Secrets.Alter(ctx, request); err != nil {
 			return diag.FromErr(err)
 		}
