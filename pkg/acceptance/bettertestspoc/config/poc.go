c28b622c931c1628a24ab8108f4633da6f703415
Artur Sawicki <artur.sawicki@snowflake.com>
2024-11-17 22:29:30 +0100

    Generate provider config

--- pkg/acceptance/bettertestspoc/config/poc.go
+++ pkg/acceptance/bettertestspoc/config/poc.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -31,29 +30,12 @@ type ProviderModelMeta struct {
 	alias string
 }
 
-// SnowflakeProviderModel contains our provider's configuration
-// TODO: generate model + builders
-type SnowflakeProviderModel struct {
-	Authenticator                  sdk.AuthenticationType `json:"authenticator,omitempty"`
-	ClientStoreTemporaryCredential string                 `json:"client_store_temporary_credential,omitempty"`
-	LoginTimeout                   string                 `json:"login_timeout,omitempty"`
-	OktaUrl                        string                 `json:"okta_url,omitempty"`
-	Port                           int                    `json:"port,omitempty"`
-	Profile                        string                 `json:"profile,omitempty"`
-	Protocol                       string                 `json:"protocol,omitempty"`
-	Role                           string                 `json:"role,omitempty"`
-	ValidateDefaultParameters      string                 `json:"validate_default_parameters,omitempty"`
-	Warehouse                      string                 `json:"warehouse,omitempty"`
-
-	*ProviderModelMeta
+func DefaultProviderMeta(name string) *ProviderModelMeta {
+	return &ProviderModelMeta{name: name}
 }
 
-func SnowflakeProvider() SnowflakeProviderModel {
-	return SnowflakeProviderModel{ProviderModelMeta: &ProviderModelMeta{name: "snowflake"}}
-}
-
-func SnowflakeProviderAlias(alias string) SnowflakeProviderModel {
-	return SnowflakeProviderModel{ProviderModelMeta: &ProviderModelMeta{name: "snowflake", alias: alias}}
+func ProviderMeta(name string, alias string) *ProviderModelMeta {
+	return &ProviderModelMeta{name: name, alias: alias}
 }
 
 func (m *ProviderModelMeta) ProviderName() string {
