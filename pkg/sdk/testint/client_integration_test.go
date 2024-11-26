0685049a81c2323738b9a8ef7ac945e21512def7
Jan Cieślak <jan.cieslak@snowflake.com>
2024-11-26 16:09:00 +0100

    Fix test

--- pkg/sdk/testint/client_integration_test.go
+++ pkg/sdk/testint/client_integration_test.go
@@ -12,7 +12,7 @@ import (
 
 func TestInt_Client_AdditionalMetadata(t *testing.T) {
 	client := testClient(t)
-	metadata := tracking.Metadata{Version: "v1.13.1002-rc-test", Resource: resources.Database.String(), Operation: tracking.CreateOperation}
+	metadata := tracking.Metadata{SchemaVersion: "1", Version: "v1.13.1002-rc-test", Resource: resources.Database.String(), Operation: tracking.CreateOperation}
 
 	assertQueryMetadata := func(t *testing.T, queryId string) {
 		t.Helper()
