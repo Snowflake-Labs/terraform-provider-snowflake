93ab8ff086d8f0ebfeb6019554a146a943aebe01
Filip Budzyński <filip.budzynski@snowflake.com>
2024-12-02 11:41:58 +0100

    refactor

--- pkg/sdk/poc/generator/show_by_id_filtering.go
+++ pkg/sdk/poc/generator/show_by_id_filtering.go
@@ -2,28 +2,21 @@ package generator
 
 import (
 	"fmt"
+	"log"
 )
 
-type ShowByIDFiltering struct {
+type ShowByIDFiltering interface {
+	WithFiltering() string
+}
+
+type ShowByIDFilter struct {
 	Name string
 	Kind string
 	Args string
 }
 
-func (s *ShowByIDFiltering) String() string {
+func (s *ShowByIDFilter) WithFiltering() string {
 	return fmt.Sprintf("With%s(%s{%s})", s.Name, s.Kind, s.Args)
-}
-
-type ShowByIDFilter interface {
-	String() string
-}
-
-func ShowByIDLikeFilteringFunc() ShowByIDFilter {
-	return &ShowByIDFiltering{
-		Name: "Like",
-		Kind: "Like",
-		Args: "Pattern: String(id.Name())",
-	}
 }
 
 type ShowByIDFilteringKind uint
@@ -39,27 +32,42 @@ const (
 	ShowByIDExtendedInFiltering
 )
 
+func NewShowByIDFiltering(name, kind, args string, identifierKind *string) ShowByIDFiltering {
+	filter := &ShowByIDFilter{
+		Name: name,
+		Kind: kind,
+		Args: args,
+	}
+	if identifierKind != nil {
+		filter.Args = fmt.Sprintf(args, *identifierKind)
+	}
+	return filter
+}
+
+func NewShowByIDLikeFiltering(string) ShowByIDFiltering {
+	return NewShowByIDFiltering("Like", "Like", "Pattern: String(id.Name())", nil)
+}
+
+func NewShowByIDInFiltering(identifierKind string) ShowByIDFiltering {
+	return NewShowByIDFiltering("In", "In", "%[1]v: id.%[1]vId()", &identifierKind)
+}
+
+func NewShowByIDExtendedInFiltering(identifierKind string) ShowByIDFiltering {
+	return NewShowByIDFiltering("In", "ExtendedIn", "In: In{%[1]v: id.%[1]vId()}", &identifierKind)
+}
+
+var filteringMap = map[ShowByIDFilteringKind]func(string) ShowByIDFiltering{
+	ShowByIDLikeFiltering:       NewShowByIDLikeFiltering,
+	ShowByIDInFiltering:         NewShowByIDInFiltering,
+	ShowByIDExtendedInFiltering: NewShowByIDExtendedInFiltering,
+}
+
 func (s *Operation) withFiltering(filtering ...ShowByIDFilteringKind) *Operation {
-	for _, f := range filtering {
-		switch f {
-		case ShowByIDLikeFiltering:
-			s.ShowByIDFiltering = append(s.ShowByIDFiltering, ShowByIDFiltering{
-				Name: "Like",
-				Kind: "Like",
-				Args: "Pattern: String(id.Name())",
-			})
-		case ShowByIDInFiltering:
-			s.ShowByIDFiltering = append(s.ShowByIDFiltering, ShowByIDFiltering{
-				Name: "In",
-				Kind: "In",
-				Args: fmt.Sprintf("%[1]v: id.%[1]vId()", s.ObjectInterface.ObjectIdentifierKind()),
-			})
-		case ShowByIDExtendedInFiltering:
-			s.ShowByIDFiltering = append(s.ShowByIDFiltering, ShowByIDFiltering{
-				Name: "In",
-				Kind: "ExtendedIn",
-				Args: fmt.Sprintf("In: In{%[1]v: id.%[1]vId()}", s.ObjectInterface.ObjectIdentifierKind()),
-			})
+	for _, filteringKind := range filtering {
+		if filter, ok := filteringMap[filteringKind]; ok {
+			s.ShowByIDFiltering = append(s.ShowByIDFiltering, filter(s.ObjectInterface.ObjectIdentifierKind()))
+		} else {
+			log.Println("No showByID filtering found for kind:", filteringKind)
 		}
 	}
 	return s
