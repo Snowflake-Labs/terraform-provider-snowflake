a3961d548f1e5c1c1d5ab3a150a162e810d732ff
Filip Budzyński <filip.budzynski@snowflake.com>
2024-12-02 11:08:11 +0100

    linter adjustemnts

--- pkg/sdk/poc/example/to_opts_optional_example_dto_gen.go
+++ pkg/sdk/poc/example/to_opts_optional_example_dto_gen.go
@@ -2,9 +2,7 @@ package example
 
 //go:generate go run ./dto-builder-generator/main.go
 
-var (
-	_ optionsProvider[AlterToOptsOptionalExampleOptions] = new(AlterToOptsOptionalExampleRequest)
-)
+var _ optionsProvider[AlterToOptsOptionalExampleOptions] = new(AlterToOptsOptionalExampleRequest)
 
 type AlterToOptsOptionalExampleRequest struct {
 	IfExists      *bool
