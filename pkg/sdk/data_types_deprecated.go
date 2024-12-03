9b8e017555ab3940a0661083b6b58bfe44998edd
Artur Sawicki <artur.sawicki@snowflake.com>
2024-12-03 12:29:47 +0100

    Remove unused methods from legacy data type

--- pkg/sdk/data_types_deprecated.go
+++ pkg/sdk/data_types_deprecated.go
@@ -1,11 +1,8 @@
 package sdk
 
 import (
-	"strconv"
 	"strings"
 
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/logging"
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/util"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk/datatypes"
 )
 
@@ -38,11 +35,6 @@ const (
 	DataTypeGeometry     DataType = "GEOMETRY"
 )
 
-var (
-	DataTypeNumberSynonyms  = []string{"NUMBER", "DECIMAL", "NUMERIC", "INT", "INTEGER", "BIGINT", "SMALLINT", "TINYINT", "BYTEINT"}
-	DataTypeVarcharSynonyms = []string{"VARCHAR", "CHAR", "CHARACTER", "STRING", "TEXT"}
-)
-
 func IsStringType(_type string) bool {
 	t := strings.ToUpper(_type)
 	return strings.HasPrefix(t, "STRING") ||
@@ -51,66 +43,6 @@ func IsStringType(_type string) bool {
 		strings.HasPrefix(t, "TEXT") ||
 		strings.HasPrefix(t, "NVARCHAR") ||
 		strings.HasPrefix(t, "NCHAR")
-}
-
-const (
-	DefaultNumberPrecision = 38
-	DefaultNumberScale     = 0
-	DefaultVarcharLength   = 16777216
-)
-
-// ParseNumberDataTypeRaw extracts precision and scale from the raw number data type input.
-// It returns defaults if it can't parse arguments, data type is different, or no arguments were provided.
-func ParseNumberDataTypeRaw(rawDataType string) (int, int) {
-	r := util.TrimAllPrefixes(strings.TrimSpace(strings.ToUpper(rawDataType)), DataTypeNumberSynonyms...)
-	r = strings.TrimSpace(r)
-	if strings.HasPrefix(r, "(") && strings.HasSuffix(r, ")") {
-		parts := strings.Split(r[1:len(r)-1], ",")
-		switch l := len(parts); l {
-		case 1:
-			precision, err := strconv.Atoi(strings.TrimSpace(parts[0]))
-			if err == nil {
-				return precision, DefaultNumberScale
-			} else {
-				logging.DebugLogger.Printf(`[DEBUG] Could not parse number precision "%s", err: %v`, parts[0], err)
-			}
-		case 2:
-			precision, err1 := strconv.Atoi(strings.TrimSpace(parts[0]))
-			scale, err2 := strconv.Atoi(strings.TrimSpace(parts[1]))
-			if err1 == nil && err2 == nil {
-				return precision, scale
-			} else {
-				logging.DebugLogger.Printf(`[DEBUG] Could not parse number precision "%s" or scale "%s", errs: %v, %v`, parts[0], parts[1], err1, err2)
-			}
-		default:
-			logging.DebugLogger.Printf("[DEBUG] Unexpected length of number arguments")
-		}
-	}
-	logging.DebugLogger.Printf("[DEBUG] Returning default number precision and scale")
-	return DefaultNumberPrecision, DefaultNumberScale
-}
-
-// ParseVarcharDataTypeRaw extracts length from the raw text data type input.
-// It returns default if it can't parse arguments, data type is different, or no length argument was provided.
-func ParseVarcharDataTypeRaw(rawDataType string) int {
-	r := util.TrimAllPrefixes(strings.TrimSpace(strings.ToUpper(rawDataType)), DataTypeVarcharSynonyms...)
-	r = strings.TrimSpace(r)
-	if strings.HasPrefix(r, "(") && strings.HasSuffix(r, ")") {
-		parts := strings.Split(r[1:len(r)-1], ",")
-		switch l := len(parts); l {
-		case 1:
-			length, err := strconv.Atoi(strings.TrimSpace(parts[0]))
-			if err == nil {
-				return length
-			} else {
-				logging.DebugLogger.Printf(`[DEBUG] Could not parse varchar length "%s", err: %v`, parts[0], err)
-			}
-		default:
-			logging.DebugLogger.Printf("[DEBUG] Unexpected length of varchar arguments")
-		}
-	}
-	logging.DebugLogger.Printf("[DEBUG] Returning default varchar length")
-	return DefaultVarcharLength
 }
 
 func LegacyDataTypeFrom(newDataType datatypes.DataType) DataType {
