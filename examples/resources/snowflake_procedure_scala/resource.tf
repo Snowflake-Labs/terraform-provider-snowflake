f16bb3b202343cd25279603af22d809e9b4d206c
Artur Sawicki <artur.sawicki@snowflake.com>
2024-12-12 19:58:40 +0100

    Pass procedure tests

--- examples/resources/snowflake_procedure_scala/resource.tf
+++ examples/resources/snowflake_procedure_scala/resource.tf
@@ -4,11 +4,11 @@ resource "snowflake_procedure_scala" "w" {
   name     = "Name"
   arguments {
     arg_data_type = "VARCHAR(100)"
-    arg_name = "x"
+    arg_name      = "x"
   }
-  return_type = "VARCHAR(100)"
-  handler = "TestFunc.echoVarchar"
+  return_type          = "VARCHAR(100)"
+  handler              = "TestFunc.echoVarchar"
   procedure_definition = "\n\timport com.snowflake.snowpark_java.Session\n\n\tclass TestFunc {\n\t\tdef echoVarchar(session : Session, x : String): String = {\n\t\t\treturn x\n\t\t}\n\t}\n"
-  runtime_version = "2.12"
-  snowpark_package = "1.14.0"
+  runtime_version      = "2.12"
+  snowpark_package     = "1.14.0"
 }
