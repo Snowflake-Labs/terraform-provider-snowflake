f684280b75fe58c34a6b11ffc568d008faf333b6
Artur Sawicki <artur.sawicki@snowflake.com>
2024-12-12 20:02:58 +0100

    Add examples for sql and scala functions

--- examples/resources/snowflake_function_scala/resource.tf
+++ examples/resources/snowflake_function_scala/resource.tf
@@ -1,2 +1,67 @@
-resource "snowflake_function_scala" "example" {
+# Minimal
+resource "snowflake_function_scala" "minimal" {
+  database = snowflake_database.test.name
+  schema   = snowflake_schema.test.name
+  name     = "my_scala_function"
+  arguments {
+    arg_data_type = "VARCHAR(100)"
+    arg_name      = "x"
+  }
+  function_definition = <<EOF
+  class TestFunc {
+    def echoVarchar(x : String): String = {
+      return x
+    }
+  }
+  EOF
+  runtime_version     = "2.12"
+  handler             = "TestFunc.echoVarchar"
+  return_type         = "VARCHAR(100)"
+}
+
+# Complete
+resource "snowflake_function_scala" "complete" {
+  database  = snowflake_database.test.name
+  schema    = snowflake_schema.test.name
+  name      = "my_scala_function"
+  is_secure = "false"
+  arguments {
+    arg_data_type = "VARCHAR(100)"
+    arg_name      = "x"
+  }
+  comment                 = "some comment"
+  external_access_integrations = ["external_access_integration_name", "external_access_integration_name_2"]
+  function_definition     = <<EOF
+  class TestFunc {
+    def echoVarchar(x : String): String = {
+      return x
+    }
+  }
+  EOF
+  handler                 = "TestFunc.echoVarchar"
+  null_input_behavior     = "CALLED ON NULL INPUT"
+  return_results_behavior = "VOLATILE"
+  return_type             = "VARCHAR(100)"
+  imports {
+    path_on_stage  = "jar_name.jar"
+    stage_location = "~"
+  }
+  imports {
+    path_on_stage  = "second_jar_name.jar"
+    stage_location = "~"
+  }
+  packages = ["com.snowflake:snowpark:1.14.0", "com.snowflake:telemetry:0.1.0"]
+  runtime_version = "2.12"
+  secrets {
+    secret_id            = snowflake_secret_with_authorization_code_grant.one.fully_qualified_name
+    secret_variable_name = "abc"
+  }
+  secrets {
+    secret_id            = snowflake_secret_with_authorization_code_grant.two.fully_qualified_name
+    secret_variable_name = "def"
+  }
+  target_path {
+    path_on_stage  = "target_jar_name.jar"
+    stage_location = snowflake_stage.test.fully_qualified_name
+  }
 }
--- examples/resources/snowflake_function_sql/resource.tf
+++ examples/resources/snowflake_function_sql/resource.tf
@@ -1,2 +1,32 @@
-resource "snowflake_function_sql" "example" {
+# Minimal
+resource "snowflake_function_sql" "minimal" {
+  database = snowflake_database.test.name
+  schema   = snowflake_schema.test.name
+  name     = "my_sql_function"
+  arguments {
+    arg_data_type = "FLOAT"
+    arg_name      = "arg_name"
+  }
+  function_definition = <<EOF
+    arg_name
+  EOF
+  return_type         = "FLOAT"
+}
+
+# Complete
+resource "snowflake_function_sql" "complete" {
+  database  = snowflake_database.test.name
+  schema    = snowflake_schema.test.name
+  name      = "my_sql_function"
+  is_secure = "false"
+  arguments {
+    arg_data_type = "FLOAT"
+    arg_name      = "arg_name"
+  }
+  function_definition     = <<EOF
+    arg_name
+  EOF
+  return_type             = "FLOAT"
+  return_results_behavior = "VOLATILE"
+  comment                 = "some comment"
 }
