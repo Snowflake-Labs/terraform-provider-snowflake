d610978fb6d972c8966d8ae2b8f362022412fada
Filip Budzyński <filip.budzynski@snowflake.com>
2024-12-16 16:24:22 +0100

    refacotred to be on Operatin level

--- pkg/sdk/poc/generator/helper_methods.go
+++ pkg/sdk/poc/generator/helper_methods.go
@@ -3,6 +3,7 @@ package generator
 import (
 	"fmt"
 	"log"
+	"slices"
 )
 
 type objectIdentifier string
@@ -29,6 +30,13 @@ func identifierStringToObjectIdentifier(s string) objectIdentifier {
 	}
 }
 
+type ObjectHelperMethodKind uint
+
+const (
+	ObjectHelperMethodID ObjectHelperMethodKind = iota
+	ObjectHelperMethodObjectType
+)
+
 type HelperMethod struct {
 	Name        string
 	StructName  string
@@ -45,71 +53,72 @@ func newHelperMethod(name, structName, returnValue string, returnType string) *H
 	}
 }
 
+func newObjectHelperMethodID(structName string, helperStructs []*Field, identifierString string) *HelperMethod {
+	objectIdentifier := identifierStringToObjectIdentifier(identifierString)
+	requiredFields, ok := requiredFieldsForIDMethodMapping[objectIdentifier]
+	if !ok {
+		log.Printf("WARNING: No required fields mapping defined for identifier %s", objectIdentifier)
+		return nil
+	}
+	if !hasRequiredFieldsForIDMethod(structName, helperStructs, requiredFields...) {
+		log.Printf("WARNING: Struct '%s' does not contain needed fields to build ID() helper method. Create the method manually in _ext file or add missing one of required fields: %v.\n", structName, requiredFields)
+		return nil
+	}
+
+	var args string
+	for _, field := range requiredFields {
+		args += fmt.Sprintf("v.%v, ", field)
+	}
+
+	returnValue := fmt.Sprintf("New%v(%v)", objectIdentifier, args)
+	return newHelperMethod("ID", structName, returnValue, string(objectIdentifier))
+}
+
+func newObjectHelperMethodObjectType(structName string) *HelperMethod {
+	returnValue := fmt.Sprintf("ObjectType%v", structName)
+	return newHelperMethod("ObjectType", structName, returnValue, "ObjectType")
+}
+
 var requiredFieldsForIDMethodMapping map[objectIdentifier][]string = map[objectIdentifier][]string{
 	AccountObjectIdentifier:  {"Name"},
 	DatabaseObjectIdentifier: {"Name", "DatabaseName"},
 	SchemaObjectIdentifier:   {"Name", "DatabaseName", "SchemaName"},
 }
 
-func newIDHelperMethod(structName string, objectIdentifier objectIdentifier) *HelperMethod {
-	var args string
-	fields := requiredFieldsForIDMethodMapping[objectIdentifier]
-	for _, field := range fields {
-		args += fmt.Sprintf("v.%v, ", field)
-	}
-	returnValue := fmt.Sprintf("New%v(%v)", objectIdentifier, args)
-	return newHelperMethod("ID", structName, returnValue, string(objectIdentifier))
-}
-
-func newObjectTypeHelperMethod(structName string) *HelperMethod {
-	returnValue := fmt.Sprintf("ObjectType%v", structName)
-	return newHelperMethod("ObjectType", structName, returnValue, "ObjectType")
-}
-
-func containsFieldNames(fields []*Field, names ...string) bool {
-	fieldNames := map[string]any{}
-	for _, field := range fields {
-		fieldNames[field.Name] = nil
-	}
-
-	for _, name := range names {
-		if _, ok := fieldNames[name]; !ok {
-			return false
+func hasRequiredFieldsForIDMethod(structName string, helperStructs []*Field, requiredFields ...string) bool {
+	for _, field := range helperStructs {
+		if field.Name == structName {
+			return containsFieldNames(field.Fields, requiredFields...)
 		}
 	}
-	return true
-}
-
-func hasRequiredFieldsForIDMethod(operations []*Operation, structName string, requiredFields ...string) bool {
-	for _, op := range operations {
-		if op.Name != string(OperationKindShow) {
-			continue
-		}
-		for _, field := range op.HelperStructs {
-			if field.Name == structName {
-				return containsFieldNames(field.Fields, requiredFields...)
-			}
-		}
-		log.Printf("WARNING: Struct: '%s' not found in '%s' operation. Couldn't generate ID() helper method.", structName, OperationKindShow)
-	}
-	log.Printf("WARNING: Operation: '%s' not found. Couldn't generate ID() helper method.", OperationKindShow)
 	return false
 }
 
-// HelperMethodID adds a helper method "ID()" to the interface file that returns the ObjectIdentifier of the object
-func (i *Interface) HelperMethodID() *Interface {
-	identifierKind := identifierStringToObjectIdentifier(i.IdentifierKind)
-	requiredFields := requiredFieldsForIDMethodMapping[identifierKind]
-	if !hasRequiredFieldsForIDMethod(i.Operations, i.NameSingular, requiredFields...) {
-		log.Printf("WARNING: Struct '%s' does not contain needed fields to build ID() helper method. Create the method manually in _ext file or add missing one of required fields: %v.\n", i.NameSingular, requiredFields)
-		return i
+func containsFieldNames(fields []*Field, names ...string) bool {
+	fieldNames := []string{}
+	for _, field := range fields {
+		fieldNames = append(fieldNames, field.Name)
 	}
-	i.HelperMethods = append(i.HelperMethods, newIDHelperMethod(i.NameSingular, identifierKind))
-	return i
+
+	for _, name := range names {
+		if !slices.Contains(names, name) {
+			return false
+		}
+	}
+
+	return true
 }
 
-// HelperMethodObjectType adds a helper method "ObjectType()" to the interface file that returns the ObjectType for the struct
-func (i *Interface) HelperMethodObjectType() *Interface {
-	i.HelperMethods = append(i.HelperMethods, newObjectTypeHelperMethod(i.NameSingular))
-	return i
+func (s *Operation) withObjectHelperMethods(structName string, helperMethods ...ObjectHelperMethodKind) *Operation {
+	for _, helperMethod := range helperMethods {
+		switch helperMethod {
+		case ObjectHelperMethodID:
+			s.HelperMethods = append(s.HelperMethods, newObjectHelperMethodID(structName, s.HelperStructs, s.ObjectInterface.IdentifierKind))
+		case ObjectHelperMethodObjectType:
+			s.HelperMethods = append(s.HelperMethods, newObjectHelperMethodObjectType(structName))
+		default:
+			log.Println("No object helper method found for kind:", helperMethod)
+		}
+	}
+	return s
 }
