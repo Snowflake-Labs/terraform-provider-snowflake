5424d8b642fc8c8aa1aad6b6f868007942a134f1
Artur Sawicki <artur.sawicki@snowflake.com>
2024-12-19 15:30:16 +0100

    chore: Add external removal tests to functions and procedures (#3305)
    - Add external removal tests to functions and procedures (done during
    old issues closing)
    - Fix the following tests:
      - TestAcc_FunctionJava_InlineFull
      - TestAcc_FunctionPython_InlineBasic
      - TestAcc_FunctionPython_InlineFull
      - TestAcc_FunctionScala_InlineFull
      - TestAcc_FunctionSql_InlineBasic

--- pkg/resources/function_scala_acceptance_test.go
+++ pkg/resources/function_scala_acceptance_test.go
@@ -69,6 +69,22 @@ func TestAcc_FunctionScala_InlineBasic(t *testing.T) {
 					assert.Check(resource.TestCheckResourceAttr(functionModel.ResourceReference(), "arguments.0.arg_default_value", "")),
 				),
 			},
+			// REMOVE EXTERNALLY (CHECK RECREATION)
+			{
+				PreConfig: func() {
+					acc.TestClient().Function.DropFunctionFunc(t, id)()
+				},
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(functionModel.ResourceReference(), plancheck.ResourceActionCreate),
+					},
+				},
+				Config: config.FromModels(t, functionModel),
+				Check: assert.AssertThat(t,
+					resourceassert.FunctionScalaResource(t, functionModel.ResourceReference()).
+						HasNameString(id.Name()),
+				),
+			},
 			// IMPORT
 			{
 				ResourceName:            functionModel.ResourceReference(),
@@ -99,6 +115,7 @@ func TestAcc_FunctionScala_InlineBasic(t *testing.T) {
 func TestAcc_FunctionScala_InlineFull(t *testing.T) {
 	_ = testenvs.GetOrSkipTest(t, testenvs.EnableAcceptance)
 	acc.TestAccPreCheck(t)
+	t.Setenv(string(testenvs.ConfigureClientOnce), "")
 
 	stage, stageCleanup := acc.TestClient().Stage.CreateStage(t)
 	t.Cleanup(stageCleanup)
