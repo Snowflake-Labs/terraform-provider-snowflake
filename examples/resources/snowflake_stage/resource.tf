6dc4b60db1d201dda82ec6facec88b582025942c
sfc-gh-dszmolka <david.szmolka@snowflake.com>
2024-12-26 13:30:03 +0100

    .maybe adding it into the resource.tf can get this through doc autogeneration and validation

--- examples/resources/snowflake_stage/resource.tf
+++ examples/resources/snowflake_stage/resource.tf
@@ -6,3 +6,10 @@ resource "snowflake_stage" "example_stage" {
   schema      = "EXAMPLE_SCHEMA"
   credentials = "AWS_KEY_ID='${var.example_aws_key_id}' AWS_SECRET_KEY='${var.example_aws_secret_key}'"
 }
+/*
+  * Examples of usage for `file_format`:
+    * with hardcoding value: `file_format="FORMAT_NAME = DB.SCHEMA.FORMATNAME"`
+    * from dynamic value: `file_format = "FORMAT_NAME = ${snowflake_database.mydb.name}.${snowflake_schema.myschema.name}.${snowflake_file_format.myfileformat.name}"`
+    * from expression: `file_format = format("FORMAT_NAME =%s.%s.MYFILEFORMAT", var.db_name, each.value.schema_name)`
+  * Reference: [#265](https://github.com/Snowflake-Labs/terraform-provider-snowflake/issues/265)
+*/
