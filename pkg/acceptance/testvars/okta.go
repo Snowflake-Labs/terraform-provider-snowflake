34068e46ff38a933c6e2f194561a5129219b25f4
Artur Sawicki <artur.sawicki@snowflake.com>
2025-01-13 13:49:44 +0100

    Run pre-push

--- pkg/acceptance/testvars/okta.go
+++ pkg/acceptance/testvars/okta.go
@@ -2,8 +2,12 @@ package testvars
 
 import "net/url"
 
-var ExampleOktaUrlString = "https://example-tf.com"
-var ExampleOktaUrl, _ = url.Parse(ExampleOktaUrlString)
+var (
+	ExampleOktaUrlString = "https://example-tf.com"
+	ExampleOktaUrl, _    = url.Parse(ExampleOktaUrlString)
+)
 
-var ExampleOktaUrlFromEnvString = "https://example-tf-env.com"
-var ExampleOktaUrlFromEnv, _ = url.Parse(ExampleOktaUrlFromEnvString)
+var (
+	ExampleOktaUrlFromEnvString = "https://example-tf-env.com"
+	ExampleOktaUrlFromEnv, _    = url.Parse(ExampleOktaUrlFromEnvString)
+)
