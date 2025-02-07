e4f8250dfb62a9ee493a7c943454141e0b861ab0
Jakub Michalak <jakub.michalak@snowflake.com>
2025-02-07 13:54:59 +0100

    TF variables

--- examples/resources/snowflake_api_authentication_integration_with_authorization_code_grant/resource.tf
+++ examples/resources/snowflake_api_authentication_integration_with_authorization_code_grant/resource.tf
@@ -2,7 +2,7 @@
 resource "snowflake_api_authentication_integration_with_authorization_code_grant" "test" {
   enabled             = true
   name                = "test"
-  oauth_client_id     = "sn-oauth-134o9erqfedlc"
+  oauth_client_id     = var.oauth_client_id
   oauth_client_secret = var.oauth_client_secret
 }
 # resource with all fields set
@@ -14,10 +14,15 @@ resource "snowflake_api_authentication_integration_with_authorization_code_grant
   oauth_allowed_scopes         = ["useraccount"]
   oauth_authorization_endpoint = "https://example.com"
   oauth_client_auth_method     = "CLIENT_SECRET_POST"
-  oauth_client_id              = "sn-oauth-134o9erqfedlc"
+  oauth_client_id              = var.oauth_client_id
   oauth_client_secret          = var.oauth_client_secret
   oauth_refresh_token_validity = 42
   oauth_token_endpoint         = "https://example.com"
+}
+
+variable "oauth_client_id" {
+  type      = string
+  sensitive = true
 }
 
 variable "oauth_client_secret" {
--- examples/resources/snowflake_api_authentication_integration_with_jwt_bearer/resource.tf
+++ examples/resources/snowflake_api_authentication_integration_with_jwt_bearer/resource.tf
@@ -2,7 +2,7 @@
 resource "snowflake_api_authentication_integration_with_jwt_bearer" "test" {
   enabled                = true
   name                   = "test"
-  oauth_client_id        = "sn-oauth-134o9erqfedlc"
+  oauth_client_id        = var.oauth_client_id
   oauth_client_secret    = var.oauth_client_secret
   oauth_assertion_issuer = "issuer"
 }
@@ -14,11 +14,16 @@ resource "snowflake_api_authentication_integration_with_jwt_bearer" "test" {
   oauth_access_token_validity  = 42
   oauth_authorization_endpoint = "https://example.com"
   oauth_client_auth_method     = "CLIENT_SECRET_POST"
-  oauth_client_id              = "sn-oauth-134o9erqfedlc"
+  oauth_client_id              = var.oauth_client_id
   oauth_client_secret          = var.oauth_client_secret
   oauth_refresh_token_validity = 42
   oauth_token_endpoint         = "https://example.com"
   oauth_assertion_issuer       = "issuer"
+}
+
+variable "oauth_client_id" {
+  type      = string
+  sensitive = true
 }
 
 variable "oauth_client_secret" {
