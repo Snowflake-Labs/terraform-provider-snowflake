4d3dc1d3c5614a760c7ae7670efa5a3b57f8b064
Artur Sawicki <artur.sawicki@snowflake.com>
2025-02-21 17:10:25 +0100

    Split test objects suffix

---
+++ pkg/acceptance/helpers/random/integrationtests/test_object_suffix.go
@@ -0,0 +1,19 @@
+package integrationtests
+
+import (
+	"log"
+
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/helpers/random"
+)
+
+// TODO [SNOW-1356199]: add generation tests
+// TODO [SNOW-1356199]: use the same fallback suffix for acceptance and integration tests (now two different ones are generated if the env is missing)
+var (
+	ObjectsSuffix = integrationTestsSuffix()
+)
+
+func integrationTestsSuffix() string {
+	suffix := "IT_" + random.ObjectSuffix()
+	log.Printf("[DEBUG] Suffix for the given test run is: %s", suffix)
+	return suffix
+}
