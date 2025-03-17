ef9d0fa57d0a1ace791970080fffd3ca92935b5b
Artur Sawicki <artur.sawicki@snowflake.com>
2025-03-17 14:24:03 +0100

    Fix streamlits datasource acceptance tests setup

--- pkg/datasources/streamlits_acceptance_test.go
+++ pkg/datasources/streamlits_acceptance_test.go
@@ -2,17 +2,18 @@ package datasources_test
 
 import (
 	"fmt"
-	"maps"
 	"regexp"
 	"testing"
 
 	acc "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance"
-	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/snowflakeroles"
+	accconfig "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config"
 
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config/datasourcemodel"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config/model"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/helpers/random"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testenvs"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/internal/snowflakeroles"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/resources"
-	"github.com/hashicorp/terraform-plugin-testing/config"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
@@ -22,10 +23,6 @@ func TestAcc_Streamlits(t *testing.T) {
 	t.Skip("Skipping because of the error: 090105 (22000): Cannot perform operation. This session does not have a current database. Call 'USE DATABASE', or use a qualified name.")
 	_ = testenvs.GetOrSkipTest(t, testenvs.EnableAcceptance)
 	acc.TestAccPreCheck(t)
-
-	databaseId := acc.TestClient().Ids.DatabaseId()
-	schemaId := acc.TestClient().Ids.SchemaId()
-	id := acc.TestClient().Ids.RandomSchemaObjectIdentifier()
 
 	stage, stageCleanup := acc.TestClient().Stage.CreateStage(t)
 	t.Cleanup(stageCleanup)
@@ -38,19 +35,28 @@ func TestAcc_Streamlits(t *testing.T) {
 	externalAccessIntegrationId, externalAccessIntegrationCleanup := acc.TestClient().ExternalAccessIntegration.CreateExternalAccessIntegration(t, networkRule.ID())
 	t.Cleanup(externalAccessIntegrationCleanup)
 
-	rootLocation := fmt.Sprintf("@%s/foo", stage.ID().FullyQualifiedName())
-	configVariables := config.Variables{
-		"database":                     config.StringVariable(databaseId.Name()),
-		"schema":                       config.StringVariable(schemaId.Name()),
-		"stage":                        config.StringVariable(stage.ID().FullyQualifiedName()),
-		"directory_location":           config.StringVariable("foo"),
-		"name":                         config.StringVariable(id.Name()),
-		"main_file":                    config.StringVariable("foo"),
-		"query_warehouse":              config.StringVariable(warehouse.ID().Name()),
-		"external_access_integrations": config.SetVariable(config.StringVariable(externalAccessIntegrationId.Name())),
-		"title":                        config.StringVariable("foo"),
-		"comment":                      config.StringVariable("foo"),
-	}
+	databaseId := acc.TestClient().Ids.DatabaseId()
+	schemaId := acc.TestClient().Ids.SchemaId()
+	id := acc.TestClient().Ids.RandomSchemaObjectIdentifier()
+	mainFile := random.AlphaN(4)
+	comment := random.Comment()
+	title := random.AlphaN(4)
+	directoryLocation := random.AlphaN(4)
+	rootLocation := fmt.Sprintf("@%s/%s", stage.ID().FullyQualifiedName(), directoryLocation)
+
+	streamlitModel := model.StreamlitWithIds("test", id, mainFile, stage.ID()).
+		WithComment(comment).
+		WithTitle(title).
+		WithDirectoryLocation(directoryLocation).
+		WithQueryWarehouse(warehouse.ID().Name()).
+		WithExternalAccessIntegrations(externalAccessIntegrationId)
+	streamlitsModel := datasourcemodel.Streamlits("test").
+		WithLike(id.Name()).
+		WithDependsOn(streamlitModel.ResourceReference())
+	streamlitsModelWithoutDescribe := datasourcemodel.Streamlits("test").
+		WithLike(id.Name()).
+		WithWithDescribe(false).
+		WithDependsOn(streamlitModel.ResourceReference())
 
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: acc.TestAccProtoV6ProviderFactories,
@@ -61,56 +67,54 @@ func TestAcc_Streamlits(t *testing.T) {
 		CheckDestroy: acc.CheckDestroy(t, resources.Streamlit),
 		Steps: []resource.TestStep{
 			{
-				ConfigDirectory: acc.ConfigurationDirectory("TestAcc_Streamlits/optionals_set"),
-				ConfigVariables: configVariables,
+				Config: accconfig.FromModels(t, streamlitModel, streamlitsModel),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.#", "1"),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.#", "1"),
 
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.#", "1"),
-					resource.TestCheckResourceAttrSet("data.snowflake_streamlits.test", "streamlits.0.show_output.0.created_on"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.name", id.Name()),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.database_name", databaseId.Name()),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.schema_name", schemaId.Name()),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.title", "foo"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.owner", acc.TestClient().Context.CurrentRole(t).Name()),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.comment", "foo"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.query_warehouse", warehouse.ID().Name()),
-					resource.TestCheckResourceAttrSet("data.snowflake_streamlits.test", "streamlits.0.show_output.0.url_id"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.owner_role_type", "ROLE"),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.#", "1"),
+					resource.TestCheckResourceAttrSet(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.created_on"),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.name", id.Name()),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.database_name", databaseId.Name()),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.schema_name", schemaId.Name()),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.title", title),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.owner", acc.TestClient().Context.CurrentRole(t).Name()),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.comment", comment),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.query_warehouse", warehouse.ID().Name()),
+					resource.TestCheckResourceAttrSet(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.url_id"),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.show_output.0.owner_role_type", "ROLE"),
 
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.name", id.Name()),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.title", "foo"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.root_location", rootLocation),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.main_file", "foo"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.query_warehouse", warehouse.ID().Name()),
-					resource.TestCheckResourceAttrSet("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.url_id"),
-					resource.TestCheckResourceAttrSet("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.default_packages"),
-					resource.TestCheckResourceAttrSet("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.user_packages.#"),
-					resource.TestCheckResourceAttrSet("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.import_urls.#"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.external_access_integrations.#", "1"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.external_access_integrations.0", externalAccessIntegrationId.Name()),
-					resource.TestCheckResourceAttrSet("data.snowflake_streamlits.test", "streamlits.0.describe_output.0.external_access_secrets"),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.name", id.Name()),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.title", title),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.root_location", rootLocation),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.main_file", mainFile),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.query_warehouse", warehouse.ID().Name()),
+					resource.TestCheckResourceAttrSet(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.url_id"),
+					resource.TestCheckResourceAttrSet(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.default_packages"),
+					resource.TestCheckResourceAttrSet(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.user_packages.#"),
+					resource.TestCheckResourceAttrSet(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.import_urls.#"),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.external_access_integrations.#", "1"),
+					resource.TestCheckResourceAttr(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.external_access_integrations.0", externalAccessIntegrationId.Name()),
+					resource.TestCheckResourceAttrSet(streamlitsModel.DatasourceReference(), "streamlits.0.describe_output.0.external_access_secrets"),
 				),
 			},
 			{
-				ConfigDirectory: acc.ConfigurationDirectory("TestAcc_Streamlits/optionals_unset"),
-				ConfigVariables: configVariables,
+				Config: accconfig.FromModels(t, streamlitModel, streamlitsModelWithoutDescribe),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.#", "1"),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.#", "1"),
 
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.#", "1"),
-					resource.TestCheckResourceAttrSet("data.snowflake_streamlits.test", "streamlits.0.show_output.0.created_on"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.name", id.Name()),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.database_name", databaseId.Name()),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.schema_name", schemaId.Name()),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.title", "foo"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.owner", snowflakeroles.Accountadmin.Name()),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.comment", "foo"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.query_warehouse", warehouse.ID().Name()),
-					resource.TestCheckResourceAttrSet("data.snowflake_streamlits.test", "streamlits.0.show_output.0.url_id"),
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.show_output.0.owner_role_type", "ROLE"),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.#", "1"),
+					resource.TestCheckResourceAttrSet(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.created_on"),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.name", id.Name()),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.database_name", databaseId.Name()),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.schema_name", schemaId.Name()),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.title", title),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.owner", snowflakeroles.Accountadmin.Name()),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.comment", comment),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.query_warehouse", warehouse.ID().Name()),
+					resource.TestCheckResourceAttrSet(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.url_id"),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.show_output.0.owner_role_type", "ROLE"),
 
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.0.describe_output.#", "0"),
+					resource.TestCheckResourceAttr(streamlitsModelWithoutDescribe.DatasourceReference(), "streamlits.0.describe_output.#", "0"),
 				),
 			},
 		},
@@ -118,34 +122,28 @@ func TestAcc_Streamlits(t *testing.T) {
 }
 
 func TestAcc_Streamlits_Filtering(t *testing.T) {
-	prefix := random.AlphaN(4)
-	idOne := acc.TestClient().Ids.RandomAccountObjectIdentifierWithPrefix(prefix)
-	idTwo := acc.TestClient().Ids.RandomAccountObjectIdentifierWithPrefix(prefix)
-	idThree := acc.TestClient().Ids.RandomAccountObjectIdentifier()
-	databaseId := acc.TestClient().Ids.DatabaseId()
-	schemaId := acc.TestClient().Ids.SchemaId()
+	_ = testenvs.GetOrSkipTest(t, testenvs.EnableAcceptance)
 	acc.TestAccPreCheck(t)
+
 	stage, stageCleanup := acc.TestClient().Stage.CreateStage(t)
 	t.Cleanup(stageCleanup)
-	commonVariables := config.Variables{
-		"name_1":    config.StringVariable(idOne.Name()),
-		"name_2":    config.StringVariable(idTwo.Name()),
-		"name_3":    config.StringVariable(idThree.Name()),
-		"main_file": config.StringVariable("foo"),
-		"schema":    config.StringVariable(schemaId.Name()),
-		"stage":     config.StringVariable(stage.ID().FullyQualifiedName()),
-		"database":  config.StringVariable(databaseId.Name()),
-	}
 
-	likeConfig := config.Variables{
-		"like": config.StringVariable(idOne.Name()),
-	}
-	maps.Copy(likeConfig, commonVariables)
+	prefix := random.AlphaN(4)
+	idOne := acc.TestClient().Ids.RandomSchemaObjectIdentifierWithPrefix(prefix)
+	idTwo := acc.TestClient().Ids.RandomSchemaObjectIdentifierWithPrefix(prefix)
+	idThree := acc.TestClient().Ids.RandomSchemaObjectIdentifier()
 
-	likeConfig2 := config.Variables{
-		"like": config.StringVariable(prefix + "%"),
-	}
-	maps.Copy(likeConfig2, commonVariables)
+	mainFile := random.AlphaN(4)
+
+	streamlitModel1 := model.StreamlitWithIds("test1", idOne, mainFile, stage.ID())
+	streamlitModel2 := model.StreamlitWithIds("test2", idTwo, mainFile, stage.ID())
+	streamlitModel3 := model.StreamlitWithIds("test3", idThree, mainFile, stage.ID())
+	streamlitsModelLikeFirst := datasourcemodel.Streamlits("test").
+		WithLike(idOne.Name()).
+		WithDependsOn(streamlitModel1.ResourceReference(), streamlitModel2.ResourceReference(), streamlitModel3.ResourceReference())
+	streamlitsModelLikePrefix := datasourcemodel.Streamlits("test").
+		WithLike(prefix+"%").
+		WithDependsOn(streamlitModel1.ResourceReference(), streamlitModel2.ResourceReference(), streamlitModel3.ResourceReference())
 
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: acc.TestAccProtoV6ProviderFactories,
@@ -156,17 +154,15 @@ func TestAcc_Streamlits_Filtering(t *testing.T) {
 		CheckDestroy: acc.CheckDestroy(t, resources.Streamlit),
 		Steps: []resource.TestStep{
 			{
-				ConfigDirectory: acc.ConfigurationDirectory("TestAcc_Streamlits/like"),
-				ConfigVariables: likeConfig,
+				Config: accconfig.FromModels(t, streamlitModel1, streamlitModel2, streamlitModel3, streamlitsModelLikeFirst),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.#", "1"),
+					resource.TestCheckResourceAttr(streamlitsModelLikeFirst.DatasourceReference(), "streamlits.#", "1"),
 				),
 			},
 			{
-				ConfigDirectory: acc.ConfigurationDirectory("TestAcc_Streamlits/like"),
-				ConfigVariables: likeConfig2,
+				Config: accconfig.FromModels(t, streamlitModel1, streamlitModel2, streamlitModel3, streamlitsModelLikePrefix),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.snowflake_streamlits.test", "streamlits.#", "2"),
+					resource.TestCheckResourceAttr(streamlitsModelLikePrefix.DatasourceReference(), "streamlits.#", "2"),
 				),
 			},
 		},
