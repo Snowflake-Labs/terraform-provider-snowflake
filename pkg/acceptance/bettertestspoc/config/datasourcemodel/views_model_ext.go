24e9fcc51e7947f22bbd8817a3d5944f031f4c20
Artur Sawicki <artur.sawicki@snowflake.com>
2025-03-17 22:33:13 +0100

    Fix views datasource acceptance test setup

---
+++ pkg/acceptance/bettertestspoc/config/datasourcemodel/views_model_ext.go
@@ -0,0 +1,32 @@
+package datasourcemodel
+
+import (
+	tfconfig "github.com/hashicorp/terraform-plugin-testing/config"
+
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
+)
+
+func (v *ViewsModel) WithInSchema(schemaId sdk.DatabaseObjectIdentifier) *ViewsModel {
+	return v.WithInValue(
+		tfconfig.ObjectVariable(map[string]tfconfig.Variable{
+			"schema": tfconfig.StringVariable(schemaId.FullyQualifiedName()),
+		}),
+	)
+}
+
+func (v *ViewsModel) WithInDatabase(databaseId sdk.AccountObjectIdentifier) *ViewsModel {
+	return v.WithInValue(
+		tfconfig.ObjectVariable(map[string]tfconfig.Variable{
+			"database": tfconfig.StringVariable(databaseId.Name()),
+		}),
+	)
+}
+
+func (v *ViewsModel) WithLimitRowsAndFrom(rows int, from string) *ViewsModel {
+	return v.WithLimitValue(
+		tfconfig.ObjectVariable(map[string]tfconfig.Variable{
+			"rows": tfconfig.IntegerVariable(rows),
+			"from": tfconfig.StringVariable(from),
+		}),
+	)
+}
