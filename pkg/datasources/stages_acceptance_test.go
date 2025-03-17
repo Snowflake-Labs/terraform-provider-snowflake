00ea8a4dedc84c26e50bc8ec2bad38355f4875ab
Artur Sawicki <artur.sawicki@snowflake.com>
2025-03-17 14:03:25 +0100

    Fix stages datasource acceptance test setup

--- pkg/datasources/stages_acceptance_test.go
+++ pkg/datasources/stages_acceptance_test.go
@@ -7,15 +7,18 @@ import (
 	acc "github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance"
 
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/helpers/random"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/testenvs"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
 func TestAcc_Stages(t *testing.T) {
-	databaseName := acc.TestClient().Ids.Alpha()
-	schemaName := acc.TestClient().Ids.Alpha()
-	storageIntegrationName := acc.TestClient().Ids.Alpha()
-	stageName := acc.TestClient().Ids.Alpha()
+	_ = testenvs.GetOrSkipTest(t, testenvs.EnableAcceptance)
+	acc.TestAccPreCheck(t)
+
+	stageId := acc.TestClient().Ids.RandomSchemaObjectIdentifier()
+	storageIntegrationId := acc.TestClient().Ids.RandomAccountObjectIdentifier()
 	comment := random.Comment()
 
 	resource.Test(t, resource.TestCase{
@@ -26,13 +29,13 @@ func TestAcc_Stages(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: stages(databaseName, schemaName, storageIntegrationName, stageName, comment),
+				Config: stages(stageId, storageIntegrationId, comment),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.snowflake_stages.test", "database", databaseName),
-					resource.TestCheckResourceAttr("data.snowflake_stages.test", "schema", schemaName),
+					resource.TestCheckResourceAttr("data.snowflake_stages.test", "database", stageId.DatabaseName()),
+					resource.TestCheckResourceAttr("data.snowflake_stages.test", "schema", stageId.SchemaName()),
 					resource.TestCheckResourceAttr("data.snowflake_stages.test", "stages.#", "1"),
-					resource.TestCheckResourceAttr("data.snowflake_stages.test", "stages.0.name", stageName),
-					resource.TestCheckResourceAttr("data.snowflake_stages.test", "stages.0.storage_integration", storageIntegrationName),
+					resource.TestCheckResourceAttr("data.snowflake_stages.test", "stages.0.name", stageId.Name()),
+					resource.TestCheckResourceAttr("data.snowflake_stages.test", "stages.0.storage_integration", storageIntegrationId.Name()),
 					resource.TestCheckResourceAttr("data.snowflake_stages.test", "stages.0.comment", comment),
 				),
 			},
@@ -40,19 +43,10 @@ func TestAcc_Stages(t *testing.T) {
 	})
 }
 
-func stages(databaseName string, schemaName string, storageIntegrationName string, stageName string, comment string) string {
+func stages(stageId sdk.SchemaObjectIdentifier, storageIntegrationId sdk.AccountObjectIdentifier, comment string) string {
 	return fmt.Sprintf(`
-	resource "snowflake_database" "test" {
-		name = "%s"
-	}
-
-	resource "snowflake_schema" "test"{
-		name 	 = "%s"
-		database = snowflake_database.test.name
-	}
-
 	resource "snowflake_storage_integration" "test" {
-		name = "%s"
+		name = "%[4]s"
 		storage_allowed_locations = ["s3://foo/"]
 		storage_provider = "S3"
 
@@ -60,19 +54,17 @@ func stages(databaseName string, schemaName string, storageIntegrationName strin
 	}
 
 	resource "snowflake_stage" "test"{
-		name 	 				= "%s"
-		database 				= snowflake_schema.test.database
-		schema 	 				= snowflake_schema.test.name
+		database 				= "%[1]s"
+		schema 	 				= "%[2]s"
+		name 	 				= "%[3]s"
 		url 					= "s3://foo/"
 		storage_integration 	= snowflake_storage_integration.test.name
-		comment  				= "%s"
+		comment  				= "%[5]s"
 	}
 
 	data "snowflake_stages" "test" {
-		depends_on = [snowflake_storage_integration.test, snowflake_stage.test]
-
 		database = snowflake_stage.test.database
 		schema = snowflake_stage.test.schema
 	}
-	`, databaseName, schemaName, storageIntegrationName, stageName, comment)
+	`, stageId.DatabaseName(), stageId.SchemaName(), stageId.Name(), storageIntegrationId.Name(), comment)
 }
