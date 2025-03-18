39eee99284b7dbcce299311026e471ce29df4cf2
Artur Sawicki <artur.sawicki@snowflake.com>
2025-03-18 11:59:17 +0100

    Use account-level only parameter in account parameters acceptance tests

--- pkg/resources/account_parameter_acceptance_test.go
+++ pkg/resources/account_parameter_acceptance_test.go
@@ -13,6 +13,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
 )
 
+// Use only parameters that can be set only on the account level for the time-being.
+// TODO [SNOW-1866453]: add more acc tests for the remaining parameters
+
 func TestAcc_AccountParameter(t *testing.T) {
 	accountParameterModel := model.AccountParameter("test", string(sdk.AccountParameterAllowIDToken), "true")
 	resource.Test(t, resource.TestCase{
@@ -76,9 +79,10 @@ func TestAcc_AccountParameter_REQUIRE_STORAGE_INTEGRATION_FOR_STAGE_CREATION(t *
 	})
 }
 
-// TODO: use different param here (account only)
+// Proves https://github.com/Snowflake-Labs/terraform-provider-snowflake/issues/2573 is solved.
+// Instead of TIMEZONE, we used INITIAL_REPLICATION_SIZE_LIMIT_IN_TB which is only settable on account so it does not mess with other tests.
 func TestAcc_AccountParameter_Issue2573(t *testing.T) {
-	accountParameterModel := model.AccountParameter("test", string(sdk.AccountParameterTimezone), "Etc/UTC")
+	accountParameterModel := model.AccountParameter("test", string(sdk.AccountParameterInitialReplicationSizeLimitInTB), "3.0")
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: acc.TestAccProtoV6ProviderFactories,
 		PreCheck:                 func() { acc.TestAccPreCheck(t) },
@@ -90,8 +94,8 @@ func TestAcc_AccountParameter_Issue2573(t *testing.T) {
 			{
 				Config: config.FromModels(t, accountParameterModel),
 				Check: assertThat(t, resourceassert.AccountParameterResource(t, accountParameterModel.ResourceReference()).
-					HasKeyString(string(sdk.AccountParameterTimezone)).
-					HasValueString("Etc/UTC"),
+					HasKeyString(string(sdk.AccountParameterInitialReplicationSizeLimitInTB)).
+					HasValueString("3.0"),
 				),
 			},
 			{
@@ -172,5 +176,3 @@ func TestAcc_AccountParameter_INITIAL_REPLICATION_SIZE_LIMIT_IN_TB(t *testing.T)
 		},
 	})
 }
-
-// TODO(SNOW-1866453): add more acc tests for the remaining parameters
