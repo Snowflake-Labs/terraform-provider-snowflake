c5500d38c8c422ff0a88ee40ffa86fa71ab400d8
Artur Sawicki <artur.sawicki@snowflake.com>
2025-03-26 15:37:46 +0100

    Fix grant privileges to share resource acceptance tests setup

--- pkg/resources/testdata/TestAcc_GrantPrivilegesToShare/OnSchema_NoGrant/test.tf
+++ pkg/resources/testdata/TestAcc_GrantPrivilegesToShare/OnSchema_NoGrant/test.tf
@@ -1,14 +1,5 @@
-resource "snowflake_share" "test" {
-  depends_on = [snowflake_database.test, snowflake_schema.test]
-  name       = var.to_share
-}
-
-resource "snowflake_database" "test" {
-  name = var.database
-}
-
 resource "snowflake_schema" "test" {
   name     = var.schema
-  database = snowflake_database.test.name
+  database = var.database
 }
 
