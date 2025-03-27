7702b0eec643f2367cb4c34cc4310b7c470dbfc9
Artur Sawicki <artur.sawicki@snowflake.com>
2025-03-27 15:32:12 +0100

    Fix setup for oauth integration for partner applications acceptance tests

--- pkg/resources/testdata/TestAcc_OauthIntegrationForPartnerApplications/basic/test.tf
+++
@@ -1,5 +0,0 @@
-resource "snowflake_oauth_integration_for_partner_applications" "test" {
-  name               = var.name
-  oauth_client       = var.oauth_client
-  oauth_redirect_uri = var.oauth_redirect_uri
-}
