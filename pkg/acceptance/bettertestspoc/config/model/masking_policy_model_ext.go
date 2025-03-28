0fd73b6e9e8d9d4aa61f97c1438799d08e0baad3
Artur Sawicki <artur.sawicki@snowflake.com>
2025-03-28 16:01:07 +0100

    Use dynamic block from model builder in an example test

--- pkg/acceptance/bettertestspoc/config/model/masking_policy_model_ext.go
+++ pkg/acceptance/bettertestspoc/config/model/masking_policy_model_ext.go
@@ -3,8 +3,26 @@ package model
 import (
 	tfconfig "github.com/hashicorp/terraform-plugin-testing/config"
 
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/acceptance/bettertestspoc/config"
+	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/provider/resources"
 	"github.com/Snowflake-Labs/terraform-provider-snowflake/pkg/sdk"
 )
+
+func MaskingPolicyDynamicArguments(
+	resourceName string,
+	id sdk.SchemaObjectIdentifier,
+	body string,
+	returnDataType sdk.DataType,
+) *MaskingPolicyModel {
+	MaskingPolicy(resourceName, nil, body, id.DatabaseName(), id.Name(), string(returnDataType), id.SchemaName())
+	m := &MaskingPolicyModel{ResourceModelMeta: config.Meta(resourceName, resources.MaskingPolicy)}
+	m.WithDatabase(id.DatabaseName())
+	m.WithSchema(id.SchemaName())
+	m.WithName(id.Name())
+	m.WithBody(body)
+	m.WithReturnDataType(string(returnDataType))
+	return m.WithDynamicBlock(config.NewDynamicBlock("argument", "arguments", []string{"name", "type"}))
+}
 
 func (p *MaskingPolicyModel) WithArgument(argument []sdk.TableColumnSignature) *MaskingPolicyModel {
 	maps := make([]tfconfig.Variable, len(argument))
