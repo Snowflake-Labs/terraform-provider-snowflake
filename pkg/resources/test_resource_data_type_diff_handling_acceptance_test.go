77229f874b287320c8ce80e0d6427bea5b349b97
Artur Sawicki <artur.sawicki@snowflake.com>
2025-04-17 17:20:32 +0200

    Add all test cases

--- pkg/resources/test_resource_data_type_diff_handling_acceptance_test.go
+++ pkg/resources/test_resource_data_type_diff_handling_acceptance_test.go
@@ -34,14 +34,123 @@ resource "%[3]s" "%[4]s" {
 `, configValue, envName, resourceType, resourceName)
 	}
 
-	testCases := []struct {
+	type DataTypeDiffHandlingTestCase struct {
 		ConfigValue    string
 		NewConfigValue string
 		ExternalValue  string
 		ExpectChanges  bool
-	}{
-		{"NUMBER(20, 4)", "VARCHAR(20)", "", true},
-		// TODO: add the rest of the use cases
+	}
+
+	changeInConfig := func(configValue string, newConfigValue string, expectChanges bool) DataTypeDiffHandlingTestCase {
+		return DataTypeDiffHandlingTestCase{
+			ConfigValue:    configValue,
+			NewConfigValue: newConfigValue,
+			ExpectChanges:  expectChanges,
+		}
+	}
+
+	externalChange := func(configValue string, externalValue string, expectChanges bool) DataTypeDiffHandlingTestCase {
+		return DataTypeDiffHandlingTestCase{
+			ConfigValue:   configValue,
+			ExternalValue: externalValue,
+			ExpectChanges: expectChanges,
+		}
+	}
+
+	testCases := []DataTypeDiffHandlingTestCase{
+		// different data type
+		changeInConfig("NUMBER(20, 4)", "VARCHAR(20)", true),
+		changeInConfig("NUMBER(20, 4)", "VARCHAR", true),
+		changeInConfig("NUMBER(20)", "VARCHAR(20)", true),
+		changeInConfig("NUMBER", "VARCHAR(20)", true),
+		changeInConfig("NUMBER", "VARCHAR", true),
+
+		// same data type - no attributes before
+		changeInConfig("NUMBER", "NUMBER", false),
+		changeInConfig("NUMBER", "NUMBER(20)", true),
+		changeInConfig("NUMBER", "NUMBER(20, 4)", true),
+		changeInConfig("NUMBER", "NUMBER(38)", true),
+		changeInConfig("NUMBER", "NUMBER(38, 0)", true),
+
+		// same data type - one attribute before
+		changeInConfig("NUMBER(20)", "NUMBER(20)", false),
+		changeInConfig("NUMBER(20)", "NUMBER", true),
+		changeInConfig("NUMBER(20)", "NUMBER(21)", true),
+		changeInConfig("NUMBER(20)", "NUMBER(20, 0)", true),
+		changeInConfig("NUMBER(20)", "NUMBER(20, 4)", true),
+		changeInConfig("NUMBER(20)", "NUMBER(21, 4)", true),
+
+		// same data type - two attributes before
+		changeInConfig("NUMBER(20, 3)", "NUMBER(20, 3)", false),
+		changeInConfig("NUMBER(20, 3)", "NUMBER", true),
+		changeInConfig("NUMBER(20, 3)", "NUMBER(20)", true),
+		changeInConfig("NUMBER(20, 3)", "NUMBER(20, 4)", true),
+		changeInConfig("NUMBER(20, 3)", "NUMBER(21)", true),
+		changeInConfig("NUMBER(20, 3)", "NUMBER(21, 3)", true),
+		changeInConfig("NUMBER(20, 3)", "NUMBER(21, 4)", true),
+
+		// same data type - one attribute but default before
+		changeInConfig("NUMBER(38)", "NUMBER(38)", false),
+		changeInConfig("NUMBER(38)", "NUMBER", true),
+		changeInConfig("NUMBER(38)", "NUMBER(20)", true),
+		changeInConfig("NUMBER(38)", "NUMBER(20, 3)", true),
+		changeInConfig("NUMBER(38)", "NUMBER(38, 2)", true),
+		changeInConfig("NUMBER(38)", "NUMBER(38, 0)", true),
+
+		// same data type - two attributes but default before
+		changeInConfig("NUMBER(38, 0)", "NUMBER(38, 0)", false),
+		changeInConfig("NUMBER(38, 0)", "NUMBER", true),
+		changeInConfig("NUMBER(38, 0)", "NUMBER(38)", true),
+		changeInConfig("NUMBER(38, 0)", "NUMBER(20)", true),
+		changeInConfig("NUMBER(38, 0)", "NUMBER(20, 3)", true),
+		changeInConfig("NUMBER(38, 0)", "NUMBER(38, 2)", true),
+
+		// different data type
+		externalChange("NUMBER(20, 4)", "VARCHAR(20)", true),
+		externalChange("NUMBER(20, 4)", "VARCHAR", true),
+		externalChange("NUMBER(20)", "VARCHAR(20)", true),
+		externalChange("NUMBER", "VARCHAR(20)", true),
+		externalChange("NUMBER", "VARCHAR", true),
+
+		// same data type - no attributes before
+		externalChange("NUMBER", "NUMBER", false),
+		externalChange("NUMBER", "NUMBER(20)", true),
+		externalChange("NUMBER", "NUMBER(20, 4)", true),
+		externalChange("NUMBER", "NUMBER(38)", true),
+		externalChange("NUMBER", "NUMBER(38, 0)", true),
+
+		// same data type - one attribute before
+		externalChange("NUMBER(20)", "NUMBER(20)", false),
+		externalChange("NUMBER(20)", "NUMBER", false),
+		externalChange("NUMBER(20)", "NUMBER(21)", true),
+		externalChange("NUMBER(20)", "NUMBER(20, 0)", true),
+		externalChange("NUMBER(20)", "NUMBER(20, 4)", true),
+		externalChange("NUMBER(20)", "NUMBER(21, 4)", true),
+
+		// same data type - two attributes before
+		externalChange("NUMBER(20, 3)", "NUMBER(20, 3)", false),
+		externalChange("NUMBER(20, 3)", "NUMBER", false),
+		externalChange("NUMBER(20, 3)", "NUMBER(20)", true),
+		externalChange("NUMBER(20, 3)", "NUMBER(20, 4)", true),
+		externalChange("NUMBER(20, 3)", "NUMBER(21)", true),
+		externalChange("NUMBER(20, 3)", "NUMBER(21, 3)", true),
+		externalChange("NUMBER(20, 3)", "NUMBER(21, 4)", true),
+
+		// same data type - one attribute but default before
+		externalChange("NUMBER(38)", "NUMBER(38)", false),
+		externalChange("NUMBER(38)", "NUMBER", false),
+		externalChange("NUMBER(38)", "NUMBER(20)", true),
+		externalChange("NUMBER(38)", "NUMBER(20, 3)", true),
+		externalChange("NUMBER(38)", "NUMBER(38, 2)", true),
+		externalChange("NUMBER(38)", "NUMBER(38, 0)", true),
+
+		// same data type - two attributes but default before
+		externalChange("NUMBER(38, 0)", "NUMBER(38, 0)", false),
+		externalChange("NUMBER(38, 0)", "NUMBER", false),
+		externalChange("NUMBER(38, 0)", "NUMBER(38)", true),
+		externalChange("NUMBER(38, 0)", "NUMBER(20)", true),
+		externalChange("NUMBER(38, 0)", "NUMBER(20, 3)", true),
+		externalChange("NUMBER(38, 0)", "NUMBER(38, 2)", true),
 	}
 
 	for _, testCase := range testCases {
